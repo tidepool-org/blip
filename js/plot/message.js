dcce7886b8653d9b37a7bafa98e592e512468930
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-01-30 00:27:20 -0800

    plot message images in their pool

---
+++ js/plot/message.js
@@ -0,0 +1,42 @@
+module.exports = function(pool, opts) {
+
+  var opts = opts || {};
+
+  var defaults = {
+    xScale: pool.xScale().copy()
+  };
+
+  _.defaults(opts, defaults);
+
+  function cbg(selection) {
+    selection.each(function(currentData) {
+      var messages = d3.select(this)
+        .selectAll('image')
+        .data(currentData, function(d) {
+          // leveraging the timestamp of each datapoint as the ID for D3's binding
+          // only top-level message get an icon on the timeline
+          if (d.parentMessage === '') {
+            return d.normalTime;
+          }
+        });
+      messages.enter()
+        .append('image')
+        .attr({ 
+          'xlink:href': '../img/post_it.svg',
+          'x': function(d) {
+            return opts.xScale(Date.parse(d.normalTime)) - opts.size / 2;
+          },
+          'y': pool.height() / 2 - opts.size / 2,
+          'width': opts.size,
+          'height': opts.size,
+          'id': function(d) {
+            return d.id;
+          }
+        })
+        .classed({'d3-image': true, 'd3-message': true});
+      messages.exit().remove();
+    });
+  }
+
+  return cbg; 
+};
