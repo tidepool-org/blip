6af42acf6a29afefaae5001974c1b3fa00cb4869
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-01-30 17:35:12 -0800

    switch to bolus groups for managing boluses enter() and exit() selections; add bolus.x for easy getting of x-coords

--- js/plot/bolus.js
+++ js/plot/bolus.js
@@ -9,35 +9,44 @@ module.exports = function(pool, opts) {
 
   _.defaults(opts, defaults);
 
-  function carbs(selection) {
+  function bolus(selection) {
     selection.each(function(currentData) {
-      var rects = d3.select(this)
-        .selectAll('rect')
+      var boluses = d3.select(this)
+        .selectAll('g')
         .data(currentData, function(d) {
           // leveraging the timestamp of each datapoint as the ID for D3's binding
           return d.normalTime;
         });
-      rects.enter()
-        .append('rect')
+      var bolusGroups = boluses.enter()
+        .append('g')
+        .attr({
+          'class': 'd3-bolus-group'
+        });
+      var top = opts.yScale.range()[0];
+      bolusGroups.append('rect')
         .attr({
           'x': function(d) {
-            return opts.xScale(Date.parse(d.normalTime)) - opts.width/2;
+            return bolus.x(d);
           },
           'y': function(d) {
             return opts.yScale(d.value);
           },
           'width': opts.width,
           'height': function(d) {
-            return opts.yScale.range()[0] - opts.yScale(d.value);
+            return top - opts.yScale(d.value);
           },
           'class': 'd3-rect-bolus d3-bolus',
           'id': function(d) {
-            return d.normalTime + ' ' + d.value;
+            return d.normalTime + ' ' + d.value + ' ' + d.recommended + ' recommended';
           }
         });
-        rects.exit().remove();
+      boluses.exit().remove();
     });
   }
+  
+  bolus.x = function(d) {
+    return opts.xScale(Date.parse(d.normalTime)) - opts.width/2;
+  };
 
-  return carbs; 
+  return bolus; 
 };
