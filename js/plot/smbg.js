348df40a08795202316d1c74320776ac69dbcf6c
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-02-03 13:12:45 -0800

    added images for smbg as per design instead of filled circles

--- js/plot/smbg.js
+++ js/plot/smbg.js
@@ -10,49 +10,57 @@ module.exports = function(pool, opts) {
       'high': 200,
       'very-high': 300
     },
+    size: 16,
     xScale: pool.xScale().copy(),
     yScale: d3.scale.linear().domain([0, 400]).range([pool.height(), 0])
   };
 
   _.defaults(opts, defaults);
 
-  function cbg(selection) {
+  function smbg(selection) {
     selection.each(function(currentData) {
       var circles = d3.select(this)
-        .selectAll('circle')
+        .selectAll('image')
         .data(currentData, function(d) {
           // leveraging the timestamp of each datapoint as the ID for D3's binding
           return d.normalTime;
         });
       circles.enter()
-        .append('circle')
+        .append('image')
         .attr({
-          'cx': function(d) {
-            return opts.xScale(Date.parse(d.normalTime));
-          },
-          'class': function(d) {
-            if (d.value < opts.classes['low']) {
-              return 'd3-bg-low';
+          'xlink:href': function(d) {
+            if (d.value <= opts.classes['very-low']) {
+              return '../img/smbg/very_low.svg';
             }
-            else if (d.value < opts.classes['target']) {
-              return 'd3-bg-target';
+            else if ((d.value > opts.classes['very-low']) && (d.value <= opts.classes['low'])) {
+              return '../img/smbg/low.svg';
             }
-            else {
-              return 'd3-bg-high'
+            else if ((d.value > opts.classes['low']) && (d.value <= opts.classes['target'])) {
+              return '../img/smbg/target.svg';
+            }
+            else if ((d.value > opts.classes['target']) && (d.value <= opts.classes['high'])) {
+              return '../img/smbg/high.svg';
+            }
+            else if (d.value > opts.classes['high']) {
+              return '../img/smbg/very_high.svg';
             }
           },
-          'cy': function(d) {
-            return opts.yScale(d.value);
+          'x': function(d) {
+            return opts.xScale(Date.parse(d.normalTime)) - opts.size / 2;
           },
-          'r': 7,
+          'y': function(d) {
+            return opts.yScale(d.value) - opts.size / 2;
+          },
+          'width': opts.size,
+          'height': opts.size,
           'id': function(d) {
             return d.normalTime + ' ' + d.value;
           }
         })
-        .classed({'d3-circle': true, 'd3-smbg': true});
+        .classed({'d3-image': true, 'd3-smbg': true, 'd3-smbg-image': true});
       circles.exit().remove();
     });
   }
 
-  return cbg; 
+  return smbg; 
 };
