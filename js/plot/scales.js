fcd33d6723e46b2682eeaed649ccd4f513345023
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-02-11 02:34:44 -0800

    added basal scale

--- js/plot/scales.js
+++ js/plot/scales.js
@@ -15,30 +15,31 @@
  * == BSD2 LICENSE ==
  */
 
-var bg = function(data, pool) {
-  var scale = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.value; })])
-    .range([pool.height(), 0]);
-
-  return scale;
+var scales = {
+  bg: function(data, pool) {
+    var scale = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([pool.height(), 0]);
+    return scale;
+  },
+  carbs: function(data, pool) {
+    var scale = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([0, 0.475 * pool.height()]);
+    return scale;
+  },
+  bolus: function(data, pool) {
+    var scale = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([pool.height(), 0.525 * pool.height()]);
+    return scale;
+  },
+  basal: function(data, pool) {
+    var scale = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([pool.height(), 0]);
+    return scale;
+  }
 };
 
-var carbs = function(data, pool) {
-  var scale = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.value; })])
-    .range([0, 0.475 * pool.height()]);
-
-  return scale;
-};
-
-var bolus = function(data, pool) {
-  var scale = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.value; })])
-    .range([pool.height(), 0.525 * pool.height()]);
-
-  return scale;
-};
-
-module.exports.bg = bg;
-module.exports.carbs = carbs;
-module.exports.bolus = bolus;
+module.exports = scales;
