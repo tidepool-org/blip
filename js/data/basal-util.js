12a6f0ee3ff199581ab5a56eaa7aa3abc95cc9e2
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-02-11 12:05:19 -0800

    bugfixes in basal-util to produce continuous, non-overlapping basal segments

--- js/data/basal-util.js
+++ js/data/basal-util.js
@@ -41,18 +41,47 @@ module.exports = function() {
       return;
     };
 
-    var checkNextSegment = function(tempSegment, i) {
+    var checkNextSegment = function(tempSegment, i, j) {
       var segment = scheduled[i + 1];
-      recursedSegments.push(segment);
+      var start = new Date(segment.start).valueOf();
       var end = new Date(segment.end).valueOf();
       var tempEnd = new Date(tempSegment.end).valueOf();
-      recursed = true;
-      if (tempEnd < end) {
+      var nextTemp = temp[j + 1];
+      if (nextTemp) {
+        var nextTempStart = new Date(nextTemp.start).valueOf();
+        var nextTempEnd = new Date(nextTemp.end).valueOf();
+      }
+      recursedSegments.push(scheduled[i]);
+      recursedSegments.push(segment);
+      // = basal-temp-both-ends.json
+      // one temp basal ends with a particular scheduled segment and another one begins
+      // scheduled segment split into three parts
+      // then following scheduled segment (possibly recursively) checked for containing the end of the second temp basal
+      if ((tempEnd > start) && (tempEnd < end) && (nextTempStart > start)) {
+        var firstCouldHaveBeen = _.clone(segment);
+        firstCouldHaveBeen.end = tempSegment.end;
+        undelivered.push(firstCouldHaveBeen);
+        var thisScheduled = _.clone(segment);
+        thisScheduled.start = tempSegment.end;
+        thisScheduled.end = nextTemp.start;
+        actual.push(thisScheduled);
+        var secondCouldHaveBeen = _.clone(segment);
+        secondCouldHaveBeen.start = nextTemp.start;
+        undelivered.push(secondCouldHaveBeen);
+        var firstTemp = _.clone(tempSegment);
+        firstTemp.start = segment.start;
+        actual.push(firstTemp);
+        var secondTemp = _.clone(nextTemp);
+        secondTemp.end = segment.end;
+        actual.push(secondTemp);
+        checkNextSegment(nextTemp, i + 1, j);
+      }
+      else if (tempEnd < end) {
         tempStartAtEndWithin(segment, tempSegment);
       }
       else if (tempEnd > end) {
         allTemp(segment, tempSegment);
-        checkNextSegment(tempSegment, i + 1);
+        checkNextSegment(tempSegment, i + 1, j);
       }
       else if (tempEnd === end) {
         allTemp(segment, tempSegment);
@@ -60,12 +89,11 @@ module.exports = function() {
       return;
     };
 
-    var tempIntervening = false;
-
     scheduled.forEach(function(segment, i) {
+      tempIntervening = false;
       var start = new Date(segment.start).valueOf();
       var end = new Date(segment.end).valueOf();
-      temp.forEach(function(tempSegment) {
+      temp.forEach(function(tempSegment, j) {
         var tempStart = new Date(tempSegment.start).valueOf();
         var tempEnd = new Date(tempSegment.end).valueOf();
         // = basal-contained.json
@@ -83,29 +111,31 @@ module.exports = function() {
           var secondScheduled = _.clone(segment);
           secondScheduled.start = tempSegment.end;
           actual.push(secondScheduled);
-          tempIntervening = true;
+          recursedSegments.push(segment);
           return;
         }
         // = basal-temp-start.json
         // exceedingly rare case where temp basal starts at the exact same time as the start of the scheduled segment
         // scheduled gets split into two segments
         else if ((tempStart === start) && (tempEnd < end)) {
-          tempStartAtEndWithin(segment, tempSegment);
-          tempIntervening = true;
+          if (recursedSegments.indexOf(segment) !== -1) {
+            tempStartAtEndWithin(segment, tempSegment); 
+          }
           return;
         }
         // = basal-temp-end.json
         // exceedingly rare case where temp basal ends at the exact same time as the end of the scheduled segment
         // scheduled gets split into two segments
         else if ((tempStart > start) && (tempEnd === end)) {
-          var firstScheduled = _.clone(segment);
-          firstScheduled.end = tempSegment.start;
-          actual.push(firstScheduled);
-          var couldHaveBeen = _.clone(segment);
-          couldHaveBeen.start = tempSegment.start;
-          undelivered.push(couldHaveBeen);
-          actual.push(tempSegment);
-          tempIntervening = true;
+          if (recursedSegments.indexOf(segment) !== -1) {
+            var firstScheduled = _.clone(segment);
+            firstScheduled.end = tempSegment.start;
+            actual.push(firstScheduled);
+            var couldHaveBeen = _.clone(segment);
+            couldHaveBeen.start = tempSegment.start;
+            undelivered.push(couldHaveBeen);
+            actual.push(tempSegment);
+          }
           return;
         }
         // = basal-temp-two-scheduled.json
@@ -114,25 +144,22 @@ module.exports = function() {
         // scheduled segment gets split into two segments
         // following scheduled segment are (possibly recursively) checked for containing the end of the temp basal
         else if ((tempStart > start) && (tempStart < end)) {
-          var firstScheduled = _.clone(segment);
-          firstScheduled.end = tempSegment.start;
-          actual.push(firstScheduled);
-          var couldHaveBeen = _.clone(segment);
-          couldHaveBeen.start = tempSegment.start;
-          undelivered.push(couldHaveBeen);
-          beginningOfTemp = _.clone(tempSegment);
-          beginningOfTemp.end = segment.end;
-          actual.push(beginningOfTemp);
-          checkNextSegment(tempSegment, i);
-          tempIntervening = true;
+          if (recursedSegments.indexOf(segment) !== -1) {
+            var firstScheduled = _.clone(segment);
+            firstScheduled.end = tempSegment.start;
+            actual.push(firstScheduled);
+            var couldHaveBeen = _.clone(segment);
+            couldHaveBeen.start = tempSegment.start;
+            undelivered.push(couldHaveBeen);
+            beginningOfTemp = _.clone(tempSegment);
+            beginningOfTemp.end = segment.end;
+            actual.push(beginningOfTemp);
+            checkNextSegment(tempSegment, i, j);
+          }
           return;
         }
-        // = basal-template.json
-        else {
-          tempIntervening = false;
-        }
       });
-      if ((!tempIntervening) && (recursedSegments.indexOf(segment) == -1)) {
+      if (recursedSegments.indexOf(segment) == -1) {
         actual.push(segment);
       }
     });
@@ -144,7 +171,25 @@ module.exports = function() {
       return new Date(u.start).valueOf();
     });
 
-    return {"actual": actual, "undelivered": undelivered};
+    var nonContinuous = [];
+
+    actual.forEach(function(segment, i) {
+      if (actual[i + 1])
+      {      
+        if (!(segment.end === actual[i + 1].start)) {
+          nonContinuous.push([segment, actual[i + 1]]);
+          return;
+        }
+      }
+    });
+
+    return {
+      "actualSegments": actual,
+      "undeliveredSegments": undelivered,
+      "scheduledSegments": scheduled,
+      "tempSegments": temp,
+      "nonContinuousSegments": nonContinuous
+    };
   }  
 
   return basal;
