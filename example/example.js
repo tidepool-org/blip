eab0ac71ce376b54bce71a73fc8bc84db3716020
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-02-21 16:18:57 -0800

    add logic to prevent attempting to locate oneDay view at a location outside of the data available

--- example/example.js
+++ example/example.js
@@ -312,19 +312,30 @@ function oneDayChart(el, options) {
 
     var start, end, localData;
 
-    if (!arguments.length) {
+    var mostRecent = function() {
       start = chart.initialEndpoints[0];
       end = chart.initialEndpoints[1];
       localData = chart.getData(chart.initialEndpoints, 'both');
+    };
+
+    if (!arguments.length) {
+      mostRecent();
     }
     else {
       start = new Date(datetime);
-      end = new Date(start);
-      start.setUTCHours(start.getUTCHours() - 12);
-      end.setUTCHours(end.getUTCHours() + 12);
+      if ((start.valueOf() < chart.endpoints[0]) || (start.valueOf() > chart.endpoints[1])) {
+        log("Please don't ask tideline to locate at a date that's outside of your data!");
+        log("Rendering most recent data instead.");
+        mostRecent();
+      }
+      else {
+        end = new Date(start);
+        start.setUTCHours(start.getUTCHours() - 12);
+        end.setUTCHours(end.getUTCHours() + 12);
 
-      localData = chart.getData([start, end], 'both');
-      chart.beginningOfData(start).endOfData(end);
+        localData = chart.getData([start, end], 'both');
+        chart.beginningOfData(start).endOfData(end);
+      }
     }
 
     chart.allData(localData, [start, end]);
