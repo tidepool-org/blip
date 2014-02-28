b3d0aa65156e3480b19427af645ac6f483da899a
Eric Tschetter <cheddar@tidepool.org>
2014-02-28 10:34:30 -0600

    Remove spurious requires and other cruft from server.js

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var http = require('http');
 var connect = require('connect');
 
-var gulp = require('gulp');
-var path = require('path');
-var pkg = require('./package.json');
-var send = require('./lib/gulp-send');
-var template = require('gulp-template');
-
 var buildDir = 'dist';
-var ROOT = __dirname;
 
 var app = connect();
 
 var staticDir = __dirname + '/' + buildDir;
-
 app.use(connect.static(staticDir));
 
 var server = http.createServer(app).listen(process.env.PORT || 3000, function() {
