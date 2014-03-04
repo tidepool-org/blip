d424aa07f91371b33cddf64bb3693f3765e183f1
Nicolas Hery <hery.nicolas@gmail.com>
2014-03-04 15:30:18 +0100

    Add query string handling to router

--- app/router.js
+++ app/router.js
@@ -16,6 +16,7 @@
 var Router = window.Router;
 var bows = window.bows;
 var _ = window._;
+var queryString = window.queryString;
 
 var router = new Router();
 
@@ -23,20 +24,26 @@ router.log = bows('Router');
 
 var configuration = {
   before: function() {
+    var redirectRoute;
     var routeBase = router.getRoute(0);
     var isAuthenticated = router.isAuthenticated();
+    // Strip potential query string before checking if route requires auth
+    routeBase = routeBase.replace(/(\?.*)/, '');
     var isNoAuthRoute = _.contains(router.noAuthRoutes, routeBase);
 
     if (!isAuthenticated && !isNoAuthRoute) {
       router.log('Not logged in, redirecting');
-      router.setRoute(router.defaultNotAuthenticatedRoute);
+      // Redirect but keep query string
+      redirectRoute = router.defaultNotAuthenticatedRoute + router.qs();
+      router.setRoute(redirectRoute);
       // Stop current routing and let new routing take over
       return false;
     }
 
     if (isAuthenticated && isNoAuthRoute) {
       router.log('Already logged in, redirecting');
-      router.setRoute(router.defaultAuthenticatedRoute);
+      redirectRoute = router.defaultAuthenticatedRoute + router.qs();
+      router.setRoute(redirectRoute);
       return false;
     }
   },
@@ -63,6 +70,8 @@ router.setup = function(routes, options) {
     options.defaultNotAuthenticatedRoute || '/';
   this.defaultAuthenticatedRoute =
     options.defaultAuthenticatedRoute || '/';
+
+  routes = this._addRoutesWithQueryStrings(routes);
   
   _.forEach(routes, function(handler, route) {
     self.on(route, handler);
@@ -84,6 +93,69 @@ router._getRouteFirstFragment = function(route) {
     return result[1];
   }
   return '';
+};
+
+// Register query string fragments
+router.param('qs', /(\?.*)/);
+router.param('idAndQs', /([._a-zA-Z0-9-]+)(\?.*)/);
+
+// Duplicate routes in routing table:
+// For each route, add the same route with an appended query string pattern
+// and make both '/patients/11' and '/patients/11?foo=bar'
+// map to the same handler
+router._addRoutesWithQueryStrings = function(routingTable) {
+  var self = this;
+  var newRoutingTable = {};
+  _.forEach(routingTable, function(handler, route) {
+    var routeWithQueryString = self._addQueryStringFragmentToRoute(route);
+    newRoutingTable[route] = handler;
+    newRoutingTable[routeWithQueryString] = handler;
+  });
+  return newRoutingTable;
+};
+
+router._addQueryStringFragmentToRoute = function(route) {
+  // Case: '/patients/:id'
+  var endingId = /:\w+$/;
+  if (route.match(endingId)) {
+    return route.replace(endingId, ':idAndQs');
+  }
+
+  // Case: '/patients', '/'
+  return route + ':qs';
+};
+
+// Implement some convenience functions to grab info from the URL
+// Inspired by AngularJS' `$location` service
+// http://docs.angularjs.org/guide/dev_guide.services.$location
+router.url = function() {
+  var url = window.location && window.location.hash;
+  url = url || '';
+  url.replace(/^#/, '');
+  return url;
+};
+
+router.path = function() {
+  var url = this.url();
+  // Strip potential query string
+  var path = url.replace(/(\?.*)/, '');
+  return path;
+};
+
+router.qs = function() {
+  var qs = '';
+  var url = this.url();
+  var match = url.match(/(\?.*)/);
+  if (match) {
+    qs = match[0];
+  }
+  return qs;
+};
+
+router.search = function(newSearch) {
+  var qs = this.qs();
+  var search = queryString.parse(qs);
+  return search;
 };
 
 router.start = function() {
