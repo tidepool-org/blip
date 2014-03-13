6af1d759218a30b28634f318c1eb23ed50681c69
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-03-13 01:57:44 -0700

    check in bower components for testing

---
+++ test/duration.js
@@ -0,0 +1,179 @@
+var Duration = (function () {
+
+    var millisecond = 1,
+        second      = 1000 * millisecond,
+        minute      = 60   * second,
+        hour        = 60   * minute,
+        day         = 24   * hour,
+        week        = 7    * day;
+
+    var unitMap = {
+        "ms" : millisecond,
+        "s"  : second,
+        "m"  : minute,
+        "h"  : hour,
+        "d"  : day,
+        "w"  : week
+    };
+
+    var Duration = function (value) {
+        if (value instanceof Duration) {
+          return value;
+        }
+        switch (typeof value) {
+            case "number":
+                this._milliseconds = value;
+                break;
+            case "string":
+                this._milliseconds = Duration.parse(value).valueOf();
+                break;
+            case "undefined":
+                this._milliseconds = 0;
+                break;
+            default:
+                throw new Error("invalid duration: " + value);
+        }
+    };
+
+    Duration.millisecond = new Duration(millisecond);
+    Duration.second      = new Duration(second);
+    Duration.minute      = new Duration(minute);
+    Duration.hour        = new Duration(hour);
+    Duration.day         = new Duration(day);
+    Duration.week        = new Duration(week);
+
+    Duration.prototype.nanoseconds = function () {
+        return this._milliseconds * 1000000;
+    };
+
+    Duration.prototype.microseconds = function () {
+        return this._milliseconds * 1000;
+    };
+
+    Duration.prototype.milliseconds = function () {
+        return this._milliseconds;
+    };
+
+    Duration.prototype.seconds = function () {
+        return Math.floor(this._milliseconds / second);
+    };
+
+    Duration.prototype.minutes = function () {
+        return Math.floor(this._milliseconds / minute);
+    };
+
+    Duration.prototype.hours = function () {
+        return Math.floor(this._milliseconds / hour);
+    };
+
+    Duration.prototype.days = function () {
+      return Math.floor(this._milliseconds / day);
+    };
+
+    Duration.prototype.weeks = function () {
+      return Math.floor(this._milliseconds / week);
+    };
+
+    Duration.prototype.toString = function () {
+      var str          = "",
+          milliseconds = Math.abs(this._milliseconds),
+          sign         = this._milliseconds < 0 ? "-" : "";
+
+      // no units for 0 duration
+      if (milliseconds === 0) {
+        return "0";
+      }
+
+      // days
+      var days = Math.floor(milliseconds / day);
+      if (days !== 0) {
+        milliseconds -= day * days;
+        str += days.toString() + "d";
+      }
+
+      // hours
+      var hours = Math.floor(milliseconds / hour);
+      if (hours !== 0) {
+        milliseconds -= hour * hours;
+        str += hours.toString() + "h";
+      }
+
+      // minutes
+      var minutes = Math.floor(milliseconds / minute);
+      if (minutes !== 0) {
+        milliseconds -= minute * minutes;
+        str += minutes.toString() + "m";
+      }
+
+      // seconds
+      var seconds = Math.floor(milliseconds / second);
+      if (seconds !== 0) {
+        milliseconds -= second * seconds;
+        str += seconds.toString() + "s";
+      }
+
+      // milliseconds
+      if (milliseconds !== 0) {
+        str += milliseconds.toString() + "ms";
+      }
+
+      return sign + str;
+    };
+
+    Duration.prototype.valueOf = function () {
+      return this._milliseconds;
+    };
+
+    Duration.parse = function (duration) {
+
+        if (duration === "0" || duration === "+0" || duration === "-0") {
+          return new Duration(0);
+        }
+
+        var regex = /([\-\+\d\.]+)([a-z]+)/g,
+            total = 0,
+            count = 0,
+            sign  = duration[0] === '-' ? -1 : 1,
+            unit, value, match;
+
+        while (match = regex.exec(duration)) {
+
+            unit  = match[2];
+            value = Math.abs(parseFloat(match[1]));
+            count++;
+
+            if (isNaN(value)) {
+              throw new Error("invalid duration");
+            }
+
+            if (typeof unitMap[unit] === "undefined") {
+              throw new Error("invalid unit: " + unit);
+            }
+
+            total += value * unitMap[unit];
+        }
+
+        if (count === 0) {
+          throw new Error("invalid duration");
+        }
+
+        return new Duration(total * sign);
+    };
+
+    Duration.fromMicroseconds = function (us) {
+        var ms = Math.floor(us / 1000);
+        return new Duration(ms);
+    };
+
+    Duration.fromNanoseconds = function (ns) {
+        var ms = Math.floor(ns / 1000000);
+        return new Duration(ms);
+    };
+
+    return Duration;
+
+}).call(this);
+
+if (typeof module !== "undefined") {
+   module.exports = Duration;
+}
--- testling.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bower install
-browserify test/*util.js -o test/test.js
