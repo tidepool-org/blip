9c943d85006de2e3042d960cc1b9b84a71d01377
Nicolas Hery <hery.nicolas@gmail.com>
2014-03-24 11:54:45 +0100

    Skip Sauce Labs tests in CI if env vars not set
    See:
    https://github.com/travis-ci/travis-ci/issues/1946

--- test/ci/test.sh
+++ test/ci/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ "$TEST_SUITE" == "unit" ]; then
-  ./node_modules/.bin/grunt test-saucelabs-travis
-elif [ "$TEST_SUITE" == "e2e" ]; then
-  make test-e2e
+if [ "$SAUCE_USERNAME" ]; then
+  if [ "$TEST_SUITE" == "unit" ]; then
+    ./node_modules/.bin/grunt test-saucelabs-travis
+  elif [ "$TEST_SUITE" == "e2e" ]; then
+    make test-e2e
+  else
+    echo "Unknown test suite. Please set TEST_SUITE=unit or TEST_SUITE=e2e."
+  fi
 else
-  echo "Unknown test suite. Please set TEST_SUITE=unit or TEST_SUITE=e2e."
+  echo "No SAUCE_USERNAME env variable, skipping Sauce Labs tests."
 fi
