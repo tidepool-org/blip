80bb74305dfa76a61f5e602d31233dddede78d8e
Nicolas Hery <hery.nicolas@gmail.com>
2014-04-10 17:13:41 +0200

    Fix typo in scales.js

--- js/plot/util/scales.js
+++ js/plot/util/scales.js
@@ -1,15 +1,15 @@
-/* 
+/*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -23,7 +23,7 @@ var scales = {
 
   bg: function(data, pool, pad) {
     var ext = d3.extent(data, function(d) { return d.value; });
-    if (ext > this.MAX_CBG) {
+    if (ext[1] > this.MAX_CBG) {
       return d3.scale.linear()
         .domain([0, this.MAX_CBG])
         .range([pool.height() - pad, pad])
@@ -37,7 +37,7 @@ var scales = {
   },
   bgLog: function(data, pool, pad) {
     var ext = d3.extent(data, function(d) { return d.value; });
-    if (ext > this.MAX_CBG) {
+    if (ext[1] > this.MAX_CBG) {
       return d3.scale.log()
         .domain(ext[0], this.MAX_CBG)
         .range([pool.height() - pad, pad])
