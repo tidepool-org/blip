5266fb339e193e4435c0abe20705b5d5c1def619
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-04-13 18:34:22 -0700

    add function to format milliseconds-in-24-hours value as a hour string - e.g., '12:00 am'

--- js/data/util/format.js
+++ js/data/util/format.js
@@ -1,4 +1,8 @@
+var d3 = require('../../lib/').d3;
+
 var format = {
+
+  MS_IN_24: 86400000,
 
   fixFloatingPoint: function(n) {
     return parseFloat(n.toFixed(3));
@@ -11,6 +15,11 @@ var format = {
     else {
       return parseInt(Math.round(f * 100), 10) + '%';
     }
+  },
+
+  millisecondsAsTimeOfDay: function(i) {
+    var d = new Date(i);
+    return d3.time.format.utc('%-I:%M %p')(d);
   }
 
 };
