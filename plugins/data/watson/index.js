25054571a72754f5c3da2b6102e0b61c39f6eb85
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-04-15 21:58:30 -0700

    add TODO to Watson re: removing references to utcTime

--- plugins/data/watson/index.js
+++ plugins/data/watson/index.js
@@ -48,6 +48,8 @@ module.exports = {
   normalize: function(i) {
     try {
       if (i.utcTime) {
+        // TODO: remove after removed from demo data, deprecated
+        // messages now come to blip with a normalTime
         var d = new Date(i.utcTime);
         var offsetMinutes = d.getTimezoneOffset();
         d.setMinutes(d.getMinutes() - offsetMinutes);
