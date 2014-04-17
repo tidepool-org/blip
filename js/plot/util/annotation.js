5fcc42a3da657199609ec1621cc268035bf39afb
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-04-16 18:51:12 -0700

    add foreignObject tooltips on hover with default text

--- js/plot/util/annotation.js
+++ js/plot/util/annotation.js
@@ -1,4 +1,7 @@
 var d3 = require('../../lib/').d3;
+var _ = require('../../lib/')._;
+
+var shapes = require('./shapes');
 
 var log = require('../../lib/').bows('AnnotationIcon');
 
@@ -6,17 +9,29 @@ module.exports = function(container, annotationsGroup) {
 
   var id, r = 8;
 
+  var defaults = {
+    'foWidth': 200,
+    'triangleWidth': 18,
+    'triangleHeight': 12,
+    'mainText': "We can't be 100% certain of the data displayed here because of how the device maker reports the data."
+  };
+
   function annotation(selection, opts) {
     opts = opts || {};
+
+    _.defaults(opts, defaults);
 
     if (!(opts.x && opts.y)) {
       log('Sorry, I need x and y coordinates to plot an annotation icon.');
       return;
     }
 
+    selection = selection.append('g')
+      .attr('class', 'd3-data-annotation-group');
+
     var x = annotation.xOffset(opts), y = annotation.yOffset(opts);
 
-    selection.append('circle')
+    var hoverTarget = selection.append('circle')
       .attr({
         'cx': x,
         'cy': y,
@@ -30,7 +45,54 @@ module.exports = function(container, annotationsGroup) {
         'class': 'd3-text-data-annotation'
       })
       .text('?');
+
+    if (opts.hoverTarget != null) {
+      hoverTarget = opts.hoverTarget;
+    }
+    annotation.tooltip(x, y, opts, selection, hoverTarget);
   }
+
+  annotation.tooltip = function(x, y, opts, selection, hoverTarget) {
+    hoverTarget.on('mouseover', function() {
+      console.log('Hi there :D');
+      var fo = selection.append('foreignObject')
+        .attr({
+          'x': x,
+          'y': y,
+          'width': opts.foWidth,
+          'transform': 'translate(' + (0-(3/2*opts.triangleWidth)) +
+            ',' + opts.triangleHeight + ')',
+          'class': 'd3-tooltip-data-annotation'
+        });
+      var div = fo.append('xhtml:body')
+        .append('div')
+        .attr('class', 'd3-div-data-annotation');
+      div.append('p')
+        .attr('class', 'd3-data-annotation-lead')
+        .html('Why is there a question mark here?');
+      div.append('p')
+        .html(opts.mainText);
+      var foHeight = div[0][0].getBoundingClientRect().height;
+      fo.attr('height', foHeight);
+      selection.insert('polygon', '.d3-tooltip-data-annotation')
+        .attr({
+          'points': shapes.tooltipPolygon({
+            'w': opts.foWidth,
+            'h': foHeight,
+            't': opts.triangleWidth,
+            'k': opts.triangleHeight
+          }),
+          'transform': 'translate(' + x + ',' + y + ')',
+          'width': opts.foWidth,
+          'height': opts.triangleHeight + foHeight,
+          'class': 'd3-polygon-data-annotation'
+        });
+    });
+    hoverTarget.on('mouseout', function() {
+      selection.selectAll('.d3-tooltip-data-annotation').remove();
+      selection.selectAll('.d3-polygon-data-annotation').remove();
+    });
+  };
 
   annotation.xOffset = function(opts, multiplier) {
     if (multiplier != null) {
