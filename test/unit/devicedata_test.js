b3409b0851c1aa6f83b3bb6740e5a11f3b09f480
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-05-05 18:32:40 -0700

    fix tests after adding deviceId for boluses

--- test/unit/devicedata_test.js
+++ test/unit/devicedata_test.js
@@ -28,22 +28,22 @@ describe('dataHelpers', function(){
     it('combines boluses', function(done){
       Rx.Observable.fromArray(
         [{
-           _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
+           id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 3.6,
            type: 'bolus', subType: 'dual/normal', joinKey: 'myJoinKey',
            source: 'demo'
          },
          {
-           _id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: 'abcde', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'myJoinKey',
            duration: 14400000, source: 'demo'
          },
          {
-           _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+           id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 0.1,
            type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey',
            source: 'demo'
          },
          {
-           _id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: '12345', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'yourJoinKey',
            duration: 14400000, source: 'demo'
          }])
@@ -53,14 +53,14 @@ describe('dataHelpers', function(){
         function(converted) {
           expect(converted).deep.equals(
             [{
-               _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 5.3,
+               id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 5.3,
                type: 'bolus',
                duration: 14400000,
                extended: true, initialDelivery: 3.6, extendedDelivery: 1.7,
                source: 'demo'
              },
              {
-               _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 1.8,
+               id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 1.8,
                type: 'bolus',
                duration: 14400000,
                extended: true, initialDelivery: 0.1, extendedDelivery: 1.7,
@@ -75,20 +75,20 @@ describe('dataHelpers', function(){
     it('fails if it gets something with the same subType before completing its current bolus', function(done){
       Rx.Observable.fromArray(
           [{
-             _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
+             id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
              type: 'bolus', subType: 'dual/normal', joinKey: 'myJoinKey'
            },
            {
-             _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+             id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
              type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey'
            },
            {
-             _id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+             id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
              type: 'bolus', subType: 'dual/square', joinKey: 'myJoinKey',
              duration: 14400000
            },
            {
-             _id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+             id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
              type: 'bolus', subType: 'dual/square', joinKey: 'yourJoinKey',
              duration: 14400000
            }])
@@ -110,20 +110,20 @@ describe('dataHelpers', function(){
     it('fails if it gets events with different joinKeys before completing its current bolus', function(done){
       Rx.Observable.fromArray(
         [{
-           _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
+           id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
            type: 'bolus', subType: 'dual/normal', joinKey: 'myJoinKey'
          },
          {
-           _id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'yourJoinKey',
            duration: 14400000
          },
          {
-           _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+           id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
            type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey'
          },
          {
-           _id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'myJoinKey',
            duration: 14400000
          }])
@@ -145,17 +145,17 @@ describe('dataHelpers', function(){
     it('passes through incomplete bolus records when completed', function(done){
       Rx.Observable.fromArray(
         [{
-           _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
+           id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 3.6,
            type: 'bolus', subType: 'dual/normal', joinKey: 'myJoinKey',
            source: 'demo'
          },
          {
-           _id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: 'abcde', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'myJoinKey',
            duration: 14400000, source: 'demo'
          },
          {
-           _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+           id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 0.1,
            type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey',
            source: 'demo'
          }])
@@ -165,14 +165,14 @@ describe('dataHelpers', function(){
         function(converted) {
           expect(converted).deep.equals(
             [{
-               _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 5.3,
+               id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 5.3,
                type: 'bolus',
                duration: 14400000,
                extended: true, initialDelivery: 3.6, extendedDelivery: 1.7,
                source: 'demo'
              },
              {
-               _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+               id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 0.1,
                type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey',
                _unmatched: true, source: 'demo'
              }]
@@ -185,54 +185,54 @@ describe('dataHelpers', function(){
     it('lets non-boluses pass through', function(done){
       Rx.Observable.fromArray(
         [{
-           _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 3.6,
+           id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 3.6,
            type: 'bolus', subType: 'dual/normal', joinKey: 'myJoinKey',
            source: 'demo'
          },
-         { _id: 'billy', type: 'howdy-ho'},
+         { id: 'billy', type: 'howdy-ho'},
          {
-           _id: 'abcde', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: 'abcde', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'myJoinKey',
            source: 'demo', duration: 14400000
          },
-         { _id: 'sally', type: 'you\'re cute'},
+         { id: 'sally', type: 'you\'re cute'},
          {
-           _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 0.1,
+           id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 0.1,
            type: 'bolus', subType: 'dual/normal', joinKey: 'yourJoinKey',
            source: 'demo'
          },
-         { _id: 'billy2', type: 'well, thank you.  Here\'s my number, call me maybe?'},
-         { _id: 'sally2', type: 'Ok Maybe.  I\'m Sally'},
+         { id: 'billy2', type: 'well, thank you.  Here\'s my number, call me maybe?'},
+         { id: 'sally2', type: 'Ok Maybe.  I\'m Sally'},
          {
-           _id: '12345', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
+           id: '12345', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00Z', value: 1.7,
            type: 'bolus', subType: 'dual/square', joinKey: 'yourJoinKey',
            duration: 14400000, source: 'demo'
          },
-         { _id: 'billy3', type: 'Do you consider yourself a comedian?' }])
+         { id: 'billy3', type: 'Do you consider yourself a comedian?' }])
         .tidepoolConvertBolus()
         .toArray()
         .subscribe(
         function(converted) {
           expect(converted).deep.equals(
             [{
-               _id: 'abcd', deviceTime: '2014-01-01T01:00:00', value: 5.3,
+               id: 'abcd', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 5.3,
                type: 'bolus',
                duration: 14400000,
                extended: true, initialDelivery: 3.6, extendedDelivery: 1.7,
                source: 'demo'
              },
-             { _id: 'billy', type: 'howdy-ho'},
-             { _id: 'sally', type: 'you\'re cute'},
+             { id: 'billy', type: 'howdy-ho'},
+             { id: 'sally', type: 'you\'re cute'},
              {
-               _id: '1234', deviceTime: '2014-01-01T01:00:00', value: 1.8,
+               id: '1234', deviceId: 'Demo - 123', deviceTime: '2014-01-01T01:00:00', value: 1.8,
                type: 'bolus',
                duration: 14400000,
                extended: true, initialDelivery: 0.1, extendedDelivery: 1.7,
                source: 'demo'
              },
-             { _id: 'billy2', type: 'well, thank you.  Here\'s my number, call me maybe?'},
-             { _id: 'sally2', type: 'Ok Maybe.  I\'m Sally'},
-             { _id: 'billy3', type: 'Do you consider yourself a comedian?' }]
+             { id: 'billy2', type: 'well, thank you.  Here\'s my number, call me maybe?'},
+             { id: 'sally2', type: 'Ok Maybe.  I\'m Sally'},
+             { id: 'billy3', type: 'Do you consider yourself a comedian?' }]
           );
           done();
         }
@@ -244,22 +244,22 @@ describe('dataHelpers', function(){
     it('combines basals', function(done){
       Rx.Observable.fromArray(
         [{
-           _id: 'abcd', type: 'basal', deliveryType: 'scheduled',
+           id: 'abcd', type: 'basal', deliveryType: 'scheduled',
            deviceTime: '2014-03-07T01:00:00', value: 0.65,
            scheduleName: 'night-shift', source: 'carelink'
          },
          {
-           _id: 'abcde', type: 'basal', deliveryType: 'temp',
+           id: 'abcde', type: 'basal', deliveryType: 'temp',
            deviceTime: '2014-03-07T01:38:27', value: 1.7,
            duration: 3600000, source: 'carelink'
          },
          {
-           _id: 'abcdef', type: 'basal', deliveryType: 'scheduled',
+           id: 'abcdef', type: 'basal', deliveryType: 'scheduled',
            deviceTime: '2014-03-07T04:00:00', value: 0.32,
            scheduleName: 'night-shift', source: 'carelink'
          },
          {
-           _id: 'abcdefg', type: 'basal', deliveryType: 'scheduled',
+           id: 'abcdefg', type: 'basal', deliveryType: 'scheduled',
            deviceTime: '2014-03-07T12:00:00', value: 1.02,
            scheduleName: 'night-shift', source: 'carelink'
          }])
@@ -269,25 +269,25 @@ describe('dataHelpers', function(){
         function(converted) {
           expect(converted).deep.equals(
             [{
-               _id: 'abcd', type: 'basal-rate-segment', deliveryType: 'scheduled',
+               id: 'abcd', type: 'basal-rate-segment', deliveryType: 'scheduled',
                deviceTime: '2014-03-07T01:00:00', start: '2014-03-07T01:00:00',
                end: '2014-03-07T04:00:00', value: 0.65,
                scheduleName: 'night-shift', source: 'carelink'
              },
              {
-               _id: 'abcde', type: 'basal-rate-segment', deliveryType: 'temp',
+               id: 'abcde', type: 'basal-rate-segment', deliveryType: 'temp',
                deviceTime: '2014-03-07T01:38:27', start: '2014-03-07T01:38:27',
                end: '2014-03-07T02:38:27', value: 1.7,
                duration: 3600000, source: 'carelink'
              },
              {
-               _id: 'abcdef', type: 'basal-rate-segment', deliveryType: 'scheduled',
+               id: 'abcdef', type: 'basal-rate-segment', deliveryType: 'scheduled',
                deviceTime: '2014-03-07T04:00:00', start: '2014-03-07T04:00:00',
                end: '2014-03-07T12:00:00', value: 0.32,
                scheduleName: 'night-shift', source: 'carelink'
              },
              {
-               _id: 'abcdefg', type: 'basal-rate-segment', deliveryType: 'scheduled',
+               id: 'abcdefg', type: 'basal-rate-segment', deliveryType: 'scheduled',
                deviceTime: '2014-03-07T12:00:00', start: '2014-03-07T12:00:00',
                end: null, value: 1.02,
                scheduleName: 'night-shift', source: 'carelink'
