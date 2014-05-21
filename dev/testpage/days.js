b7a560855638ae260161198cdab819a7402fa6e7
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-05-21 03:27:31 -0700

    'test page' data generators, all but basals

---
+++ dev/testpage/days.js
@@ -0,0 +1,222 @@
+/* 
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ * 
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
+var _ = require('lodash');
+var moment = require('moment');
+
+var types = require('./types');
+
+var day = {
+  HOURS_PER_DAY: 24,
+  MINUTES_PER_HOUR: 60,
+  addInterval: function(datetime, duration) {
+    duration = moment.duration(duration);
+    return moment(datetime).add(duration);
+  },
+  START: moment('2008-01-01T00:00:00.000Z')
+};
+
+var CBGDay = function(opts) {
+  var defaults = {
+    interval: 5,
+    patterns: {
+      steady: function(value) {
+        return value || 100;
+      }
+    },
+    spread: {
+      min: 39,
+      max: 401,
+      jump: 4
+    }
+  };
+
+  this.opts = opts || {};
+
+  this.opts = _.defaults(this.opts, defaults);
+
+  this.maxDatapoints = (this.HOURS_PER_DAY * this.MINUTES_PER_HOUR)/this.opts.interval;
+
+  this.generateFull = function(pattern, opts) {
+    opts = opts || {};
+    var datetime = opts.start || this.START, count = 0, events = [];
+    while (count <= this.maxDatapoints) {
+      events.push(new types.CBG(datetime.utc().format().slice(0,-6), pattern(opts.seedValue)));
+      datetime = this.addInterval(datetime, {'minutes': this.opts.interval});
+      count += 1;
+    }
+
+    for (var i = 0; i < events.length; ++i) {
+      events[i] = events[i].asObject();
+    }
+    return events;
+  };
+};
+
+CBGDay.prototype = day;
+
+var SMBGDay = function(opts) {
+  var defaults = {
+    interval: 180,
+    patterns: {
+      ident: function(value) {
+        return value || 100;
+      }
+    },
+    spread: {
+      min: 20,
+      max: 600,
+      jump: 25
+    }
+  };
+
+  this.opts = opts || {};
+
+  this.opts = _.defaults(this.opts, defaults);
+
+  this.generateFull = function(pattern, opts) {
+    opts = opts || {};
+    var datetime = opts.start || this.START, count = 0, events = [];
+    var values = [this.opts.spread.min, 79, 80, 180, 181, this.opts.spread.max];
+    if (opts.reverse) {
+      values.reverse();
+    }
+    while (count < values.length) {
+      events.push(new types.SMBG(datetime.utc().format().slice(0, -6), pattern(values[count])));
+      datetime = this.addInterval(datetime, {'minutes': this.opts.interval});
+      count += 1;
+    }
+
+    for (var i = 0; i < events.length; ++i) {
+      events[i] = events[i].asObject();
+    }
+    return events;
+  };
+};
+
+SMBGDay.prototype = day;
+
+var CarbsDay = function(opts) {
+  var defaults = {
+    interval: 90,
+    patterns: {
+      alternating: function(value) {
+        var big = true;
+        return function() {
+          var toReturn = big ? value : 10;
+          if (big) {
+            big = false;
+          }
+          else {
+            big = true;
+          }
+          return toReturn;
+        };
+      }
+    },
+    spread: {
+      min: 5,
+      max: 150,
+      jump: 25
+    }
+  };
+
+  this.opts = opts || {};
+
+  this.opts = _.defaults(this.opts, defaults);
+
+  this.maxDatapoints = (this.HOURS_PER_DAY * this.MINUTES_PER_HOUR)/this.opts.interval;
+
+  this.generateFull = function(pattern, opts) {
+    opts = opts || {};
+    var datetime = opts.start || this.START, count = 0, events = [];
+
+    while (count < this.maxDatapoints) {
+      events.push(new types.Carbs(datetime.utc().format().slice(0, -6), pattern()));
+      datetime = this.addInterval(datetime, {'minutes': this.opts.interval});
+      count += 1;
+    }
+
+    for (var i = 0; i < events.length; ++i) {
+      events[i] = events[i].asObject();
+    }
+    return events;
+  };
+};
+
+CarbsDay.prototype = day;
+
+var BolusDay = function(opts) {
+  var defaults = {
+    interval: 60,
+    patterns: {
+      allFeatureSets: function() {
+        var values = [2,4.5,6,7.5,9.25,10], i = 0;
+        var featureSets = new types.Bolus().getAllFeatureSetNames();
+        console.log(featureSets);
+        return function() {
+          // reset i
+          if (i === values.length) {
+            i = 0;
+          }
+          return {
+            value: values[i],
+            featureSet: featureSets[i++]
+          };
+        };
+      }
+    },
+    spread: {
+      min: 5,
+      max: 20,
+      jump: 0.8
+    }
+  };
+
+  this.opts = opts || {};
+
+  this.opts = _.defaults(this.opts, defaults);
+
+  this.maxDatapoints = (this.HOURS_PER_DAY * this.MINUTES_PER_HOUR)/this.opts.interval;
+
+  this.generateFull = function(pattern, opts) {
+    opts = opts || {};
+    var datetime = opts.start || this.START, count = 0, events = [];
+
+    while (count < this.maxDatapoints) {
+      events.push(new types.Bolus(datetime.utc().format().slice(0, -6), pattern()));
+      datetime = this.addInterval(datetime, {'minutes': this.opts.interval});
+      count += 1;
+    }
+
+    for (var i = 0; i < events.length; ++i) {
+      events[i] = events[i].asObject();
+    }
+    return events;
+  };
+};
+
+BolusDay.prototype = day;
+
+module.exports = (function() {
+  return {
+    CBGDay: CBGDay,
+    SMBGDay: SMBGDay,
+    CarbsDay: CarbsDay,
+    BolusDay: BolusDay
+  };
+}());
---
+++ dev/testpage/types.js
@@ -0,0 +1,125 @@
+/* 
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ * 
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
+var _ = require('lodash');
+
+var guid = require('./guid');
+
+var MS_IN_24 = 86400000;
+
+var common = {
+  deviceId: 'Demo Data - 123',
+  source: 'demo',
+  asObject: function() {
+    var clone = {}, key;
+    for (key in this) {
+      if (typeof this[key] !== 'function') {
+        clone[key] = this[key];
+      }
+    }
+    return clone;
+  },
+  makeId: function() { return guid(); }
+};
+
+var CBG = function(deviceTime, value) {
+  this.deviceTime = deviceTime;
+  this.id = this.makeId();
+  this.type = 'cbg';
+  this.value = value;
+};
+
+CBG.prototype = common;
+
+var SMBG = function(deviceTime, value) {
+  this.deviceTime = deviceTime;
+  this.id = this.makeId();
+  this.type = 'smbg';
+  this.value = value;
+};
+
+SMBG.prototype = common;
+
+var Carbs = function(deviceTime, value) {
+  this.deviceTime = deviceTime;
+  this.id = this.makeId();
+  this.type = 'carbs';
+  this.value = value;
+};
+
+Carbs.prototype = common;
+
+var Bolus = function(deviceTime, features) {
+  features = features || {'value': null};
+  var featureSets = {
+    'normal': {
+    },
+    'extendedHalf': {
+      'extended': true,
+      'extendedDelivery': 0.5 * features.value,
+      'initialDelivery': 0.5 * features.value,
+      'duration': MS_IN_24/12
+    },
+    'extendedQuarterUnderride': {
+      'extended': true,
+      'extendedDelivery': 0.75 * features.value,
+      'initialDelivery': 0.25 * features.value,
+      'duration': MS_IN_24/18,
+      'recommended': features.value + 2
+    },
+    'square': {
+      'extended': true,
+      'extendedDelivery': features.value,
+      'initialDelivery': 0.0,
+      'duration': MS_IN_24/24
+    },
+    'override': {
+      'recommended': features.value - 1,
+    },
+    'underride': {
+      'recommended': features.value + 1.5
+    }
+  };
+  this.getAllFeatureSetNames = function() {
+    var names = [];
+    for (var key in featureSets) {
+      if (featureSets.hasOwnProperty(key)) {
+        names.push(key);
+      }
+    }
+    return names;
+  };
+  // only fill out attributes if arguments
+  if (arguments.length) {
+    this.deviceTime = deviceTime;
+    this.id = this.makeId();
+    this.type = 'bolus';
+    this.value = features.value;
+    _.defaults(this, featureSets[features.featureSet]);
+  }
+};
+
+Bolus.prototype = common;
+
+module.exports = (function() {
+  return {
+    CBG: CBG,
+    SMBG: SMBG,
+    Carbs: Carbs,
+    Bolus: Bolus
+  };
+}());
