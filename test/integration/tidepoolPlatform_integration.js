8d3fa98803aada3b1a6cff5e5d6fb3689ade299f
Jamie <jamie@tidepool.org>
2014-06-09 16:55:49 +1200

    rationalise existing test so they read better

--- test/integration/tidepoolPlatform_integration.js
+++ test/integration/tidepoolPlatform_integration.js
@@ -30,20 +30,34 @@ describe('platform client', function () {
    */
   this.timeout(10000);
 
-  var mrT1Client = null;
-  var careTeamClient = null;
+  var pwdClient = null;
+  var memberClient = null;
 
-  var mrT1 = {
+  /*
+   * Represents a 'Person With Data' as used in the tests
+   *
+   * This will typically be a diabetic :)
+   */
+  var a_PWD = {
     id: null,
     token: null,
-    username: 'mrT1@user.com',
-    password: 'mrT1',
-    emails: ['mrT1@user.com'],
+    username: 'a_PWD@user.com',
+    password: 'a_PWD',
+    emails: ['a_PWD@user.com'],
     firstName: 'Jamie',
     lastName: 'T1'
   };
 
-  var careTeamMember = {
+  /*
+   * Represents a member of a team that has no data themselves as such but has explicitly
+   * been given access to the team of someone that has diabetes and is a `pwd`
+   *
+   * Use cases include:
+   *
+   * - parent of someone with diabeties
+   * - dr or another health professional
+   */
+  var a_Member = {
     id: null,
     token: null,
     username: 'team@member.com',
@@ -95,51 +109,22 @@ describe('platform client', function () {
 
     async.parallel(
       [
-        createClient.bind(null, mrT1,noLoginOpts,myLocalStore),
-        createClient.bind(null, careTeamMember,noLoginOpts,myLocalStore)
+        createClient.bind(null, a_PWD,noLoginOpts,myLocalStore),
+        createClient.bind(null, a_Member,noLoginOpts,myLocalStore)
       ],
       function(err, clients) {
         if (err != null) {
           return done(err);
         }
-        mrT1Client = clients[0];
-        careTeamClient = clients[1];
+        pwdClient = clients[0];
+        memberClient = clients[1];
         done();
       }
     );
   });
 
-  describe('allows platform user to stay logged in', function () {
-
-    it('but will NOT stay logged in if the remember option is false', function (done) {
-
-      createClient(mrT1, {remember:false}, require('./mockedLocalStorage')(),function(error,loggedIn){
-
-        expect(error).to.not.exist;
-
-        loggedIn.initialize(function(){
-          expect(loggedIn.isLoggedIn()).to.be.false;
-          done();
-        });
-
-      });
-    });
-
-    it('and will NOT stay logged in if the remember option not set', function (done) {
-
-      createClient(mrT1, {} , require('./mockedLocalStorage')(), function(error,loggedIn){
-
-        expect(error).to.not.exist;
-
-        loggedIn.initialize(function(){
-          expect(loggedIn.isLoggedIn()).to.be.false;
-          done();
-        });
-
-      });
-    });
-
-    it('so refesh means you stay logged in if the remember is set', function (done) {
+  describe('on initialization', function () {
+    it('when the remeber flag is true the user stays logged in', function (done) {
 
       var store = require('./mockedLocalStorage')();
 
@@ -164,26 +149,48 @@ describe('platform client', function () {
 
       });
     });
+    it('when the remember flag is false the user does NOT stay logged in', function (done) {
 
+      createClient(a_PWD, {remember:false}, require('./mockedLocalStorage')(),function(error,loggedIn){
+
+        expect(error).to.not.exist;
+
+        loggedIn.initialize(function(){
+          expect(loggedIn.isLoggedIn()).to.be.false;
+          done();
+        });
+
+      });
+    });
+    it('when the remember flag is not set the user does NOT stay logged in', function (done) {
+
+      createClient(a_PWD, {} , require('./mockedLocalStorage')(), function(error,loggedIn){
+
+        expect(error).to.not.exist;
+
+        loggedIn.initialize(function(){
+          expect(loggedIn.isLoggedIn()).to.be.false;
+          done();
+        });
+
+      });
+    });
   });
 
-  describe('allows applictaions', function () {
-
+  describe('allows applications too', function () {
     var defaulted = null;
+    it('track metrics to tidepool', function (done) {
 
-    it('to track metrics', function (done) {
-
-      createClient(mrT1, defaulted, defaulted,function(error,loggedInApp){
+      createClient(a_PWD, defaulted, defaulted,function(error,loggedInApp){
 
         expect(error).to.not.exist;
         loggedInApp.trackMetric('Platform Client Metrics Test',defaulted,done);
 
       });
     });
+    it('log errors to tidepool', function (done) {
 
-    it('to log errors', function (done) {
-
-      createClient(mrT1, defaulted , defaulted, function(error,loggedInApp){
+      createClient(a_PWD, defaulted , defaulted, function(error,loggedInApp){
 
         expect(error).to.not.exist;
         var appErrorToLog = new Error('Error From Platform Client Tests');
@@ -191,102 +198,129 @@ describe('platform client', function () {
 
       });
     });
-
   });
 
-  describe('sets up the profiles', function () {
-
-    it('for mrT1', function (done) {
-      mrT1Client.addOrUpdateProfile(mrT1, done);
+  describe('handles user profiles', function () {
+    it('so we can add or update the logged in users profile', function (done) {
+      //add or update for both our users
+      async.parallel([
+        pwdClient.addOrUpdateProfile.bind(null,a_PWD),
+        memberClient.addOrUpdateProfile.bind(null,a_Member)
+      ],
+        function(err, profiles) {
+          if (err != null) {
+            return done(err);
+          }
+          done();
+        }
+      );
     });
-
-    it('for mrT1 we can get the profile', function (done) {
-      mrT1Client.findProfile(mrT1.id, function (error, profile) {
+    it('get another users public profile', function (done) {
+      //logged in as a_PWD you can get the profile for a_Member
+      pwdClient.findProfile(a_Member.id, function (error, profile) {
         expect(error).to.not.exist;
 
         expect(profile).to.exist;
-        expect(profile.firstName).to.equal(mrT1.firstName);
-        expect(profile.lastName).to.equal(mrT1.lastName);
+        expect(profile.firstName).to.equal(a_Member.firstName);
+        expect(profile.lastName).to.equal(a_Member.lastName);
         expect(profile).to.not.have.property('password');
         expect(profile).to.not.have.property('username');
 
         done();
       });
     });
-
-    it('for careTeamMember', function (done) {
-      careTeamClient.addOrUpdateProfile(careTeamMember, done);
-    });
-
-    it('for careTeamMember we can get the profile', function (done) {
-      careTeamClient.findProfile(careTeamMember.id, function (error, profile) {
+    it('get the logged in users profile', function (done) {
+      //logged in as a_Member you can get your profile
+      memberClient.findProfile(a_Member.id, function (error, profile) {
         expect(error).to.not.exist;
 
         expect(profile).to.be.exist;
-        expect(profile.firstName).to.equal(careTeamMember.firstName);
-        expect(profile.lastName).to.equal(careTeamMember.lastName);
+        expect(profile.firstName).to.equal(a_Member.firstName);
+        expect(profile.lastName).to.equal(a_Member.lastName);
         expect(profile).to.not.have.property('password');
         expect(profile).to.not.have.property('username');
         done();
       });
     });
-
   });
 
-  describe('get the team for mrT1', function () {
-
-    it('returns the team group for mrT1', function (done) {
-      mrT1Client.getTeamMembers(mrT1.id, function (error, team) {
-        expect(error).to.not.exist;
-
-        var expectation = {};
-        expectation[mrT1.id] = {root: {}};
-        expect(team).to.deep.equal(expectation);
-        done();
-      });
-    });
-
-  });
-
-  describe('messaging for mrT1', function () {
+  describe('handles messages', function () {
     var noteToAddId;
     var noteToAdd;
     var commentOnNote;
 
-    it('allows mrT1 to add a note', function (done) {
+    /*
+     * Give a_Memeber to a_PWD for these tests
+     */
+    before(function (done) {
+      pwdClient.setAccessPermissions(a_Member.id, {view: {}}, function(err, permissions) {
+        expect(permissions).to.deep.equal({view: {}});
+        done(err);
+      });
+    });
+
+    /*
+     * Revoke a_Memeber permissons to a_PWD after these tests
+     */
+    after(function (done) {
+      pwdClient.setAccessPermissions(a_Member.id, null, function(err, permissions) {
+        expect(permissions).to.be.empty;
+        done(err);
+      });
+    });
+
+    it('so a_PWD can add a note that goes to the team also', function (done) {
 
       noteToAdd = {
-        userid: mrT1.id,
-        groupid: mrT1.id,
+        userid: a_PWD.id,
+        groupid: a_PWD.id,
         timestamp: new Date().toISOString(),
         messagetext: 'In three words I can sum up everything I have learned about life: it goes on.'
       };
 
       //add note
-      mrT1Client.startMessageThread(noteToAdd, function (error, data) {
+      pwdClient.startMessageThread(noteToAdd, function (error, added) {
         expect(error).to.not.exist;
-        expect(data).to.exist;
-        noteToAddId = data;
+        expect(added).to.exist;
+        noteToAddId = added;
         done();
       });
     });
-
-    it('and add a comment on the note', function (done) {
+    it('and a_Member can comment on the note', function (done) {
 
       //comment on the note
       commentOnNote = {
-        userid: mrT1.id,
-        groupid: mrT1.id,
+        userid: a_Member.id,
+        groupid: a_PWD.id,
         parentmessage : noteToAddId,
         timestamp: new Date().toISOString(),
         messagetext: 'Good point bro!'
       };
 
-      mrT1Client.replyToMessageThread(commentOnNote, done);
+      memberClient.replyToMessageThread(commentOnNote, function (error, replied) {
+        expect(error).to.not.exist;
+        expect(replied).to.exist;
+        done();
+      });
     });
+    it('and a_Member can also start a thread for a_PWD', function (done) {
 
-    it('and then get the whole thread', function (done) {
-      mrT1Client.getMessageThread(noteToAddId, function (error, data) {
+      //comment on the note
+      var noteFromAMember = {
+        userid: a_Member.id,
+        groupid: a_PWD.id,
+        timestamp: new Date().toISOString(),
+        messagetext: 'Just a note to say you should consider ....'
+      };
+
+      memberClient.startMessageThread(noteFromAMember, function(error,added){
+        expect(error).to.not.exist;
+        expect(added).to.exist;
+        done();
+      });
+    });
+    it('a_PWD can get a message thread', function (done) {
+      pwdClient.getMessageThread(noteToAddId, function (error, data) {
         expect(error).to.not.exist;
         expect(data).to.exist;
         expect(data.length).to.equal(2);
@@ -301,9 +335,7 @@ describe('platform client', function () {
         done();
       });
     });
-
-    it('allows mrT1 to get all messages for his team for the last two weeks', function (done) {
-
+    it('a_Member can get message in a date range for a_PWD', function (done) {
 
       var twoWeeksAgo = new Date();
       twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
@@ -313,7 +345,7 @@ describe('platform client', function () {
         end : new Date()
       };
 
-      mrT1Client.getAllMessagesForUser(mrT1.id, dates, function (error, data) {
+      memberClient.getAllMessagesForUser(a_PWD.id, dates, function (error, data) {
 
         expect(error).to.not.exist;
         expect(data).to.exist;
@@ -321,10 +353,9 @@ describe('platform client', function () {
         done();
       });
     });
+    it('a_PWD can get all notes (i.e. no comments)', function (done) {
 
-    it('allows mrT1 to get just the parent messages for his team ', function (done) {
-
-      mrT1Client.getNotesForUser(mrT1.id, null, function (error, data) {
+      pwdClient.getNotesForUser(a_PWD.id, null, function (error, data) {
 
         expect(error).to.not.exist;
         expect(data).to.exist;
@@ -336,73 +367,26 @@ describe('platform client', function () {
         done();
       });
     });
-
   });
 
-  describe('mrT1 groups', function () {
-
-    it('has a team', function (done) {
-      mrT1Client.getTeamMembers(mrT1.id, function (error, team) {
-        var expectation = {};
-        expectation[mrT1.id] = {root: {}};
-        expect(team).to.deep.equal(expectation);
-
-        done(error);
-      });
-    });
-
-    it('has patients', function (done) {
-      mrT1Client.getViewableUsers(mrT1.id, function (error, viewableUsers) {
-        var expectation = {};
-        expectation[mrT1.id] = {root: {}};
-        expect(viewableUsers).to.deep.equal(expectation);
-
-        done(error);
-      });
-    });
-  });
-
-  describe('careTeamMember groups', function () {
-
-    it('has a team', function (done) {
-      careTeamClient.getTeamMembers(careTeamMember.id, function (error, team) {
-        var expectation = {};
-        expectation[careTeamMember.id] = {root: {}};
-        expect(team).to.deep.equal(expectation);
-
-        done(error);
-      });
-    });
-
-    it('has patients', function (done) {
-      careTeamClient.getViewableUsers(careTeamMember.id, function (error, viewableUsers) {
-        var expectation = {};
-        expectation[careTeamMember.id] = {root: {}};
-        expect(viewableUsers).to.deep.equal(expectation);
-
-        done(error);
-      });
-    });
-  });
-
-  describe('careTeamMember', function () {
+  describe('handles team persmissons', function () {
 
     var careTeamViewable;
-    var mrT1sTeam;
-    var notesForThePatientMrT1;
+    var pwdsTeam;
+    var notesForThePatientpwd;
 
-    it('can set permissions for careTeam on mrT1', function(done){
-      mrT1Client.setAccessPermissions(careTeamMember.id, {view: {}}, function(err, permissions) {
+    it('so we can give a_Member permisson to view a_PWD', function(done){
+      pwdClient.setAccessPermissions(a_Member.id, {view: {}}, function(err, permissions) {
         expect(permissions).to.deep.equal({view: {}});
         done(err);
       });
     });
 
-    it('can get the patients and mrT1 is included', function (done) {
-      careTeamClient.getViewableUsers(careTeamMember.id, function (error, viewableUsers) {
+    it('so a_Member has a_PWD as a viewable user', function (done) {
+      memberClient.getViewableUsers(a_Member.id, function (error, viewableUsers) {
         var expectation = {};
-        expectation[careTeamMember.id] = {root: {}};
-        expectation[mrT1.id] = {view: {}};
+        expectation[a_Member.id] = {root: {}};
+        expectation[a_PWD.id] = {view: {}};
         expect(viewableUsers).to.deep.equal(expectation);
 
         careTeamViewable = viewableUsers;
@@ -410,45 +394,45 @@ describe('platform client', function () {
       });
     });
 
-    it('mrT1 gets his team and careTeamMember is included', function (done) {
-      mrT1Client.getTeamMembers(mrT1.id, function (error, team) {
+    it('and a_PWD has a_Member included in the team ', function (done) {
+      pwdClient.getTeamMembers(a_PWD.id, function (error, team) {
         var expectation = {};
-        expectation[mrT1.id] = {root: {}};
-        expectation[careTeamMember.id] = {view: {}};
+        expectation[a_PWD.id] = {root: {}};
+        expectation[a_Member.id] = {view: {}};
 
         expect(team).to.deep.equal(expectation);
-        mrT1sTeam = team;
+        pwdsTeam = team;
         done(error);
       });
     });
 
-    it('cannot get the team for mrT1', function (done) {
-      careTeamClient.getTeamMembers(mrT1.id, function (error, patientsTeam) {
+    it('but a_Member cannot see the other team members for a_PWD', function (done) {
+      memberClient.getTeamMembers(a_PWD.id, function (error, patientsTeam) {
         expect(error).to.deep.equal({ status: 401, body: 'These are not the droids you are looking for.' });
         done();
       });
     });
 
-    it('can get the notes for the team of mrT1', function (done) {
-      mrT1Client.getNotesForUser(mrT1.id, null, function (error, patientsNotes) {
+    it('a_Member can see the messages for a_PWD', function (done) {
+      pwdClient.getNotesForUser(a_PWD.id, null, function (error, patientsNotes) {
         expect(patientsNotes).to.exist;
         expect(patientsNotes).to.have.length.above(0);
-        notesForThePatientMrT1 = patientsNotes;
+        notesForThePatientpwd = patientsNotes;
         done();
       });
     });
 
-    it('can see the notes for patient mrT1 are the same as he sees', function (done) {
-      careTeamClient.getNotesForUser(mrT1.id, null, function (error, mrT1TeamNotes) {
-        expect(mrT1TeamNotes).to.exist;
-        expect(mrT1TeamNotes).to.have.length.above(0);
-        expect(mrT1TeamNotes).that.deep.equals(notesForThePatientMrT1);
+    it('a_Member sees the messages as a_PWD sees them', function (done) {
+      memberClient.getNotesForUser(a_PWD.id, null, function (error, pwdTeamNotes) {
+        expect(pwdTeamNotes).to.exist;
+        expect(pwdTeamNotes).to.have.length.above(0);
+        expect(pwdTeamNotes).that.deep.equals(notesForThePatientpwd);
         done();
       });
     });
 
-    it('can remove careTeam\'s permissions from mrT1', function(done) {
-      mrT1Client.setAccessPermissions(careTeamMember.id, null, function(err, perms){
+    it('a_PWD can remove the permissions for a_Member', function(done) {
+      pwdClient.setAccessPermissions(a_Member.id, null, function(err, perms){
         expect(perms).to.be.empty;
         done(err);
       });
