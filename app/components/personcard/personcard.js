96a5f7484e321af67d458ac783f1d76ae61872c8
Nicolas Hery <hery.nicolas@gmail.com>
2014-07-11 12:21:07 -0400

    Introduce PersonCard component

---
+++ app/components/personcard/personcard.js
@@ -0,0 +1,64 @@
+/** @jsx React.DOM */
+/**
+ * Copyright (c) 2014, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ */
+
+var React = window.React;
+var _ = window._;
+
+var PersonCard = React.createClass({
+  propTypes: {
+    href: React.PropTypes.string,
+    onClick: React.PropTypes.func
+  },
+
+  render: function() {
+    var classes = React.addons.classSet({
+      'personcard': true,
+      'personcard-clickable': this.isClickable(),
+      'personcard-empty': this.isEmpty()
+    });
+
+    if (!this.isClickable()) {
+      /* jshint ignore:start */
+      return (
+        <div className={classes}>
+          {this.props.children}
+        </div>
+      );
+      /* jshint ignore:end */
+    }
+
+    /* jshint ignore:start */
+    return (
+      <a
+        className={classes}
+        href={this.props.href}
+        onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+    /* jshint ignore:end */
+  },
+
+  isClickable: function() {
+    return this.props.href || this.props.onClick;
+  },
+
+  isEmpty: function() {
+    return _.isEmpty(this.props.children);
+  }
+});
+
+module.exports = PersonCard;
