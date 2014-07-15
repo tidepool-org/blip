46519209f6f5a5e1f79cea7c8418bbe2c12851b1
Nicolas Hery <hery.nicolas@gmail.com>
2014-07-15 12:48:35 -0400

    Publish to hakken when env vars are set

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http');
+var https = require('https');
 var connect = require('connect');
+
+var config = require('./env.js');
 
 var buildDir = 'dist';
 
@@ -8,7 +11,39 @@ var app = connect();
 var staticDir = __dirname + '/' + buildDir;
 app.use(connect.static(staticDir));
 
-var server = http.createServer(app).listen(process.env.PORT || 3000, function() {
-  console.log('Connect server started on port', server.address().port);
-  console.log('Serving static directory "' + staticDir + '/"'); 
-});
+// If no ports specified, just start on default HTTP port
+if (!(config.httpPort || config.httpsPort)) {
+  config.httpPort = 3000;
+}
+
+if (config.httpPort) {
+  http.createServer(app).listen(config.httpPort, function() {
+    console.log('Connect server started on port', config.httpPort);
+    console.log('Serving static directory "' + staticDir + '/"');
+  });
+}
+
+if (config.httpsPort) {
+  https.createServer(config.httpsConfig, app).listen(config.httpsPort, function() {
+    console.log('Connect server started on HTTPS port', config.httpsPort);
+    console.log('Serving static directory "' + staticDir + '/"');
+  });
+}
+
+if (config.discoveryHost && config.publishHost) {
+  var hakken = require('hakken')({host: config.discoveryHost}).client();
+  hakken.start();
+
+  var serviceDescriptor = {service: config.serviceName};
+  if (config.httpsPort) {
+    serviceDescriptor['host'] = config.publishHost + ':' + config.httpsPort;
+    serviceDescriptor['protocol'] = 'https';
+  }
+  else if (config.httpPort) {
+    serviceDescriptor['host'] = config.publishHost + ':' + config.httpPort;
+    serviceDescriptor['protocol'] = 'http';
+  }
+
+  console.log('Publishing to service discovery');
+  hakken.publish(serviceDescriptor);
+}
