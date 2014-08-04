73128c0d3cea4b9367e1ae6cc127c6676d6d2f14
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-08-04 15:44:21 -0700

    beautifully simple new basal plot module, all functionality except tooltips :D

--- js/plot/basal.js
+++ js/plot/basal.js
@@ -24,34 +24,205 @@ var log = require('../lib/').bows('Basal');
 module.exports = function(pool, opts) {
   opts = opts || {};
 
-  var defaults = {};
+  var defaults = {
+    opacity: 0.3,
+    opacityDelta: 0.1,
+    pathStroke: 1.5
+  };
 
   opts = _.defaults(opts, defaults);
 
-  // refers to the relevant top-level SVG element for the view you're in
   var mainGroup = pool.parent();
+
+  function getUndelivereds(data) {
+    var undelivereds = [];
+    for (var i = 0; i < data.length; ++i) {
+      var d = data[i];
+      if (d.suppressed) {
+        undelivereds = undelivereds
+          // TODO: eventually we'll want a path for each category of suppresseds
+          // where 'scheduled' is just one such category
+          .concat(_.filter(d.suppressed, function(s) { return s.deliveryType === 'scheduled'; }));
+      }
+    }
+    // there can be duplicate suppressed segments, not quite sure why this happens
+    // TODO: possibly move this filtering to segmentutil
+    return _.reject(undelivereds, function(d, i, segments) { return _.isEqual(d, segments[i - 1]); });
+  }
 
   function basal(selection) {
     opts.xScale = pool.xScale().copy();
+
     selection.each(function(currentData) {
 
-      // TIPS:
-      // do not use d3.select() or d3.selectAll()!
-      // always chain your selections from `selection` instead
-      // e.g., selection.select(), selection.selectAll()
+      basal.addAnnotations(_.filter(currentData, function(d) { return d.annotations; }));
 
-      // make sure you capture the exit selection and remove stale DOM elements
-      // e.g., mySelection.exit().remove()
+      var basalSegments = d3.select(this)
+        .selectAll('.d3-basal-group')
+        .data(currentData, function(d) {
+          return d.id;
+        });
 
+      var basalSegmentGroups = basalSegments.enter()
+        .append('g')
+        .attr({
+          'class': 'd3-basal-group',
+          id: function(d) {
+            return 'basal_group_' + d.id;
+          },
+          'clip-path': 'url(#mainClipPath)'
+        });
+
+      var nonZero = basalSegmentGroups.filter(function(d) {
+        return d.value !== 0;
+      });
+
+      basal.addRectToPool(nonZero);
+
+      // add invisible rects as hover targets for all basals
+      basal.addRectToPool(basalSegmentGroups, true);
+
+      var basalPathsGroup = selection.selectAll('.d3-basal-path-group').data(['d3-basal-path-group']);
+      basalPathsGroup.enter().append('g').attr('class', 'd3-basal-path-group');
+      var paths = basalPathsGroup.selectAll('.d3-basal.d3-path-basal')
+        .data(['d3-basal d3-path-basal', 'd3-basal d3-path-basal d3-path-basal-undelivered']);
+      paths.enter().append('path').attr({
+        'class': function(d) { return d; },
+        'clip-path': 'url(#mainClipPath)'
+      });
+
+      // d3.selects are OK here because `paths` is a chained selection
+      var actualpath = d3.select(paths[0][0]);
+      var undeliveredPath = d3.select(paths[0][1]);
+
+      basal.updatePath(actualpath, currentData);
+
+      basal.updatePath(undeliveredPath, getUndelivereds(currentData));
+
+      basalSegments.exit().remove();
+
+      // tooltips
+      basalSegmentGroups.on('mouseover', function() {
+        basal.addTooltip(d3.select(this).datum());
+        d3.select(this).selectAll('.d3-basal.d3-rect-basal')
+          .attr('opacity', opts.opacity + opts.opacityDelta);
+      });
+      basalSegmentGroups.on('mouseout', function() {
+        var id = d3.select(this).attr('id').replace('basal_group_', 'tooltip_');
+        mainGroup.select('#' + id).remove();
+        d3.select(this).selectAll('.d3-basal.d3-rect-basal').attr('opacity', opts.opacity);
+      });
     });
   }
 
-  basal.addTooltip = function() {
+  basal.addRectToPool = function(selection, invisible) {
+    opts.xScale = pool.xScale().copy();
 
+    var rectClass = invisible ? 'd3-basal d3-basal-invisible' : 'd3-basal d3-rect-basal';
+
+    var heightFn = invisible ? basal.invisibleRectHeight : basal.height;
+
+    var yPosFn = invisible ? basal.invisibleRectYPosition : basal.yPosition;
+
+    selection.append('rect')
+      .attr({
+        x: basal.xPosition,
+        y: yPosFn,
+        opacity: opts.opacity,
+        width: basal.width,
+        height: heightFn,
+        'class': rectClass
+      });
   };
 
-  basal.addAnnotations = function() {
+  basal.updatePath = function(selection, data) {
+    opts.xScale = pool.xScale().copy();
 
+    selection.attr({
+      d: basal.pathData(data)
+    });
+  };
+
+  basal.pathData = function(data) {
+    opts.xScale = pool.xScale().copy();
+
+    function stringCoords(datum) {
+      return basal.xPosition(data[i]) + ',' + basal.pathYPosition(data[i]) + ' ';
+    }
+    var d = '';
+    for (var i = 0; i < data.length; ++i) {
+      if (i === 0) {
+        // start with a moveto command
+        d += 'M' + stringCoords(data[i]);
+      }
+      else if (data[i].normalTime === data[i - 1].normalEnd) {
+        // if segment is contiguous with previous, draw a vertical line connecting their values
+        d += 'V' + basal.pathYPosition(data[i]) + ' ';
+      }
+      // TODO: maybe a robust check for a gap in time here instead of just !==?
+      else if (data[i].normalTime !== data[i - 1].normalEnd) {
+        // if segment is not contiguous with previous, skip to beginning of segment
+        d += 'M' + stringCoords(data[i]);
+      }
+      // always add a horizontal line corresponding to current segment
+      d += 'H' + basal.segmentEndXPosition(data[i]) + ' ';
+    }
+    return d;
+  };
+
+  basal.xPosition = function(d) {
+    return opts.xScale(Date.parse(d.normalTime));
+  };
+
+  basal.segmentEndXPosition = function(d) {
+    return opts.xScale(Date.parse(d.normalEnd));
+  };
+
+  basal.yPosition = function(d) {
+    return opts.yScale(d.value);
+  };
+
+  basal.pathYPosition = function(d) {
+    return opts.yScale(d.value) - opts.pathStroke/2;
+  };
+
+  basal.invisibleRectYPosition = function(d) {
+    return 0;
+  };
+
+  basal.width = function(d) {
+    return opts.xScale(Date.parse(d.normalEnd)) - opts.xScale(Date.parse(d.normalTime));
+  };
+
+  basal.height = function(d) {
+    return pool.height() - opts.yScale(d.value);
+  };
+
+  basal.invisibleRectHeight = function(d) {
+    return pool.height();
+  };
+
+  basal.addTooltip = function() {
+  };
+
+  basal.addAnnotations = function(data, selection) {
+    for (var i = 0; i < data.length; ++i) {
+      var d = data[i];
+      var annotationOpts = {
+        x: basal.xPosition(d),
+        y: opts.yScale(0),
+        xMultiplier: 2,
+        yMultiplier: 1,
+        orientation: {
+          up: true
+        },
+        d: d
+      };
+      if (mainGroup.select('#annotation_for_' + d.id)[0][0] == null) {
+        mainGroup.select('#tidelineAnnotations_basal-rate-segment')
+          .call(pool.annotations(), annotationOpts);
+      }
+    }
   };
 
   return basal;
