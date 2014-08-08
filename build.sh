ebec9c07620726593fba76d06799e0f800114ff8
Nicolas Hery <hery.nicolas@gmail.com>
2014-08-08 17:47:53 -0400

    Add timing to npm install

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #! /bin/bash -eu
 
 rm -rf node_modules
+T="$(date +%s)"
 npm install --production
+T="$(($(date +%s)-T))"
+echo "npm install completed in ${T} seconds"
 npm dedupe
 npm run build-app
