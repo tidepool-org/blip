b2d7e63ba37646f1aa04e204f5d0cf1456dfd7f9
Nicolas Hery <hery.nicolas@gmail.com>
2014-09-04 17:51:01 -0400

    Use mock data as mini-database

--- mock/api.js
+++ mock/api.js
@@ -15,20 +15,45 @@
 
 var _ = require('lodash');
 
+var personUtils = require('../app/core/personutils');
+
+var userIdLocalKey = 'mockUserId';
+var tokenLocalKey = 'mockAuthToken';
+
+var userIdSize = 10;
+var tokenIdSize = 16;
+
+// http://pragmatic-coding.blogspot.ca/2012/01/javascript-pseudo-random-id-generator.html
+function generateRandomId(length) {
+  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", returnValue = "", x, i;
+
+  for (x = 0; x < length; x += 1) {
+    i = Math.floor(Math.random() * 62);
+    returnValue += chars.charAt(i);
+  }
+
+  return returnValue;
+}
+
+function generateUserId() {
+  return generateRandomId(userIdSize);
+}
+
+function generateTokenId() {
+  return generateRandomId(tokenIdSize);
+}
+
 var createPatch = function(options) {
   var patch = function(api) {
     var getParam = options.getParam || {};
     var data = options.data || {};
-
-    var mockToken = '123';
-    var mockUsername = 'demo';
-    var mockPassword = 'demo';
 
     var getDelayFor = function(name) {
       return (getParam('delay') || getParam(name + '.delay') || 0);
     };
 
     api.token = null;
+    api.userId = null;
 
     api.init = function(callback) {
       loadSession(function() {
@@ -39,21 +64,63 @@ var createPatch = function(options) {
 
     // ----- User -----
 
+    function isMatchedTokenPair(userId, token, pair) {
+      return pair.userid === userId && pair.token === token;
+    }
+
+    function isValidToken(userId, token) {
+      var result = _.find(data.tokens,
+        isMatchedTokenPair.bind(null, userId, token));
+      return Boolean(result);
+    }
+
+    function addToken(userId, token) {
+      data.tokens.push({
+        userid: userId,
+        token: token
+      });
+    }
+
+    function destroyToken(userId, token) {
+      data.tokens = _.reject(data.tokens,
+        isMatchedTokenPair.bind(null, userId, token));
+    }
+
+    function getUserWithCredentials(username, password) {
+      return _.find(data.users, function(user) {
+        return user.username === username && user.password === password;
+      });
+    }
+
+    function usernameAlreadyExists(username) {
+      var result = _.find(data.users, {username: username});
+      return Boolean(result);
+    }
+
+    function addUser(user) {
+      data.users[user.userid] = user;
+    }
+
     function loadSession(callback) {
+      var userId;
       var token;
       var localStorage = window.localStorage;
 
       if (getParam('auth.skip')) {
         api.log('[mock] Skipping auth');
-        saveSession(mockToken);
+        userId = data.defaultUserId;
+        token = generateTokenId();
+        addToken(userId, token);
+        saveSession(userId, token);
         setTimeout(callback, getDelayFor('auth.loadsession'));
         return;
       }
 
       if (localStorage && localStorage.getItem) {
-        token = localStorage.getItem('mockAuthToken');
-        if (token) {
-          saveSession(token);
+        userId = localStorage.getItem(userIdLocalKey);
+        token = localStorage.getItem(tokenLocalKey);
+        if (userId && token && isValidToken(userId, token)) {
+          saveSession(userId, token);
         }
         setTimeout(callback, getDelayFor('auth.loadsession'));
       }
@@ -63,29 +130,33 @@ var createPatch = function(options) {
       api.log('[mock] Session loaded');
     }
 
-    function saveSession(token, options) {
+    function saveSession(userId, token, options) {
       options = options || {};
 
+      api.userId = userId;
       api.token = token;
       if (options.remember) {
         var localStorage = window.localStorage;
         if (localStorage && localStorage.setItem) {
-          localStorage.setItem('mockAuthToken', token);
+          localStorage.setItem(userIdLocalKey, userId);
+          localStorage.setItem(tokenLocalKey, token);
         }
       }
     }
 
     function destroySession() {
+      api.userId = null;
       api.token = null;
       var localStorage = window.localStorage;
       if (localStorage && localStorage.removeItem) {
-        localStorage.removeItem('mockAuthToken');
+        localStorage.removeItem(userIdLocalKey);
+        localStorage.removeItem(tokenLocalKey);
       }
     }
     api.user.destroySession = destroySession;
 
     api.user.isAuthenticated = function() {
-      return Boolean(api.token);
+      return Boolean(api.userId && api.token);
     };
 
     api.user.login = function(user, options, callback) {
@@ -101,11 +172,15 @@ var createPatch = function(options) {
 
       setTimeout(function() {
         var err;
-        if (username !== mockUsername || password !== mockPassword) {
+        var user = getUserWithCredentials(username, password);
+        if (!user) {
           err = {status: 401, response: 'Wrong username or password.'};
         }
         if (!err) {
-          saveSession(mockToken, options);
+          var userId = user.userid;
+          var token = generateTokenId();
+          addToken(userId, token);
+          saveSession(userId, token, options);
           api.log('[mock] Login success');
         }
         else {
@@ -122,6 +197,7 @@ var createPatch = function(options) {
           err = {status: 500, response: 'Logout failed, please try again.'};
         }
         if (!err) {
+          destroyToken(api.userId, api.token);
           destroySession();
           api.log('[mock] Logout success');
         }
@@ -133,25 +209,30 @@ var createPatch = function(options) {
     };
 
     api.user.signup = function(user, callback) {
-      user = _.clone(user);
-      user.userid = '1';
-      delete user.password;
+      user = _.cloneDeep(user);
 
       setTimeout(function() {
         var err;
-        if (user.username === mockUsername) {
+        if (usernameAlreadyExists(user.username)) {
           err = {
             status: 400,
             response: 'An account already exists for that username.'
           };
         }
         if (!err) {
-          saveSession(mockToken);
+          user.userid = generateUserId();
+          addUser(user);
+
+          var token = generateTokenId();
+          addToken(user.userid, token);
+          saveSession(user.userid, token);
           api.log('[mock] Signup success');
         }
         else {
           api.log('[mock] Signup failed');
         }
+
+        user = _.omit(user, 'password');
         callback(err, user);
       }, getDelayFor('api.user.signup'));
     };
@@ -159,7 +240,10 @@ var createPatch = function(options) {
     api.user.get = function(callback) {
       api.log('[mock] GET /user');
 
-      var user = _.cloneDeep(data.user);
+      var user = data.users[api.userId];
+      user = _.cloneDeep(user);
+      user = _.omit(user, 'password');
+
       if (getParam('api.user.get.nopatient')) {
         user.profile = _.omit(user.profile, 'patient');
       }
@@ -180,55 +264,122 @@ var createPatch = function(options) {
         if (getParam('api.user.put.error')) {
           err = {status: 500};
         }
-        user = _.assign({}, data.user, user);
-        delete user.password;
-        delete user.passwordConfirm;
+
+        var savedUser = data.users[api.userId];
+        user = _.assign({}, savedUser, user);
+        data.users[api.userId] = user;
+
+        user = _.omit(user, 'password');
         callback(err, user);
       }, getDelayFor('api.user.put'));
     };
 
     // ----- Patient -----
 
+    function publicPersonInfo(person) {
+      return _.omit(person, 'password', 'username', 'emails');
+    }
+
+    function updatePatient(patient) {
+      var patientId = patient.userid;
+      var patientInfo = personUtils.patientInfo(patient);
+
+      var profile = data.users[patientId].profile;
+      data.users[patientId].profile = _.assign(profile, {
+        patient: patientInfo
+      });
+    }
+
     api.patient.getAll = function(callback) {
       api.log('[mock] GET /patients');
-      var patients = [];
-
-      if (!getParam('api.patient.getall.empty')) {
-        patients = _.toArray(data.patients);
-        var userId = data.user.userid;
-        patients = _.filter(patients, function(patient) {
-          return patient.userid !== userId;
-        });
-      }
 
       setTimeout(function() {
+        if (getParam('api.patient.getall.empty')) {
+          return callback(null, []);
+        }
+
+        var userId = api.userId;
+        var groups = data.groups[userId];
+        if (_.isEmpty(groups)) {
+          return callback(null, []);
+        }
+
+        var patients = _.reduce(groups, function(result, permissions, groupId) {
+          if (groupId === userId) {
+            return result;
+          }
+
+          var person = data.users[groupId];
+          if (!personUtils.isPatient(person)) {
+            return result;
+          }
+
+          var patient = _.cloneDeep(person);
+          patient = publicPersonInfo(patient);
+          patient.permissions = permissions;
+          result.push(patient);
+          return result;
+        }, []);
+
         callback(null, patients);
       }, getDelayFor('api.patient.getall'));
     };
 
     api.patient.get = function(patientId, callback) {
       api.log('[mock] GET /patients/' + patientId);
-      var patient = data.patients[patientId];
-      var err;
-      if (!patient) {
-        err = {status: 404, response: 'Not found'};
-      }
-
-      if (getParam('api.patient.get.emptyteam')) {
-        patient.team = [];
-      }
 
       setTimeout(function() {
-        callback(err, patient);
+        var person = data.users[patientId];
+
+        var userId = api.userId;
+        var permissions;
+        if (patientId === userId) {
+          permissions = {root: {}};
+        }
+        else {
+          permissions = data.groups[userId] || {};
+          permissions = permissions[patientId];
+        }
+        var canViewPatient = !_.isEmpty(permissions);
+
+        if (!(person && personUtils.isPatient(person) && canViewPatient)) {
+          var err = {status: 404, response: 'Not found'};
+          return callback(err);
+        }
+
+        var patient = _.cloneDeep(person);
+        patient = publicPersonInfo(patient);
+        patient.permissions = permissions;
+
+        patient.team = [];
+        if (!getParam('api.patient.get.emptyteam')) {
+          patient.team = _.reduce(data.groups,
+          function(result, memberGroups, memberId) {
+            if (memberId === patientId) {
+              return result;
+            }
+
+            var permissions = memberGroups[patientId];
+            if (!permissions) {
+              return result;
+            }
+
+            var member = data.users[memberId];
+            member = publicPersonInfo(member);
+            member.permissions = permissions;
+            result.push(member);
+            return result;
+          }, []);
+        }
+
+        callback(null, patient);
       }, getDelayFor('api.patient.get'));
     };
 
     api.patient.post = function(patient, callback) {
       api.log('[mock] POST /patients');
-      var userId = data.user.userid;
-      patient = _.cloneDeep(patient);
-      patient.userid = userId;
-      data.patients[userId] = patient;
+      patient = _.assign({}, patient, {userid: api.userId});
+      updatePatient(patient);
       setTimeout(function() {
         callback(null, patient);
       }, getDelayFor('api.patient.post'));
@@ -239,12 +390,12 @@ var createPatch = function(options) {
       api.log('[mock] PUT /patients/' + patientId);
       var updatedPatient;
       var err;
-      if (!data.patients[patientId]) {
+      if (!data.users[patientId]) {
         err = {status: 404, response: 'Not found'};
       }
       else {
-        updatedPatient = _.assign({}, data.patients[patientId], patient);
-        data.patients[patientId] = updatedPatient;
+        updatePatient(patient);
+        updatedPatient = data.users[patientId];
       }
       setTimeout(function() {
         callback(err, updatedPatient);
