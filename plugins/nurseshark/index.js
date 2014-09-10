85a509fddfd2e5ef7977dc443e2a54ba0ce268dd
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-09-10 00:39:44 -0700

    first draft of nurseshark, minus suspends -> temp basals of zero

--- plugins/nurseshark/index.js
+++ plugins/nurseshark/index.js
@@ -1,13 +1,261 @@
+/*
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ * 
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
 var _ = require('lodash');
+var crossfilter = require('crossfilter');
+var util = require('util');
+
+var dt = require('./datetime');
+
+
+function translateBg(value) {
+  var GLUCOSE_MM = 18.01559;
+  return Math.round(GLUCOSE_MM * value);
+}
+
+function isBadStatus(d) {
+  if (d.annotations == null) {
+    return false;
+  }
+  else {
+    var numAnnotations = d.annotations.length;
+    for (var i = 0; i < numAnnotations; ++i) {
+      var code = d.annotations[i].code;
+      if (code === 'status/incomplete-tuple' || code === 'status/unknown-previous') {
+        return true;
+      }
+    }
+  }
+}
+
+function cloneDeep(d) {
+  var newObj = {}, keys = Object.keys(d);
+  var numKeys = keys.length;
+  for (var i = 0; i < numKeys; ++i) {
+    var key =  keys[i];
+    if (typeof d[key] === 'object') {
+      newObj[key] = _.cloneDeep(d[key]);
+    }
+    else {
+      newObj[key] = d[key];
+    }
+  }
+  return newObj;
+}
+
+var handlers = {
+  basal: function(d) {
+    return cloneDeep(d);
+  },
+  bolus: function(d, collections) {
+    d = cloneDeep(d);
+    if (d.joinKey != null) {
+      collections.bolusesToJoin[d.joinKey] = d;
+    }
+    if (d.duration != null) {
+      collections.extendedIntervals[d.time + '/' + dt.addDuration(d.time, d.duration)] = d;
+    }
+    return d;
+  },
+  cbg: function(d) {
+    d = cloneDeep(d);
+    if (d.units === 'mg/dL') {
+      d.value = translateBg(d.value);
+    }
+    return d;
+  },
+  deviceMeta: function(d) {
+    return cloneDeep(d);
+  },
+  message: function(d) {
+    return cloneDeep(d);
+  },
+  smbg: function(d) {
+    d = cloneDeep(d);
+    if (d.units === 'mg/dL') {
+      d.value = translateBg(d.value);
+    }
+    return d;
+  },
+  settings: function(d) {
+    d = cloneDeep(d);
+    if (d.units.bg === 'mg/dL') {
+      if (d.bgTarget) {
+        for (var key in d.bgTarget) {
+          if (key !== 'range' && key !== 'start') {
+            d.bgTarget[key] = translateBg(d.bgTarget[key]);
+          }
+        }
+      }
+      if (d.insulinSensitivity) {
+        var isfLen = d.insulinSensitivity.length;
+        for (var i = 0; i < isfLen; ++i) {
+          var item = d.insulinSensitivity[i];
+          item.amount = translateBg(item.amount);
+        }
+      }      
+    }
+    return d;
+  },
+  wizard: function(d) {
+    d = cloneDeep(d);
+    if (d.units === 'mg/dL') {
+      // truthiness is *required* here
+      // bgInput of 0 occurs often but means bgInput was skipped in wizard interaction
+      if (d.bgInput) {
+        d.bgInput = translateBg(d.bgInput);
+      }
+      if (d.bgTarget) {
+        for (var key in d.bgTarget) {
+          if (key !== 'range') {
+            d.bgTarget[key] = translateBg(d.bgTarget[key]);
+          }
+        }
+      }
+      if (d.insulinSensitivity) {
+        d.insulinSensitivity = translateBg(d.insulinSensitivity);
+      }
+    }
+    return d;
+  }
+};
 
 module.exports = {
+  erroredData: [],
+  mergeSuspendsIntoBasals: function(basals, suspendIntervals) {
+    basals = crossfilter(basals);
+    var basalsByTime = basals.dimension(function(d) {
+      return d.time + '/' + dt.addDuration(d.time, d.duration || 0); 
+    });
+    var numSuspends = suspendIntervals.length;
+    function intervalFilter(d) {
+      var interval = d.split('/');
+      // TODO: filter for intersection of basals and suspend intervals
+    }
+    for (var i = 0; i < numSuspends; ++i) {
+      var matches = basalsByTime.filterFunction(intervalFilter).top(Infinity);
+      if (matches.length > 0) {
+        console.log('Found at least one basal intersecting with a suspend!');
+      }
+    }
+  },
+  suspendedExtendeds: function(suspends, extendedIntervals) {
+    suspends = crossfilter(suspends);
+    var suspendsByTime = suspends.dimension(function(d) {
+      return d.time;
+    });
+    var intervals = Object.keys(extendedIntervals);
+    var numExtendeds = intervals.length;
+    for (var i = 0; i < numExtendeds; ++i) {
+      var matches = suspendsByTime.filter(intervals[i].split('/')).top(Infinity);
+      var bolus = extendedIntervals[intervals[i]];
+      // suspend interrupts an extended bolus
+      if (matches.length > 0) {
+        var suspend = matches[0];
+        if (!(bolus.extended >= 0 && bolus.expectedExtended > 0)) {
+          var err = new Error('An extended bolus interrupted by a suspend should have ' +
+            '`extended` and `expectedExtended` properties.');
+          bolus.errorMessage = err.message;
+          this.erroredData.push(bolus);
+        }
+        bolus.expectedDuration = bolus.duration;
+        bolus.duration = dt.difference(suspend.time, bolus.time);
+      }
+      // user cancels an extended bolus
+      if (bolus.expectedExtended && bolus.expectedExtended > 0 && !bolus.expectedDuration) {
+        var percentComplete = bolus.extended/bolus.expectedExtended;
+        bolus.expectedDuration = bolus.duration;
+        bolus.duration = percentComplete * bolus.duration;
+      }
+    }
+  },
+  joinWizardsAndBoluses: function(wizards, bolusesToJoin) {
+    var numWizards = wizards.length;
+    for (var i = 0; i < numWizards; ++i) {
+      var wizard = wizards[i];
+      if (wizard.joinKey != null) {
+        if (typeof bolusesToJoin[wizard.joinKey] === 'object') {
+          wizard.bolus = bolusesToJoin[wizard.joinKey];
+        }
+      }
+    }
+  },
   processData: function(data) {
+    var self = this;
     if (!(data && data.length >= 0 && Array.isArray(data))) {
       throw new Error('An array is required.');
     }
-    data = _.map(data, function(d) {
-      return _.cloneDeep(d);
-    });
-    return data;
+    var processedData = [];
+    var collections = {
+      bolusesToJoin: {},
+      extendedIntervals: {}
+    };
+    var typeGroups = {};
+
+    function process(d) {
+      try {
+        d = handlers[d.type](d, collections);
+      }
+      catch (e) {
+        var err = new Error(util.format('No nurseshark handler defined for type [%s]', d.type));
+        d.errorMessage = err.message;
+        self.erroredData.push(d);
+      }
+      // group data
+      if (typeGroups[d.type] == null) {
+        typeGroups[d.type] = [d];
+      }
+      else {
+        typeGroups[d.type].push(d);
+      }
+      processedData.push(d);
+    }
+
+    var iterations = Math.floor(data.length / 8);
+    var leftover = data.length % 8;
+    var i = 0;
+
+    if (leftover > 0){
+        do {
+          process(data[i++]);
+        } while (--leftover > 0);
+    }
+
+    if (iterations > 0) {
+      do {
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+        process(data[i++]);
+      } while (--iterations > 0);      
+    }
+
+    this.joinWizardsAndBoluses(typeGroups.wizard || [], collections.bolusesToJoin);
+
+    if (Object.keys(collections.extendedIntervals).length > 0) {
+      this.suspendedExtendeds(typeGroups.deviceMeta || [], collections.extendedIntervals);
+    }
+    if (typeGroups.deviceMeta && typeGroups.deviceMeta.length > 0) {
+      this.mergeSuspendsIntoBasals(typeGroups.basal || [], typeGroups.deviceMeta);
+    }
+    return {erroredData: this.erroredData, processedData: processedData};
   }
 };
--- test/nurseshark_test.js
+++ test/nurseshark_test.js
@@ -1,5 +1,18 @@
 /*
  * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ * 
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
  */
 
 var chai = require('chai');
@@ -21,22 +34,204 @@ describe('nurseshark', function() {
       expect(fn).to.throw('An array is required.');
     });
 
-    it('should return an array, empty if initial array was empty', function() {
-      var res = nurseshark.processData([]);
+    it('should return an object, with errors and processedData', function() {
+      var res = nurseshark.processData([{type:'cbg'},{type:'wizard'}]);
+      assert.isObject(res);
+      expect(res.erroredData).not.to.be.undefined;
+      expect(res.processedData).not.to.be.undefined;
+    });
+
+    it('should log an error when no type handler exists for an obj in the input array', function() {
+      var res = nurseshark.processData([{type:'foo'}]);
+      expect(res.erroredData.length).to.equal(1);
+    });
+
+    it('should return an array for processedData, empty if initial array was empty', function() {
+      var res = nurseshark.processData([]).processedData;
       expect((res && res.length >= 0 && Array.isArray(res))).to.be.true;
     });
 
     it('should return an array of new (not mutated) objects', function() {
-      var input = [{a: 1, z: {x: 100, y: 99}}, {b: 2}];
-      var output = nurseshark.processData(input);
+      var input = [{
+        type: 'bolus',
+        a: 1,
+        z: {
+          b: 2,
+          c: 3
+        }
+      }, {
+        type: 'wizard',
+        d: [{x: 4},{y: 5}]
+      }];
+      var output = nurseshark.processData(input).processedData;
       for (var i = 0; i < input.length; ++i) {
         expect(input[i] === output[i]).to.be.false;
       }
       expect(input[0].z === output[0].z).to.be.false;
+      expect(input[1].d[0] === output[1].d[0]).to.be.false;
+    });
+
+    it('should translate cbg and smbg into mg/dL when such units specified', function() {
+      var bgs = [{
+        type: 'cbg',
+        units: 'mg/dL',
+        value: 14.211645580300173
+      }, {
+        type: 'smbg',
+        units: 'mg/dL',
+        value: 2.487452256628842
+      }, {
+        type: 'cbg',
+        units: 'mmol/L',
+        value: 7.048584587016023
+      }];
+      var res = nurseshark.processData(bgs).processedData;
+      expect(res[0].value).to.equal(256);
+      expect(res[1].value).to.equal(45);
+      expect(res[2].value).to.equal(7.048584587016023);
+    });
+
+    it('should translate wizard bg-related fields to mg/dL when such units specified', function() {
+      var datum = [{
+        type: 'wizard',
+        units: 'mg/dL',
+        bgInput: 15.1518112923307,
+        bgTarget: {
+          high: 5.550747991045533,
+          low: 5.550747991045533
+        },
+        insulinSensitivity: 3.7753739955227665
+      }];
+      var res = nurseshark.processData(datum).processedData[0];
+      expect(res.bgInput).to.equal(273);
+      expect(res.bgTarget.low).to.equal(100);
+      expect(res.bgTarget.high).to.equal(100);
+      expect(res.insulinSensitivity).to.equal(68);
+    });
+
+    it('should translate settings bg-related fields to mg/dL when such units specified', function() {
+      var settings = [{
+        type: 'settings',
+        units: {
+          bg: 'mg/dL',
+          carb: 'grams'
+        },
+        bgTarget: {
+          target: 6.66089758925464,
+          range: 10
+        },
+        insulinSensitivity: [
+          {
+            amount: 4.440598392836427,
+            start: 0
+          },
+          {
+            amount: 4.9956731919409805,
+            start: 43200000
+          }
+        ]
+      }];
+      var res = nurseshark.processData(settings).processedData[0];
+      expect(res.bgTarget.target).to.equal(120);
+      expect(res.bgTarget.range).to.equal(10);
+      expect(res.insulinSensitivity[0].amount).to.equal(80);
+      expect(res.insulinSensitivity[1].amount).to.equal(90);
     });
   });
 
-  describe('mungeSuspends', function() {
+  describe('joinWizardsAndBoluses', function() {
+    it('should be a function', function() {
+      assert.isFunction(nurseshark.joinWizardsAndBoluses);
+    });
 
+    it('should join a bolus and wizard with matching joinKey', function() {
+      var data = [{
+        type: 'bolus',
+        joinKey: '12345',
+        id: 'abcde'
+      }, {
+        type: 'wizard',
+        joinKey: '12345',
+        id: 'bcdef'
+      }, {
+        type: 'bolus',
+        id: 'cdefg'
+      }, {
+        type: 'wizard',
+        id: 'defgh'
+      }];
+      var res = nurseshark.processData(data).processedData;
+      var embeddedBolus = res[1].bolus;
+      var secondWiz = res[3];
+      expect(embeddedBolus.id).to.equal(data[0].id);
+      expect(secondWiz.bolus).to.be.undefined;
+    });
+  });
+
+  describe('suspendedExtendeds', function() {
+    it('should be a function', function() {
+      assert.isFunction(nurseshark.suspendedExtendeds);
+    });
+
+    it('should find an extended interrupted by a suspend event', function() {
+      var now = new Date();
+      var earlier = new Date(now.valueOf() - 720000);
+      var input = [{
+        type: 'deviceMeta',
+        subType: 'status',
+        status: 'suspended',
+        duration: 10000,
+        time: now.toISOString()
+      }, {
+        type: 'bolus',
+        subType: 'extended',
+        extended: 0.2,
+        expectedExtended: 1.0,
+        duration: 3600000,
+        time: earlier.toISOString()
+      }];
+      var output = [{
+        type: 'deviceMeta',
+        subType: 'status',
+        status: 'suspended',
+        duration: 10000,
+        time: now.toISOString()
+      }, {
+        type: 'bolus',
+        subType: 'extended',
+        extended: 0.2,
+        expectedExtended: 1.0,
+        duration: 720000,
+        expectedDuration: 3600000,
+        time: earlier.toISOString()
+      }];
+      var res = nurseshark.processData(input).processedData;
+      expect(res[1].duration).to.equal(output[1].duration);
+    });
+
+    it('should add an expectedDuration to user-cancelled extended boluses', function() {
+      var now = new Date().toISOString();
+      var cancelled = [{
+        type: 'bolus',
+        subType: 'square',
+        extended: 0.4,
+        expectedExtended: 2.0,
+        duration: 10000,
+        time: now
+      }];
+      var res = nurseshark.processData(cancelled).processedData[0];
+      expect(res.duration).to.equal(2000);
+      expect(res.expectedDuration).to.equal(10000);
+    });
+  });
+
+  // TODO: remove this! just for development
+  describe('nurseshark on real data', function() {
+    var data = require('../example/data/blip-input.json');
+    it('should succeed', function() {
+      console.time('Nurseshark');
+      assert.isArray(nurseshark.processData(data).processedData);
+      console.timeEnd('Nurseshark');
+    });
   });
 });
