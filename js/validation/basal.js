3dc76db78cf2f0b7a7631ed97d32f68a6c862067
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-09-25 19:12:48 -0700

    unbreak master

--- js/validation/basal.js
+++ js/validation/basal.js
@@ -5,7 +5,6 @@ module.exports = schema(
   common,
   {
     deliveryType: schema().in(['scheduled', 'suspend', 'temp']),
-    deviceTime: schema().isDeviceTime(),
     duration: schema().ifExists().number().min(0),
     normalEnd: schema().isISODateTime(),
     value: schema().number().min(0)
