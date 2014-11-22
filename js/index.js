3c7cf0d426f7311c2e232a074a4fad676b760536
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-11-21 17:18:31 -0800

    misc housekeeping: move d3.chart into a dependency instead of devDependency, require it in the tideline export

--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
  */
+require('script!d3/d3.min.js');
+require('script!d3.chart/d3.chart.min.js');
 
 module.exports = {
   TidelineData: require('./tidelinedata'),
