cc96ea4b74d514418bc83ef319049b734f0fb077
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-12-15 00:38:03 -0800

    update annotation definitions for new uploader

--- js/plot/util/annotations/annotationdefinitions.js
+++ js/plot/util/annotations/annotationdefinitions.js
@@ -16,43 +16,6 @@
  */
 
 // You can view the full text of each annotation by running mocha test/annotations_test.js
-// Current output:
-//
-// Main text annotation for carelink/basal/temp-percent-create-scheduled:
-// We are calculating the temp basal rates here by applying the percentage of the temp basal to your current schedule, but Demo did not directly provide us with these rate changes.
-//
-// Main text annotation for carelink/basal/off-schedule-rate:
-// You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how Demo reports the data, we can't be 100% certain of your basal rate here.
-//
-// Main text annotation for carelink/settings/basal-mismatch:
-// You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how Demo reports the data, we can't be 100% certain of your basal settings here.
-//
-// Main text annotation for carelink/settings/wizard-mismatch:
-// You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how Demo reports the data, we can't be 100% certain of your bolus wizard settings here.
-//
-// Main text annotation for carelink/settings/activeSchedule-mismatch:
-// You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how Demo reports the data, we can't be 100% certain of your active basal pattern here.
-//
-// Main text annotation for carelink/device-overlap-boundary:
-// We are not showing data here because we've received datasets from multiple pumps that are in conflict. Demo did not provide enough information to distinguish the data from each pump.
-//
-// Main text annotation for diasend/basal/temp-basal-fabrication:
-// The Demo .xls file doesn't report temp basals directly, so we have to infer from other evidence where you might have been using a temp basal rate. We think this segment could be a temp basal, but it may not be.
-//
-// Main text annotation for diasend/basal/temp-duration-truncated:
-// Because of how the Demo .xls file reports the data, we've truncated what may have been a temp basal here to a maximum duration of 120 hours.
-//
-// Main text annotation for diasend/bolus/extended:
-// The Demo .xls file doesn't report the split between the intitial and the extended delivery during a combo bolus. All we can display is the duration of the combo bolus and the total dose delivered.
-//
-// Main text annotation for basal/intersects-incomplete-suspend
-// Within this basal segment, we are omitting a suspend event that didn't end. This may have resulted from switching to a new device. As a result, this basal segment may be inaccurate.
-//
-// Main text annotation for stats-insufficient-data:
-// There is not enough data to show this statistic.
-//
-// Lead text annotation for stats-insufficient-data:
-// Why is this grey? 
 
 var format = require('../../../data/util/format');
 
@@ -66,9 +29,14 @@ var definitions = {
     }
   },
   MAIN_TEXT: {
-    'carelink/basal/temp-percent-create-scheduled': function(source, defs) {
-      var a = "We are calculating the temp basal rates here by applying the percentage of the temp basal to your current schedule, but ";
+    'carelink/basal/fabricated-from-schedule': function(source, defs) {
+      var a = "We are calculating the basal rates here using the active basal schedule in your pump settings (and applying the percentage of an active temp basal where applicable), but ";
       var b = " did not directly provide us with these rate changes.";
+      return defs.stitch(a, b, source);
+    },
+    'carelink/basal/fabricated-from-suppressed': function(source, defs) {
+      var a = "We are deriving the basal rate to display here assuming that your pump resumed to the basal that was active before the pump was suspended, but ";
+      var b = " did not directly provide us with this rate change.";
       return defs.stitch(a, b, source);
     },
     'carelink/basal/off-schedule-rate': function(source, defs) {
@@ -76,43 +44,16 @@ var definitions = {
       var b = " reports the data, we can't be 100% certain of your basal rate here.";
       return defs.stitch(a, b, source);
     },
-    'carelink/settings/basal-mismatch': function(source, defs) {
-      var a = "You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how ";
-      var b = " reports the data, we can't be 100% certain of your basal settings here.";
-      return defs.stitch(a, b, source);
-    },
-    'carelink/settings/wizard-mismatch': function(source, defs) {
-      var a = "You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how ";
-      var b = " reports the data, we can't be 100% certain of your bolus wizard settings here.";
-      return defs.stitch(a, b, source);
-    },
-    'carelink/settings/activeSchedule-mismatch': function(source, defs) {
-      var a = "You may have changed pumps recently - perhaps because you had to have your pump replaced due to malfuction. As a result of how ";
-      var b = " reports the data, we can't be 100% certain of your active basal pattern here.";
-      return defs.stitch(a, b, source);
-    },
-    'carelink/device-overlap-boundary': function(source, defs) {
-      var a = "We are not showing data here because we've received datasets from multiple pumps that are in conflict.";
-      var b = " did not provide enough information to distinguish the data from each pump.";
-      return defs.stitch(a, b, source);
-    },
-    'diasend/basal/temp-basal-fabrication': function(source, defs) {
-      var a = "The ";
-      var b = " .xls file doesn't report temp basals directly, so we have to infer from other evidence where you might have been using a temp basal rate. We think this segment could be a temp basal, but it may not be.";
-      return defs.stitch(a, b, source);
-    },
-    'diasend/basal/temp-duration-truncated': function(source, defs) {
-      var a = "Because of how the ";
-      var b = " .xls file reports the data, we've truncated what may have been a temp basal here to a maximum duration of 120 hours.";
-      return defs.stitch(a, b, source);
-    },
-    'diasend/bolus/extended': function(source, defs) {
-      var a = "The ";
-      var b = " .xls file doesn't report the split between the intitial and the extended delivery during a combo bolus. All we can display is the duration of the combo bolus and the total dose delivered.";
-      return defs.stitch(a, b, source);
-    },
     'basal/intersects-incomplete-susppend': function() {
       return "Within this basal segment, we are omitting a suspend event that didn't end. This may have resulted from switching to a new device. As a result, this basal segment may be inaccurate.";
+    },
+    'basal/mismatched-series': function() {
+      return "A basal rate segment may be missing here because it wasn't reported to the Tidepool API in sequence. We can't be 100% certain of your basal rate here.";
+    },
+    'basal/unknown-duration': function(source, defs) {
+      var a = "Because of how ";
+      var b = " reports the data, we could not determine the duration of this basal rate segment.";
+      return defs.stitch(a, b, source);
     },
     'stats-insufficient-data': function() {
       return 'There is not enough data to show this statistic.';
