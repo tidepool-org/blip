38e7718f1087ac3e61dfdf9ae98273497709dbdf
Jamie <jamie@tidepool.org>
2014-12-18 10:43:35 +1300

    cleanup after review

--- test/unit/idTest.js
+++ test/unit/idTest.js
@@ -50,14 +50,23 @@ describe('id generator', function () {
 
   it('makes diff ids', function (done) {
     try{
-      var myIdOne = id.generateId(['carelink','token']);
+      var myIdOne = id.generateId(['carelink','2014-12-17T21:40:21+13:00','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkdXIiOjM2MDAsImV4cCI6MTQxODgwOTE3MSwic3ZyIjoibm8iLCJ1c3IiOiJmYThiYmU2OTE4In0.372YtFsvoeTBzqLinf8FQC6enzNpiKvToI30P_c4Kpo']);
       expect(myIdOne).is.not.empty;
-      var myIdTwo = id.generateId(['carelinkto','ken']);
+      var myIdTwo = id.generateId(['carelink','2014-12-17T21:42:19+13:00','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkdXIiOjM2MDAsImV4cCI6MTQxODgwOTE3MSwic3ZyIjoibm8iLCJ1c3IiOiJmYThiYmU2OTE4In0.372YtFsvoeTBzqLinf8FQC6enzNpiKvToI30P_c4Kpo']);
       expect(myIdTwo).is.not.empty;
       expect(myIdOne).to.not.equal(myIdTwo);
       done();
     }catch(error){
       expect(error).is.empty;
     }
+  });
+
+  it.skip('crypto sha1 is the same as other sha1', function (done) {
+
+    var PYTHON_SHA1 = '6a2adc8bdba3823dff69b7bfdca87ea150b3931d';
+    var ourSha1 = id.generateId(['abc','def','hij']);
+    console.log('ours ',ourSha1);
+    expect(ourSha1).to.equal(PYTHON_SHA1);
+    done();
   });
 });
