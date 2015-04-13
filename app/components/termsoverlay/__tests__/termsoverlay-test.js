c4a61c4ec1cc5589cf34064d3cac95b18c6ec189
Jamie <jamie@tidepool.org>
2015-04-13 08:21:53 -0700

    now showing and tested age verification step

--- app/components/termsoverlay/__tests__/termsoverlay-test.js
+++ app/components/termsoverlay/__tests__/termsoverlay-test.js
@@ -17,19 +17,79 @@
 
 jest.dontMock('../termsoverlay');
 
+var React = require('react/addons');
+var TermsOverlay = require('../termsoverlay.js');
+var TestUtils = React.addons.TestUtils;
+
+/*
+TOU = Terms of Use
+PP = Privacy Policy
+
+Final URLs will be: TOU: http://developer.tidepool.io/privacy-policy/
+PP: http://developer.tidepool.io/terms-of-use/
+
+Prior to showing TOU and PP, present a
+Before you can sign up for Blip, we need to know how old you are: [ ] I am 18 years old or older. (default selection)
+[ ] I am between 13 and 17 years old. You'll need to have a parent or guardian agree to the terms on the next screen.
+[ ] I am 12 years old or younger.
+[ CONTINUE ]
+
+Store the state of this selection for the user.
+
+== For under 12 login flow. ==
+Display: "We are really sorry, but you need to be 13 or older in order to create an account and use Tidepool's Applications."
+
+== For 18 and over login flow: ==
+Present TOU and PP in separate scrollable windows. Can be side by side or top/bottom or one followed by the next: Present one checkbox and text.
+[ ] "I am 18 or older and I accept the terms of the Tidepool Applications Terms of Use and Privacy Policy".
+Do not enable [I ACCEPT] button until the checkbox is selected.
+
+== For 13 - 17 login flow: ==
+Present TOU and PP in separate scrollable windows (as above).
+Present TWO checkboxes: [ ] "I am 18 or older and I accept the terms of the Tidepool Applications Terms of Use and Privacy Policy".
+[ ] "I to my child aged 13 through 17 using Tidepool Applications and agree that they are also bound to the terms of the Tidepool Applications Terms of Use and Privacy Policy".
+
+Do not enable [I ACCEPT] button until BOTH checkboxes are selected.
+*/
+
+
+
 describe('termsoverlay', function() {
-
-  var React = require('react/addons');
-  var TermsOverlay = require('../termsoverlay.js');
-  var TestUtils = React.addons.TestUtils;
-
-
   it('is not agreed by default', function() {
-    // Render a checkbox with label in the document
+    var terms = TestUtils.renderIntoDocument(
+      <TermsOverlay />
+    );
+    expect(terms.state.agreed).toEqual(false);
+  });
+  it('is not agreed by default', function() {
+    var terms = TestUtils.renderIntoDocument(
+      <TermsOverlay />
+    );
+    expect(terms.state.isChecked).toEqual(false);
+  });
+  it('age is not confirmed by default', function() {
+    var terms = TestUtils.renderIntoDocument(
+      <TermsOverlay />
+    );
+    expect(terms.state.ageConfirmed).toEqual(false);
+  });
+  it('shows age confirmation form by defaut', function() {
     var terms = TestUtils.renderIntoDocument(
       <TermsOverlay />
     );
 
-    expect(terms.state.agreed).toEqual(false);
+    var age = TestUtils.findRenderedDOMComponentWithClass(terms, 'terms-overlay-age-form');
+    expect(age).not.toBeNull();
+
+    var options = age.props.children[0];
+    var button = age.props.children[1];
+
+    expect(options.length).toEqual(3);
+
+    for (var i = options.length - 1; i >= 0; i--) {
+      expect(options[i].type).toEqual('input');
+    };
+
+    expect(button.type).toEqual('button');
   });
 });
--- app/components/termsoverlay/termsoverlay.js
+++ app/components/termsoverlay/termsoverlay.js
@@ -15,20 +15,53 @@
  */
 
 var React = require('react');
+var _ = require('lodash');
+
+var AGES = [
+  {value: '>=18', isDefault: true, label: 'I am 18 years old or older.'},
+  {value: '13-17', isDefault: false, label: 'I am between 13 and 17 years old. You\'ll need to have a parent or guardian agree to the terms on the next screen.' },
+  {value: '<=12', isDefault: false, label: 'I am 12 years old or younger.'}
+];
 
 var TermsOverlay = React.createClass({
   propTypes: {
     onSubmit: React.PropTypes.func,
     trackMetric: React.PropTypes.func.isRequired
   },
-
   getInitialState: function() {
     return {
-      agreed: false
+      agreed: false,
+      isChecked: false,
+      ageConfirmed: false
     };
   },
+  onChange: function() {
+    this.setState({isChecked: !this.state.isChecked});
+  },
+  /*
+   * Age consent
+   */
+  renderStepOne:function(ages){
 
-  render: function() {
+    var opts=[];
+
+    for (var i = ages.length - 1; i >= 0; i--) {
+      opts.push(<input type="radio" key={ages[i].value} value={ages[i].value}>{ages[i].label}</input>);
+    };
+
+    return (
+      <form ref='ageConfirmation' className="terms-overlay-age-form">
+        {opts}
+        <button
+          className="btn btn-primary js-terms-submit"
+          onClick={this.handleAgeSubmit}>Continue</button>
+      </form>
+    );
+  },
+  /*
+   * Terms & privacy acceptance
+   */
+  renderStepTwo:function(){
     var checkbox = this.renderCheckbox();
     var submitButton = this.renderSubmitButton();
     var terms = this.websiteTerms();
@@ -51,7 +84,12 @@ var TermsOverlay = React.createClass({
       /* jshint ignore:end */
     );
   },
-
+  render: function() {
+    if(this.state.ageConfirmed){
+      return this.renderStepTwo();
+    }
+    return this.renderStepOne(AGES);
+  },
   websiteTerms: function() {
     return React.DOM.iframe({
       className         : "terms-overlay-iframe",
@@ -61,7 +99,6 @@ var TermsOverlay = React.createClass({
       allowTransparency : "true"
     });
   },
-
   websitePrivacy: function() {
     return React.DOM.iframe({
       className         : "terms-overlay-iframe",
@@ -71,10 +108,8 @@ var TermsOverlay = React.createClass({
       allowTransparency : "true"
     });
   },
-
   renderSubmitButton: function() {
     var disabled = !this.state.agreed;
-
     /* jshint ignore:start */
     return (
       <button
@@ -84,11 +119,18 @@ var TermsOverlay = React.createClass({
     );
     /* jshint ignore:end */
   },
+  handleAgeSubmit: function(e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.props.trackMetric('Confirmed age');
+    this.setState({ageConfirmed: true});
+  },
 
   renderCheckbox: function() {
     /* jshint ignore:start */
     return (
-      <div className="terms-overlay-form-checkbox">
+      <div className="terms-overlay-accept-checkbox">
         <label htmlFor="agreed">
           <input
             id="agreed"
