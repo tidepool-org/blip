98305bbaa0194273c9e8a3f94eaccadcde71de63
kentquirk <kentquirk@gmail.com>
2015-04-20 23:19:51 -0400

    Re-add DEV -- it's still needed

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ var defineEnvPlugin = new webpack.DefinePlugin({
   __UPLOAD_API__: JSON.stringify(process.env.UPLOAD_API || null),
   __API_HOST__: JSON.stringify(process.env.API_HOST || null),
   __SHOW_ACCEPT_TERMS__: JSON.stringify(process.env.SHOW_ACCEPT_TERMS || null),
-  __PASSWORD_MIN_LENGTH__: JSON.stringify(process.env.PASSWORD_MIN_LENGTH || null)
+  __PASSWORD_MIN_LENGTH__: JSON.stringify(process.env.PASSWORD_MIN_LENGTH || null),
+  __DEV__: false
 });
 
 module.exports = {
