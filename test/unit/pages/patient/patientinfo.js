bf71b75db61b9dd31691136d27c79a9ffcd87aac
Gordon Dent <gordonmdent@gmail.com>
2015-05-28 15:41:04 +0100

    describe blocks for render and isSamePersonUserAndPatient

--- test/unit/pages/patient/patientinfo.js
+++ test/unit/pages/patient/patientinfo.js
@@ -13,8 +13,61 @@ describe('PatientInfo', function () {
     expect(TestUtils.isElement(<PatientInfo/>)).toBe(true);
   });
 
-  it('should render without problems', function () {
-    var elem = TestUtils.renderIntoDocument(<PatientInfo/>);
-    expect(elem).toExist();
+
+  describe('render', function() {
+    it('should console.warn when trackMetric not set', function () {
+      console.warn = sinon.spy();
+      var elem = TestUtils.renderIntoDocument(<PatientInfo/>);
+      expect(elem).toExist();
+      expect(console.warn.calledWith('Warning: Required prop `trackMetric` was not specified in `PatientInfo`.')).toBe(true);
+    });
+
+    it('should not console.warn when trackMetric set', function() {
+      console.warn = sinon.spy();
+      var props = {
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      expect(elem).toExist();
+      expect(console.warn.callCount).toBe(0);
+    });
+  });
+  
+  describe('isSamePersonUserAndPatient', function() {
+    it('should return false when both userids are the different', function() {
+      var props = {
+        user: {
+          userid: 'foo'
+        },
+        patient: {
+          userid: 'bar'
+        },
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      expect(elem).toExist();
+      expect(elem.isSamePersonUserAndPatient()).toBe(false);
+    });
+
+    it('should return true when both userids are the same', function() {
+      var props = {
+        user: {
+          userid: 1
+        },
+        patient: {
+          userid: 1
+        },
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      expect(elem).toExist();
+      expect(elem.isSamePersonUserAndPatient()).toBe(true);
+    });
   });
 });
