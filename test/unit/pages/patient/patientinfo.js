c896a5aa53903efa7fa0d104d00dcfafd80459c1
Gordon Dent <gordonmdent@gmail.com>
2015-05-28 16:54:04 +0100

    Add getDiagnosisText and test behaviour

--- test/unit/pages/patient/patientinfo.js
+++ test/unit/pages/patient/patientinfo.js
@@ -142,5 +142,73 @@ describe('PatientInfo', function () {
   });
 
   describe('getDiagnosisText', function() {
+    it('should return unknown birthday if less than 1 years old, or birthdate in future', function() {
+      var props = {
+        patient: {
+          userid: 1,
+          profile: {
+            patient: {
+              diagnosisDate: '1984-05-18'
+            }
+          }
+        },
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      expect(elem).toExist();
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1983, 3, 20))).toBe('Diagnosis date not known');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1982, 4, 20))).toBe('Diagnosis date not known');
+    });
+
+    it('should return text representing years difference', function() {
+      var props = {
+        patient: {
+          userid: 1,
+          profile: {
+            patient: {
+              diagnosisDate: '1984-05-18'
+            }
+          }
+        },
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      expect(elem).toExist();
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1984, 4, 18))).toBe('Diagnosed this year');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1985, 4, 19))).toBe('Diagnosed 1 year ago');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1986, 4, 19))).toBe('Diagnosed 2 years ago');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1987, 4, 19))).toBe('Diagnosed 3 years ago');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1988, 4, 19))).toBe('Diagnosed 4 years ago');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(1999, 4, 19))).toBe('Diagnosed 15 years ago');
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(2015, 4, 19))).toBe('Diagnosed 31 years ago');
+    });
+
+    it('should handle return correct text representation for various diagnosisDates', function() {
+      var props = {
+        patient: {
+          userid: 1,
+          profile: {
+            patient: {
+              diagnosisDate: '1984-05-18'
+            }
+          }
+        },
+        trackMetric: function() {}
+      };
+
+      var patientInfoElem = React.createElement(PatientInfo, props);
+      var elem = TestUtils.renderIntoDocument(patientInfoElem);
+      var today = new Date(2015, 4, 28); //for testing purposes - set today as fixed
+      expect(elem).toExist();
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(2015, 4, 28))).toBe('Diagnosed 31 years ago');
+      elem.props.patient.profile.patient.diagnosisDate = '1984-04-30';
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(2015, 4, 28))).toBe('Diagnosed 31 years ago');
+      elem.props.patient.profile.patient.diagnosisDate = '1984-05-29';
+      expect(elem.getDiagnosisText(elem.props.patient, new Date(2015, 4, 28))).toBe('Diagnosed 30 years ago');
+    });
   });
 });
