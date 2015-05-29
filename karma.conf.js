6dd274e097c293d3e3b176063b2154ae994be5a0
Gordon Dent <gordonmdent@gmail.com>
2015-05-29 13:47:48 +0100

    Use chai over expect library and remove redundant  dependency

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
   config.set({
     browsers: [ 'PhantomJS' ], // Use PhantomJS for now (@gordyd - I'm using a VM)
     singleRun: true,
-    frameworks: [ 'mocha', 'sinon' ], // Mocha is our testing framework of choice
+    frameworks: [ 'mocha', 'sinon', 'chai' ], // Mocha is our testing framework of choice
     files: [
       'tests.webpack.js' // We're using Webpack to build
     ],
