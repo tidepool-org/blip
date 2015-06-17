d4d43d3996f153485e79c454134dfebe23cfe73b
Gordon Dent <gordonmdent@gmail.com>
2015-06-17 16:56:53 +0100

    New tests

---
+++ test/unit/components/browserwarningoverlay.test.js
@@ -0,0 +1,46 @@
+/** @jsx React.DOM */
+/* global chai */
+
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+var expect = chai.expect;
+
+var BrowserWarningOverlay = require('../../../app/components/browserwarningoverlay');
+
+describe('BrowserWarningOverlay', function () {
+
+  it('should be exported correctly as a module and function', function() {
+    expect(BrowserWarningOverlay).to.be.a('function');
+  });
+
+  describe('render', function() {
+    it('should render without problems', function () {
+      var elem = TestUtils.renderIntoDocument(<BrowserWarningOverlay/>);
+      expect(elem).to.be.ok;
+    });
+  });
+
+  describe('handleSubmit', function() {
+    it('should do nothing when no onSubmit defined and submit is clicked', function () {
+      var elem = TestUtils.renderIntoDocument(<BrowserWarningOverlay/>);
+      expect(elem).to.be.ok;
+      var submitButton = TestUtils.findRenderedDOMComponentWithClass(elem, 'js-terms-submit');
+
+      TestUtils.Simulate.click(submitButton);
+    });
+
+    it('should call onSubmit when it is defined and submit is clicked', function () {
+      var props = {
+        onSubmit: sinon.stub()
+      };
+      var warningElem = React.createElement(BrowserWarningOverlay, props);
+      var elem = TestUtils.renderIntoDocument(warningElem);
+      expect(elem).to.be.ok;
+      var submitButton = TestUtils.findRenderedDOMComponentWithClass(elem, 'js-terms-submit');
+
+      expect(props.onSubmit.callCount).to.equal(0);
+      TestUtils.Simulate.click(submitButton);
+      expect(props.onSubmit.callCount).to.equal(1);
+    });
+  });
+});
