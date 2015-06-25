bf1a7e5143a2632e34b1f29ed08a5eefd178e52b
Gordon Dent <gordonmdent@gmail.com>
2015-06-25 13:12:23 +0100

    Update invitation to make more of the required props set to isRequired and write testing around them

---
+++ test/unit/components/invitation.test.js
@@ -0,0 +1,40 @@
+/** @jsx React.DOM */
+
+/* global chai */
+window.config = {};
+
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+var expect = chai.expect;
+
+var Invitation = require('../../../app/components/invitation');
+
+describe('Invitation', function () {
+  it('should be exposed as a module and be of type function', function() {
+    expect(Invitation).to.be.a('function');
+  });
+
+  describe('render', function() {
+    it('should render console.warn when required props are missing', function () {
+      console.warn = sinon.stub();
+      var elem = TestUtils.renderIntoDocument(<Invitation />);
+      expect(console.warn.callCount).to.equal(4);
+      expect(console.warn.calledWith('Warning: Required prop `trackMetric` was not specified in `Invitation`.')).to.equal(true);
+    });
+
+    it('should render without problems with required props are present', function () {
+      console.warn = sinon.stub();
+      var props = {
+        invitation: {
+          creator: 'awesome'
+        },
+        onAcceptInvitation: sinon.stub(),
+        onDismissInvitation: sinon.stub(),
+        trackMetric: sinon.stub(),
+      };
+      var elem = React.createElement(Invitation, props);
+      var render = TestUtils.renderIntoDocument(elem);
+      expect(console.warn.callCount).to.equal(0);
+    });
+  });
+});
