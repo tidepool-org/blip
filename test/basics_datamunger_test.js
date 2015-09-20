47bae4ca17618750dd19bca18b7ba731df485b42
Jana E. Beck <jana.eliz.beck@gmail.com>
2015-09-21 01:29:56 +0200

    fill out basics data tests for calc of BG distribution

--- test/basics_datamunger_test.js
+++ test/basics_datamunger_test.js
@@ -19,7 +19,12 @@ var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 
+var _ = require('lodash');
+var d3 = require('d3');
+
 var dm = require('../plugins/blip/basics/logic/datamunger');
+
+var types = require('../dev/testpage/types');
 
 describe('basics datamunger', function() {
   it('should be an object', function() {
@@ -27,19 +32,79 @@ describe('basics datamunger', function() {
   });
 
   describe('bgDistribution', function() {
+    var bgClasses = {
+      'very-low': {boundary: 10},
+      low: {boundary: 20},
+      target: {boundary: 30},
+      high: {boundary: 40},
+      'very-high': {boundary: 50}
+    };
+    var zeroes = {
+      veryhigh: 0,
+      high: 0,
+      target: 0,
+      low: 0,
+      verylow: 0
+    };
     it('should be a function', function() {
       assert.isFunction(dm.bgDistribution);
     });
 
-    it('should always calculate a BG distribution for smbg data');
+    it('should always calculate a BG distribution for smbg data, should calculate a BG distribution for cbg data if averages >= 144 readings per day, and should yield cgmStatus `calculatedCGM` when have calculated a BG distribution for cbg data', function() {
+      var now = new Date();
+      var smbg = [
+        new types.SMBG({value: 25})
+      ];
+      var cbg = [];
+      for (var i = 0; i < 144; ++i) {
+        cbg.push(new types.CBG({
+          deviceTime: new Date(now.valueOf() + i*2000).toISOString().slice(0,-5),
+          value: 50
+        }));
+      }
+      expect(dm.bgDistribution({
+        data: {smbg: {data: smbg}, cbg: {data: cbg}},
+        dateRange: [d3.time.day.utc.floor(now), d3.time.day.utc.ceil(now)]
+      }, bgClasses)).to.deep.equal({
+        cbg: _.defaults({veryhigh: 1}, zeroes),
+        cgmStatus: 'calculatedCGM',
+        smbg: _.defaults({target: 1}, zeroes)
+      });
+    });
 
-    it('should yield cgmStatus `noCGM` if no cbg data');
+    it('should yield cgmStatus `noCGM` if no cbg data', function() {
+      var now = new Date();
+      var smbg = [
+        new types.SMBG({value: 1}),
+        new types.SMBG({value: 25})
+      ];
+      expect(dm.bgDistribution({
+        data: {smbg: {data: smbg}},
+        dateRange: [d3.time.day.utc.floor(now), d3.time.day.utc.ceil(now)]
+      }, bgClasses)).to.deep.equal({
+        cgmStatus: 'noCGM',
+        smbg: _.defaults({target: 0.5, verylow: 0.5}, zeroes)
+      });
+    });
 
-    it('should yield cgmStatus `notEnoughCGM` if not enough cbg data');
+    it('should yield cgmStatus `notEnoughCGM` if not enough cbg data', function() {
+      var now = new Date();
+      var smbg = [
+        new types.SMBG({value: 1}),
+        new types.SMBG({value: 25})
+      ];
+      var cbg = [
+        new types.CBG({value: 50})
+      ];
+      expect(dm.bgDistribution({
+        data: {smbg: {data: smbg}, cbg: {data: cbg}},
+        dateRange: [d3.time.day.utc.floor(now), d3.time.day.utc.ceil(now)]
+      }, bgClasses)).to.deep.equal({
+        cgmStatus: 'notEnoughCGM',
+        smbg: _.defaults({target: 0.5, verylow: 0.5}, zeroes)
+      });
 
-    it('should calculate a BG distribution for cbg data if averages >= 144 readings per day');
-
-    it('should yield cgmStatus `calculatedCGM` when have calculated a BG distribution for cbg data');
+    });
   });
 
   describe('calculateBasalBolusStats', function() {
