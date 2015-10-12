a013c73d1074c28474f17b99958027e44df44d3a
Jana E. Beck <jana.eliz.beck@gmail.com>
2015-10-12 15:17:30 -0700

    finish out classifiers tests

--- test/basics_classifiers_test.js
+++ test/basics_classifiers_test.js
@@ -19,6 +19,14 @@ var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 
+var bgClasses = {
+  'very-low': {boundary: 10},
+  low: {boundary: 20},
+  target: {boundary: 30},
+  high: {boundary: 40},
+  'very-high': {boundary: 50}
+};
+
 var classifiers = require('../plugins/blip/basics/logic/classifiers');
 
 describe('basics classifiers', function() {
@@ -26,13 +34,118 @@ describe('basics classifiers', function() {
     assert.isFunction(classifiers);
   });
 
-  it('should return an object');
+  it('should return an object', function() {
+    assert.isObject(classifiers());
+  });
 
-  it('should return all tags that apply to a basal (based on deliveryType)');
+  describe('basal', function() {
+    var classifier = classifiers().basal;
+    it('should return no tags for a scheduled basal', function() {
+      expect(classifier({deliveryType: 'scheduled'})).to.deep.equal([]);
+    });
 
-  it('should return all tags that apply to a bolus');
+    it('should return `temp` for a temp basal', function() {
+      expect(classifier({deliveryType: 'temp'})).to.deep.equal(['temp']);
+    });
 
-  it('should classify an smbg as manual or meter');
+    it('should return `suspended` for a temp basal', function() {
+      expect(classifier({deliveryType: 'suspended'})).to.deep.equal(['suspended']);
+    });
+  });
 
-  it('should also categorize the value of an smbg');
+  describe('bolus', function() {
+    var classifier = classifiers().bolus;
+
+    it('should return `wizard` and `correction` tags for a correction-only bolus from wizard', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 1.0, carb: 0, net: 1.0}},
+        normal: 1.0
+      })).to.deep.equal(['wizard', 'correction']);
+    });
+
+    it('should return `wizard` and `override` for an underridden bolus', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 1.0, carb: 2.0, net: 3.0}},
+        normal: 1.5
+      })).to.deep.equal(['wizard', 'override']);
+    });
+
+    it('should return `manual`, `extended`, and `interrupted` for an interrupted non-wizard extended bolus', function() {
+      expect(classifier({
+        extended: 5.0,
+        expectedExtended: 5.5
+      })).to.deep.equal(['manual', 'interrupted', 'extended']);
+    });
+
+    it('is possible to get all tags but `manual` on a single bolus', function() {
+      expect(classifier({
+        extended: 1.2,
+        expectedExtended: 2.0,
+        wizard: {recommended: {correction: 2.5, carb: 0, net: 2.5}}
+      })).to.deep.equal(['wizard', 'override', 'correction', 'interrupted', 'extended']);
+    });
+
+    it('net recommendation is what counts for determining override', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 2.5, carb: 0, net: 2.2}},
+        normal: 2.5
+      })).to.deep.equal(['wizard', 'override', 'correction']);
+    });
+
+    it('corner case: interrupted correction zero bolus', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 1.0, carb: 0, net: 1.0}},
+        normal: 0.0,
+        expectedNormal: 1.0
+      })).to.deep.equal(['wizard', 'correction', 'interrupted']);
+    });
+
+    it('corner case: interrupt an override to recommended amount', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 3.0, carb: 5.0, net: 7.5}},
+        normal: 7.5,
+        expectedNormal: 8.5
+      })).to.deep.equal(['wizard', 'override', 'interrupted']);
+    });
+
+    it('corner case: good intentions count! (interrupted bolus does not automatically = override)', function() {
+      expect(classifier({
+        wizard: {recommended: {correction: 2.5, carb: 0, net: 2.2}},
+        normal: 1.0,
+        expectedNormal: 2.2
+      })).to.deep.equal(['wizard', 'correction', 'interrupted']);
+    });
+  });
+
+  describe('smbg', function() {
+    var classifier = classifiers(bgClasses).smbg;
+    it('should classify a non-subTyped smbg as `meter`', function() {
+      expect(classifier({value: 25})).to.deep.equal(['meter']);
+    });
+
+    it('should classify a `linked` smbg as `meter`', function() {
+      expect(classifier({value: 25, subType: 'linked'})).to.deep.equal(['meter']);
+    });
+
+    it('should classify a `manual` smbg as `manual`', function() {
+      expect(classifier({value: 25, subType: 'manual'})).to.deep.equal(['manual']);
+    });
+
+    it('should classify an smbg below the very-low threshold as `verylow` and `belowtarget`', function() {
+      expect(classifier({value: 5})).to.deep.equal(['meter', 'verylow', 'belowtarget']);
+    });
+
+    it('should classify an smbg above the very-low and below the low threshold as `belowtarget`', function() {
+      expect(classifier({value: 15})).to.deep.equal(['meter', 'belowtarget']);
+    });
+
+    it('should classify an smbg above the target threshold as `abovetarget`', function() {
+      expect(classifier({value: 35})).to.deep.equal(['meter', 'abovetarget']);
+    });
+
+    it('should classify an smbg above the high threshold as `abovetarget`', function() {
+      expect(classifier({value: 45})).to.deep.equal(['meter', 'abovetarget']);
+      expect(classifier({value: 55})).to.deep.equal(['meter', 'abovetarget']);
+    });
+  });
 });
