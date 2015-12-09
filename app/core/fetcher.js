77da77920ec49aece3d819f235e6dfe2eb2765c6
Gordon Dent <gordonmdent@gmail.com>
2015-12-09 14:43:24 -0800

    Bug fix

--- app/core/fetcher.js
+++ app/core/fetcher.js
@@ -114,7 +114,7 @@ export default class Fetcher {
         if (err.status === 404) {
           comp.props.route.log('Patient not found with id '+patientId);
           var setupMsg = (patientId === comp.state.user.userid) ? usrMessages.ERR_YOUR_ACCOUNT_NOT_CONFIGURED : usrMessages.ERR_ACCOUNT_NOT_CONFIGURED;
-          var dataStoreLink = (<Link to="#/patients/new" onClick={comp.closeNotification}>{usrMessages.YOUR_ACCOUNT_DATA_SETUP}</Link>);
+          var dataStoreLink = (<Link to="/patients/new" onClick={comp.closeNotification}>{usrMessages.YOUR_ACCOUNT_DATA_SETUP}</Link>);
           return comp.actionHandlers.handleActionableError(err, setupMsg, dataStoreLink);
         }
         // we can't deal with it so just show error handler
