11ca19bfd47331bbdaf3d3507d507b120375de01
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-01-08 17:48:21 -0800

    attempt to get integration tests running on Travis w/JavaScript to set up

---
+++ integration/setup.js
@@ -0,0 +1,32 @@
+var createTidepoolClient = require('tidepool-platform-client');
+
+var tidepool = createTidepoolClient({
+  host: 'http://localhost:8009',
+  metricsSource: 'travis',
+  metricsVersion: '0.0.0'
+});
+
+console.log();
+
+tidepool.initialize(function() {
+  console.log('API initialized.');
+  console.log();
+});
+
+tidepool.signup({
+  emails: ['jane+skip@tidepool.org'],
+  username: 'jane+skip@tidepool.org',
+  password: 'password'
+}, function(err, resp) {
+  // most (all?) errors come thru as resp
+  if (err) {
+    console.log(err);
+  }
+  console.log(resp);
+  console.log();
+  if (!resp.code) {
+    console.log('Jane Doe account created.');
+    console.log();
+  }
+  process.exit();
+});
