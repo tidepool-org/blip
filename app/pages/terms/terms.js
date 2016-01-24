29ea4461245f16ed47a9e98721c513739a0faebf
Chris <krystophv@gmail.com>
2016-01-24 00:57:54 -0500

    cleanup unused variables

--- app/pages/terms/terms.js
+++ app/pages/terms/terms.js
@@ -14,11 +14,6 @@
  * not, you can obtain one from Tidepool Project at tidepool.org.
  */
 var React = require('react');
-var _ = require('lodash');
-
-var config = require('../../config');
-
-var utils = require('../../core/utils');
 var LoginNav = require('../../components/loginnav');
 
 var Terms = React.createClass({
