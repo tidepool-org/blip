88c991050cd3e4d2b83a23b40f85d13a028da927
Chris McGee <krystophv@gmail.com>
2016-03-18 19:16:35 -0400

    removing unnecessary nesting in integration tests

--- integration/signup.js
+++ integration/signup.js
@@ -6,41 +6,38 @@
 /* global it */
 
 describe('Signup', function() {
+  before(function(client, done) {
+    done();
+  });
 
-  describe('with Nightwatch', function() {
-    before(function(client, done) {
+  after(function(client, done) {
+    client.end(function() {
       done();
     });
+  });
 
-    after(function(client, done) {
-      client.end(function() {
-        done();
-      });
-    });
+  afterEach(function(client, done) {
+    done();
+  });
 
-    afterEach(function(client, done) {
-      done();
-    });
+  beforeEach(function(client, done) {
+    done();
+  });
 
-    beforeEach(function(client, done) {
-      done();
-    });
+  it('Should show the sign up form when sign up link is clicked', function(client) {
+    client
+      .url('http://localhost:3000')
+      .expect.element('#app > div > div > div.login > div.container-nav-outer.login-nav > div > ul.nav.nav-right > li > a')
+      .to.be.present
+      .and.to.be.an('a')
+      .and.to.be.visible
+      .and.to.have.attribute('href').equals('http://localhost:3000/signup');
 
-    it('Should show the sign up form when sign up link is clicked', function(client) {
-      client
-        .url('http://localhost:3000')
-        .expect.element('#app > div > div > div.login > div.container-nav-outer.login-nav > div > ul.nav.nav-right > li > a')
-        .to.be.present
-        .and.to.be.an('a')
-        .and.to.be.visible
-        .and.to.have.attribute('href').equals('http://localhost:3000/signup');
+    client.click('#app > div > div > div.login > div.container-nav-outer.login-nav > div > ul.nav.nav-right > li > a');
 
-      client.click('#app > div > div > div.login > div.container-nav-outer.login-nav > div > ul.nav.nav-right > li > a');
-
-      client.expect.element('#fullName')
-        .to.be.present
-        .and.to.be.an('input')
-        .and.to.be.visble;
-    });
+    client.expect.element('#fullName')
+      .to.be.present
+      .and.to.be.an('input')
+      .and.to.be.visble;
   });
 });
--- integration/test1.js
+++ integration/test1.js
@@ -6,38 +6,35 @@
 /* global it */
 
 describe('Blip demo test for Mocha', function() {
+  before(function(client, done) {
+    done();
+  });
 
-  describe('with Nightwatch', function() {
-    before(function(client, done) {
+  after(function(client, done) {
+    client.end(function() {
       done();
     });
+  });
 
-    after(function(client, done) {
-      client.end(function() {
-        done();
-      });
-    });
+  afterEach(function(client, done) {
+    done();
+  });
 
-    afterEach(function(client, done) {
-      done();
-    });
+  beforeEach(function(client, done) {
+    done();
+  });
 
-    beforeEach(function(client, done) {
-      done();
-    });
+  it('uses BDD to run the simple test', function(client) {
+    client
+      .url('http://localhost:3000/')
+      .expect.element('.login-nav-tidepool-logo').to.be.present;
+  });
 
-    it('uses BDD to run the simple test', function(client) {
-      client
-        .url('http://localhost:3000/')
-        .expect.element('.login-nav-tidepool-logo').to.be.present;
-    });
+  it('can set the username', function(client) {
+    client.expect.element('#username').to.be.present;
 
-    it('can set the username', function(client) {
-      client.expect.element('#username').to.be.present;
-
-      client.setValue('#username', ['duderino'])
-        .pause(500)
-        .assert.value('#username', 'duderino');
-    });
+    client.setValue('#username', ['duderino'])
+      .pause(500)
+      .assert.value('#username', 'duderino');
   });
 });
