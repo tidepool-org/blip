4f29f8df68412c85b0a9f064933d4ce3fa60c348
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-03-22 22:38:34 -0700

    adjust language on ACCOUNT_ALREADY_EXISTS error per SK

--- app/redux/constants/errorMessages.js
+++ app/redux/constants/errorMessages.js
@@ -1,5 +1,5 @@
 export const STANDARD = 'Sorry! Something went wrong...';
 export const ALREADY_SENT_TO_EMAIL = 'Looks like you\'ve already sent an invitation to that email.'
 export const SIGNUP_ERROR = 'An error occured while signing up.';
-export const ACCOUNT_ALREADY_EXISTS = 'There is already an account using that e-mail address.';
+export const ACCOUNT_ALREADY_EXISTS = 'That e-mail address already has an account.';
 export const EMAIL_NOT_VERIFIED = 'Looks like your email address has not been verified.';
