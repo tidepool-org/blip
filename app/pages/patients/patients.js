e2bd560896bb9fc8c72eb5bd7514b9c6ef282431
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-04-08 17:56:54 -0700

    prevent metric call and fetching before default route component has mounted

--- app/pages/patients/patients.js
+++ app/pages/patients/patients.js
@@ -292,9 +292,6 @@ export let Patients = React.createClass({
   },
 
   doFetching: function(nextProps) {
-    if (this.props.trackMetric) {
-      this.props.trackMetric('Viewed Care Team List');
-    }
 
     if (!nextProps.fetchers) {
       return
@@ -304,14 +301,19 @@ export let Patients = React.createClass({
       fetcher();
     });
   },
-
-  /**
-   * Before rendering for first time
-   * begin fetching any required data
-   */
   componentWillMount: function() {
     if (this.props.clearPatientInView) {
       this.props.clearPatientInView();
+    }
+  },
+
+  /**
+   * After rendering for first time
+   * begin fetching any required data
+   */
+  componentDidMount: function() {
+    if (this.props.trackMetric) {
+      this.props.trackMetric('Viewed Care Team List');
     }
     
     this.doFetching(this.props);
