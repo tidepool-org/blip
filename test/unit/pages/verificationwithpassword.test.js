4fad690a536efc0ea9254bb2a4386569646e0d9a
Gordon Dent <gordonmdent@gmail.com>
2016-04-28 10:34:46 -0700

    Add basic unit testing for verificationwithpassword (more to come)

--- test/unit/pages/verificationwithpassword.test.js
+++ test/unit/pages/verificationwithpassword.test.js
@@ -8,11 +8,37 @@ import TestUtils from 'react-addons-test-utils';
 var assert = chai.assert;
 var expect = chai.expect;
 
-import comp, { mapStateToProps } from '../../../app/pages/verificationwithpassword/verificationwithpassword';
+import { VerificationWithPassword, mapStateToProps } from '../../../app/pages/verificationwithpassword/verificationwithpassword';
 
 describe('VerificationWithPassword', () => {
   it('should be a function', () => {
-    assert.isFunction(comp);
+    assert.isFunction(VerificationWithPassword);
+  });
+
+  describe('render', function() {
+    it('should render with 8 warnings when no props provided', function () {
+      console.error = sinon.stub();
+      var props = {};
+      var elem = React.createElement(VerificationWithPassword, props);
+      var render = TestUtils.renderIntoDocument(elem);
+      expect(console.error.callCount).to.equal(8);
+    });
+
+    it('should render without wanrings when all required props provided', function () {
+      console.error = sinon.stub();
+      var props = {
+        acknowledgeNotification: sinon.stub(),
+        api: {},
+        configuredInviteKey: 'foo',
+        signupKey: 'bar',
+        onSubmit: sinon.stub(),
+        trackMetric: sinon.stub(),
+        working: false
+      };
+      var elem = React.createElement(VerificationWithPassword, props);
+      var render = TestUtils.renderIntoDocument(elem);
+      expect(console.error.callCount).to.equal(0);
+    });
   });
 
   describe('mapStateToProps', () => {
