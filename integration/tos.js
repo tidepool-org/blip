ba04b9d0518634847487b0fb377b43483e4d8506
Gordon Dent <gordonmdent@gmail.com>
2016-04-28 11:48:21 -0700

    Update TOS integration test

--- integration/tos.js
+++ integration/tos.js
@@ -27,18 +27,18 @@ describe('Terms of Service', function() {
   it('Should show the terms on the terms route', function(client) {
     client
       .url('http://localhost:3000/terms')
-      .expect.element('#app > div > div > div.terms.js-terms > div.terms-content.terms-box > div > iframe:nth-child(2)')
+      .expect.element('#app > div > div > div.terms.js-terms > div.terms-content.terms-box > div > .iframe-holder > iframe.terms-iframe-terms')
       .to.be.present.before(1000)
       .and.to.be.an('iframe')
       .and.to.be.visible
-      .and.to.have.attribute('src').equals('https://tidepool.org/terms-of-use/');
+      .and.to.have.attribute('src').equals('https://tidepool.org/terms-of-use-summary');
   });
 
   it('Should show the privacy policy', function(client) {
-    client.expect.element('#app > div > div > div.terms.js-terms > div.terms-content.terms-box > div > iframe:nth-child(4)')
+    client.expect.element('#app > div > div > div.terms.js-terms > div.terms-content.terms-box > div > .iframe-holder > iframe.terms-iframe-privacy')
       .to.be.present.before(1000)
       .and.to.be.an('iframe')
       .and.to.be.visible
-      .and.to.have.attribute('src').equals('https://tidepool.org/privacy-policy/');
+      .and.to.have.attribute('src').equals('https://tidepool.org/privacy-policy-summary');
   });
 });
