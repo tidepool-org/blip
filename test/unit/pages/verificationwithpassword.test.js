226cd1a14527d7e467f2716c278e0f5b81e24383
Gordon Dent <gordonmdent@gmail.com>
2016-04-29 11:15:42 -0700

    Further testing of verificationwithpassword component

--- test/unit/pages/verificationwithpassword.test.js
+++ test/unit/pages/verificationwithpassword.test.js
@@ -18,15 +18,15 @@ describe('VerificationWithPassword', () => {
   describe('render', function() {
     it('should render with 8 warnings when no props provided', function () {
       console.error = sinon.stub();
-      var props = {};
-      var elem = React.createElement(VerificationWithPassword, props);
-      var render = TestUtils.renderIntoDocument(elem);
+      let props = {};
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
       expect(console.error.callCount).to.equal(8);
     });
 
     it('should render without wanrings when all required props provided', function () {
       console.error = sinon.stub();
-      var props = {
+      let props = {
         acknowledgeNotification: sinon.stub(),
         api: {},
         configuredInviteKey: 'foo',
@@ -35,9 +35,139 @@ describe('VerificationWithPassword', () => {
         trackMetric: sinon.stub(),
         working: false
       };
-      var elem = React.createElement(VerificationWithPassword, props);
-      var render = TestUtils.renderIntoDocument(elem);
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
       expect(console.error.callCount).to.equal(0);
+    });
+  });
+
+  describe('getInitialState', () => {
+    it('should return an Object that matches expectedInitialState', () => {
+      let props = {
+        acknowledgeNotification: sinon.stub(),
+        api: {},
+        configuredInviteKey: 'foo',
+        signupKey: 'bar',
+        onSubmit: sinon.stub(),
+        trackMetric: sinon.stub(),
+        working: false
+      }
+      let expectedInitialState = {
+        loading: true,
+        formValues: {},
+        validationErrors: {},
+        notification: null
+      };
+
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
+      expect(console.error.callCount).to.equal(0);
+
+      expect(render.getInitialState()).to.eql(expectedInitialState);
+    });
+
+    it('should return an Object that matches expectedInitialState with inviteEmail when set in props', () => {
+      let props = {
+        acknowledgeNotification: sinon.stub(),
+        api: {},
+        configuredInviteKey: 'foo',
+        signupKey: 'bar',
+        onSubmit: sinon.stub(),
+        trackMetric: sinon.stub(),
+        working: false,
+        inviteEmail: 'bill@ted.com'
+      }
+      let expectedInitialState = {
+        loading: true,
+        formValues: {
+          username: props.inviteEmail
+        },
+        validationErrors: {},
+        notification: null
+      };
+
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
+      expect(console.error.callCount).to.equal(0);
+
+      expect(render.getInitialState()).to.eql(expectedInitialState);
+    });
+  });
+
+
+  describe('handleInputChange', () => {
+    it('should update formValues in state with changed value', () => {
+      let props = {
+        acknowledgeNotification: sinon.stub(),
+        api: {},
+        configuredInviteKey: 'foo',
+        signupKey: 'bar',
+        onSubmit: sinon.stub(),
+        trackMetric: sinon.stub(),
+        working: false,
+        inviteEmail: 'bill@ted.com'
+      }
+      let expectedInitialState = {
+        loading: true,
+        formValues: {
+          username: props.inviteEmail
+        },
+        validationErrors: {},
+        notification: null
+      };
+
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
+
+      expect(render.getInitialState()).to.eql(expectedInitialState);
+
+      render.handleInputChange({name: 'username', value: 'dean@frank.co.uk'});
+
+      expect(render.state.formValues.username).to.equal('dean@frank.co.uk');
+    });
+  });
+
+  describe('resetFormStateBeforeSubmit', () => {
+    it('should update state ith supplied formValues and empty validation errors and notification', () => {
+      let props = {
+        acknowledgeNotification: sinon.stub(),
+        api: {},
+        configuredInviteKey: 'foo',
+        signupKey: 'bar',
+        onSubmit: sinon.stub(),
+        trackMetric: sinon.stub(),
+        working: false,
+        inviteEmail: 'bill@ted.com'
+      }
+      let expectedInitialState = {
+        loading: true,
+        formValues: {
+          username: props.inviteEmail
+        },
+        validationErrors: {},
+        notification: null
+      };
+
+      let elem = React.createElement(VerificationWithPassword, props);
+      let render = TestUtils.renderIntoDocument(elem);
+
+      let intermediateState = {
+        validationErrors: {
+          username: 'This is not valid'
+        }
+      };
+      render.setState(intermediateState);
+      
+      expect(render.state.validationErrors).to.eql(intermediateState.validationErrors);
+
+      render.resetFormStateBeforeSubmit({});
+
+      expect(render.state).to.eql({
+        loading: false,
+        formValues: {},
+        validationErrors: {},
+        notification: null
+      });
     });
   });
 
