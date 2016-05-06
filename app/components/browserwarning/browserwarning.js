c1be1abc69c692fd0db561d5e6e3e7e0718bf3bf
Gordon Dent <gordonmdent@gmail.com>
2016-05-06 13:07:43 -0700

    Add in graceful degradation when working with Safari!

--- app/components/browserwarning/browserwarning.js
+++ app/components/browserwarning/browserwarning.js
@@ -17,10 +17,27 @@ import React, { Component } from 'react'
 
 import utils from '../../core/utils';
 
+const COPY_STATUS_NULL = 0;
+const COPY_STATUS_SUCCESS = 10;
+const COPY_STATUS_FAIL = 20;
+
 export default class BrowserWarning extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copyStatus: COPY_STATUS_NULL
+    };
+  }
   render() {
     var self = this;
     var downloadCopy = <div className="browser-warning-chrome-image"></div>;
+    var copyButton = <button className="btn browser-warning-copy-button" onClick={() => self.copyText()}>Copy link</button>;
+
+    if (this.state.copyStatus === COPY_STATUS_SUCCESS) {
+      copyButton = <button className="btn browser-warning-copy-button" onClick={() => self.copyText()}>Copied!</button>
+    } else if (this.state.copyStatus === COPY_STATUS_FAIL) {
+      copyButton = <button className="btn browser-warning-copy-button" onClick={() => self.copyText()}>Please press Ctrl + C now</button>
+    }
 
     if (!utils.isMobile()) {
       downloadCopy = (<div>
@@ -32,11 +49,10 @@ export default class BrowserWarning extends Component {
           <input type="text" className="blip-link-text" value="blip.tidepool.org"></input>
           <span className="dark-text">into Chrome.</span>
         </div>
-        <button className="btn browser-warning-copy-button" onClick={() => self.copyText()}>Copy link</button>
+        {copyButton}
         <div className="browser-warning-download-text">Or download Chrome <a href="https://www.google.com/intl/en/chrome/browser/desktop/index.html" target="_blank">here</a></div>
       </div>);
     }
-
     
     return (
       <div className="browser-warning js-terms">
@@ -54,8 +70,14 @@ export default class BrowserWarning extends Component {
 
     try {
       var copyCmd = document.execCommand('copy');
+      this.setState( {
+        copyStatus: (copyCmd) ? COPY_STATUS_SUCCESS : COPY_STATUS_FAIL
+      });
     } catch (err) {
       console.log('Unable to copy - unsupported browser');
+      this.setState({
+        copyStatus: COPY_STATUS_FAIL
+      });
     }
   }
 }
