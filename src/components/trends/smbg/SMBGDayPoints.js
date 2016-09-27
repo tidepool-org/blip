6ee90779ed9873f835ff1bd671ea79e98d50c025
Jamie <jamie@tidepool.org>
2016-09-28 11:36:30 +1300

    wip

--- src/components/trends/smbg/SMBGDayPoints.js
+++ src/components/trends/smbg/SMBGDayPoints.js
@@ -20,3 +20,50 @@
 // for focusing that particular smbg
 // when an smbg is focused, it's circle gets bigger
 // and so do the circles for every smbg in the day and the line too (see notes in SMBGDayLine)
+
+import React, { PropTypes } from 'react';
+
+import { findClassForValue } from '../../../utils/bgBoundary';
+
+import styles from './SMBGDayPoints.css';
+
+const SMBGDayPoints = (props) => {
+  const { datum } = props;
+  if (!datum) {
+    return null;
+  }
+
+  const { radius, xScale, yScale } = props;
+
+  return (
+    <g id={`cbgSlice-${datum.id}`}>
+      {
+        <circle
+          className={findClassForValue(datum.value)}
+          key={`datum-${datum.id}`}
+          id={`datum-${datum.id}`}
+          cx={xScale(datum.msX)}
+          cy={yScale(datum.value)}
+          r={radius}
+        />,
+      }
+    </g>
+  );
+};
+
+SMBGDayPoints.defaultProps = {
+  radius: 7,
+};
+
+SMBGDayPoints.propTypes = {
+	data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    msX: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  })).isRequired,
+	xScale: PropTypes.func.isRequired,
+	yScale: PropTypes.func.isRequired,
+	radius: PropTypes.number.isRequired,
+};
+
+export default SMBGDayPoints;
---
+++ src/utils/trends/bgBoundary.js
@@ -0,0 +1,42 @@
+/*
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2016, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
+export function findClassForValue(bgValue){
+  const classes = {
+    'very-low': {boundary: 60},
+    low: {boundary: 80},
+    target: {boundary: 180},
+    high: {boundary: 200},
+    'very-high': {boundary: 300},
+  };
+
+  if (bgValue < classes['very-low'].boundary) {
+    return 'bg-low';
+  }
+  else if ((bgValue >= classes['very-low'].boundary) && (bgValue < classes.low.boundary)) {
+    return 'bg-low circle-open';
+  }
+  else if ((bgValue >= classes.low.boundary) && (bgValue <= classes.target.boundary)) {
+    return 'bg-target';
+  }
+  else if ((bgValue > classes.target.boundary) && (bgValue <= classes.high.boundary)) {
+    return 'bg-high circle-open';
+  }
+  else if (bgValue > classes.high.boundary) {
+    return 'bg-high';
+  }
+}
