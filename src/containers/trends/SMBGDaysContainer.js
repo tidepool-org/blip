66a256aefb2d1644cb8e7051202dc7b4e36c80c7
Jamie <jamie@tidepool.org>
2016-10-17 14:25:48 +1300

    cleanup

--- src/containers/trends/SMBGDaysContainer.js
+++ src/containers/trends/SMBGDaysContainer.js
@@ -15,25 +15,6 @@
  * == BSD2 LICENSE ==
  */
 
-// TODO: this component should roughly follow the model of the other animation containers
-// but since there really isn't any data munging needed (at least not that jebeck can think of now)
-// it can probably be a pure functional component that just recalculates xPositions every render
-// depending on whether smbgGrouped is true or false
-
-// it is the xPositions for grouping/ungrouping that will be animated via this container
-// animating the lines (if turned on) based on grouping/ungrouping is a tougher problem
-// because the number of line segments can vary, so we could experiment with it, but it may
-// not be possible. if it isn't, it won't be a regression in functionality compared with
-// tideline trends view, which also does *not* animate the lines on grouping/ungrouping change
-
-// components to be rendered by this container:
-// - SMBGDayPoints
-//    + renders a circle for each individual smbg in the day
-//    + attaches hover handler for focusing a single smbg
-// - SMBGDayLine
-//    + render a line connecting smbgs in the day
-//    + attaches hover handler for focusing the day of smbgs
-
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
 
