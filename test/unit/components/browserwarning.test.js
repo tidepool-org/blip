8d68507d2a8551a8b79d3dd57e58eed25efea100
Hunter Dougless <git@hntrdglss.com>
2016-10-24 18:47:06 -0400

    Add unit test for displaying BrowserWarning metric

--- test/unit/components/browserwarning.test.js
+++ test/unit/components/browserwarning.test.js
@@ -23,5 +23,16 @@ describe('BrowserWarning', function () {
       var elem = TestUtils.renderIntoDocument(browserWarningElem);
       expect(elem).to.be.ok;
     });
+
+    it('should fire metric when mounted/rendered', function() {
+      var props = {
+        trackMetric: sinon.stub()
+      };
+      var browserWarningElem = React.createElement(BrowserWarning, props);
+      var elem = TestUtils.renderIntoDocument(browserWarningElem);
+      expect(elem).to.be.ok;
+      expect(props.trackMetric.callCount).to.equal(1);
+      expect(props.trackMetric.calledWith('Chrome Required - Screen Displayed')).to.be.true;
+    });
   });
 });
