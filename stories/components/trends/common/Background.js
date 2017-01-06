bf5530e40d79c6654c3ca00a72f3765912625b74
Jana E. Beck <jana.eliz.beck@gmail.com>
2017-01-06 11:41:20 -0800

    sweeeeet the linter pointed out that I needed to update the React Storybook stuff too

--- stories/components/trends/common/Background.js
+++ stories/components/trends/common/Background.js
@@ -4,17 +4,11 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 
 // eslint-disable-next-line max-len
-import BackgroundWithTargetRange from '../../../../src/components/trends/common/BackgroundWithTargetRange';
+import Background from '../../../../src/components/trends/common/Background';
 
 const w = 800;
 const h = 450;
 const props = {
-  bgBounds: {
-    veryHighThreshold: 300,
-    targetUpperBound: 180,
-    targetLowerBound: 80,
-    veryLowThreshold: 60,
-  },
   margins: {
     left: 0,
     right: 0,
@@ -26,17 +20,16 @@ const props = {
     height: h,
   },
   xScale: scaleLinear().domain([0, 864e5]).range([0, w]),
-  yScale: scaleLinear().domain([27, 400]).range([h, 0]),
 };
 
-storiesOf('BackgroundWithTargetRange', module)
+storiesOf('Background', module)
   .add('without lines', () => (
     <svg width={w} height={h}>
-      <BackgroundWithTargetRange {...props} />
+      <Background {...props} />
     </svg>
   ))
   .add('with lines at three-hour intervals', () => (
     <svg width={w} height={h}>
-      <BackgroundWithTargetRange {...props} linesAtThreeHrs />
+      <Background {...props} linesAtThreeHrs />
     </svg>
   ));
