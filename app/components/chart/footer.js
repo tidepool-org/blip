5ed906ad4fa09232df34cd741b0eb18e7e1cf3cb
Jana E. Beck <jana.eliz.beck@gmail.com>
2017-02-13 16:52:49 -0800

    Revert "change 'Range & Average' to just 'Range' in footer checkboxes"
    This reverts commit 72395fa4eaada1e0c073d435e5ea9370055568cc.

--- app/components/chart/footer.js
+++ app/components/chart/footer.js
@@ -65,7 +65,7 @@ var TidelineFooter = React.createClass({
         <label htmlFor="overlayCheckbox">
           <input type="checkbox" name="overlayCheckbox" id="overlayCheckbox"
             checked={this.props.boxOverlay}
-            onChange={this.props.onClickBoxOverlay} /> Range
+            onChange={this.props.onClickBoxOverlay} /> Range &amp; Average
         </label>
 
         <label htmlFor="groupCheckbox">
