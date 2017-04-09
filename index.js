5e06a0efd428af2d006ec222555e59518d9d821b
Jamie <jamie@tidepool.org>
2017-04-10 08:08:57 +1200

    All users users (#75)
    * addition of endpoint for unified user api
    
    * rename method
    
    * v0.30.0

--- index.js
+++ index.js
@@ -295,6 +295,26 @@ module.exports = function (config, deps) {
       );
     },
     /**
+     * Get the users 'patients' and their associated data
+     * includes profile, permissions and metadata
+     *
+     * @param {String} userId id of the user
+     * @param cb
+     * @returns {cb}  cb(err, response)
+     */
+    getAssociatedUsersDetails: function (userId, cb) {
+      if (userId == null) {
+        return cb({ status : common.STATUS_BAD_REQUEST,  message: 'Must specify a userId' });
+      }
+      common.assertArgumentsSize(arguments, 2);
+
+      common.doGetWithToken(
+        '/metadata/users/' + userId + '/users',
+        { 200: function(res){ return res.body; }, 404: null },
+        cb
+      );
+    },
+    /**
      * Get the users 'patients' to whom they can upload for.
      *
      * @param {String} userId of the user
