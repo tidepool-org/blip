9e932e52ab5b1c3cc338a402ede19f7a95e29c10
Darin Krauss <darin@tidepool.org>
2017-04-20 17:20:02 -0700

    Create deploy artifacts via Travis; update start script to use node version

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #! /bin/bash -eu
 
+source "${NVM_DIR}/nvm.sh"
+source "deploy-env.sh"
+
+nvm ls "${DEPLOY_NODE_VERSION}" > /dev/null || { echo "ERROR: Node version ${DEPLOY_NODE_VERSION} not installed"; exit 1; }
+nvm use --delete-prefix "${DEPLOY_NODE_VERSION}"
+
 . config/env.sh
 npm run build-config
 exec node server
