38b22bf251f28cd45aac540e8ab4381f86f86129
Clint Beacock <clint.beacock@gmail.com>
2017-06-01 12:26:47 -0400

    Webpack minification for production builds

--- buildapp.js
+++ buildapp.js
@@ -12,7 +12,7 @@ mkdir('-p', 'dist');
 var entry = './app/main.prod.js';
 
 console.log('Building app from "' + entry + '"...');
-exec('webpack --entry \'' + entry + '\' --output-filename \'bundle.[hash].js\' --devtool source-map --colors --progress');
+exec('webpack --entry \'' + entry + '\' --output-filename \'bundle.[hash].js\' --devtool source-map --colors --progress --optimize-minimize');
 
 var end = new Date();
 console.log('App built in ' + ms(end - start));
