d98e0461bd53b745b20211c8901d419c195e44d2
Clint Beacock <clint.beacock@gmail.com>
2018-03-15 16:34:29 -0400

    Fix bolus tooltips positioning issues
     - Initial position was being incorrectly calculated due to parent
    container being now relatively positioned.
     - Position was going awry when window was scrolled in the y position

--- js/plot/quickbolus.js
+++ js/plot/quickbolus.js
@@ -1,15 +1,15 @@
 /*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -95,7 +95,11 @@ module.exports = function(pool, opts) {
       // tooltips
       selection.selectAll('.d3-bolus-group').on('mouseover', function(d) {
         highlight.on(d3.select(this));
-        drawBolus.tooltip.add(d, this.getBoundingClientRect());
+        var parentContainer = document.getElementsByClassName('patient-data')[0].getBoundingClientRect();
+        var container = this.getBoundingClientRect();
+        container.y = container.top - parentContainer.top;
+
+        drawBolus.tooltip.add(d, container);
       });
       selection.selectAll('.d3-bolus-group').on('mouseout', function(d) {
         highlight.off();
