cd67d3a1a3cad3492b54506364a06813c9ebb1d0
coyotte508 <coyotte508@gmail.com>
2018-04-25 18:42:33 +0200

    Lock down package versions & fix loadtests

--- loadtests.js
+++ loadtests.js
@@ -1,5 +1,5 @@
 require('babel-polyfill');
 
-const context = require.context('./test', true, /PrintView\.test\.js$/); // Load .js files in /test
+const context = require.context('./test', true, /\.js$/); // Load .js files in /test
 // eslint-disable-next-line lodash/prefer-lodash-method
 context.keys().forEach(context);
