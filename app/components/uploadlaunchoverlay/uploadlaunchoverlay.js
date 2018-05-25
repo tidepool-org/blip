d0cc4afea3ba550f69855db2304d0ffe6afb02f5
Courtenay Huffman <courtenay.huffman@gmail.com>
2018-05-25 16:07:03 -0700

    Fix unit test error

--- app/components/uploadlaunchoverlay/uploadlaunchoverlay.js
+++ app/components/uploadlaunchoverlay/uploadlaunchoverlay.js
@@ -79,9 +79,7 @@ class UploadLaunchOverlay extends Component {
               <img src={logoSrc} />
             </div>
             <div>
-              <div className=uploadDismiss>
-                <a className='uploadDismiss' onClick={this.props.modalDismissHandler}>&times;</a>
-              </div>
+              <a className='uploadDismiss' onClick={this.props.modalDismissHandler}>&times;</a>
               <div className='UploadLaunchOverlay-title'>Launching Uploader</div>
               <div className='UploadLaunchOverlay-text'>If you don't yet have the Tidepool Uploader, please install the appropriate version below</div>
             </div>
