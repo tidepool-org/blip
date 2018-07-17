d43887f6ef12c38f96f2a5f96ff7e9c9069525fc
Clint Beacock <clint.beacock@gmail.com>
2018-07-17 10:49:36 -0400

    Upgrade webpack to latest v2 to allow running new storybook

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,71 @@
-const format = require('util').format;
 const path = require('path');
-const calc = require('postcss-calc');
-const cssVariables = require('postcss-custom-properties');
+const webpack = require('webpack');
 
-const cssModules = process.env.NODE_ENV === 'test' ?
-  // Enzyme as of v2.4.1 has trouble with classes
-  // that do not start and *end* with an alpha character
-  // but that will sometimes happen with the base64 hashes
-  // so we leave them off in the test env
-  'modules&localIdentName=[name]--[local]' :
-  'modules&localIdentName=[name]--[local]--[hash:base64:5]';
-const importLoaders = 'importLoaders=1';
+const appDirectory = path.resolve(__dirname);
+
+// eslint-disable-next-line no-underscore-dangle
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+// Enzyme as of v2.4.1 has trouble with classes
+// that do not start and *end* with an alpha character
+// but that will sometimes happen with the base64 hashes
+// so we leave them off in the test env
+const localIdentName = process.env.NODE_ENV === 'test'
+  ? '[name]--[local]'
+  : '[name]--[local]--[hash:base64:5]';
+
+// This is needed for webpack to compile JavaScript.
+// Many OSS React Native packages are not compiled to ES5 before being
+// published. If you depend on uncompiled packages they may cause webpack build
+// errors. To fix this webpack can be configured to compile to the necessary
+// `node_module`.
+const babelLoaderConfiguration = {
+  test: /\.js$/,
+  // Add every directory that needs to be compiled by Babel during the build
+  include: [
+    path.resolve(appDirectory, 'src'),
+  ],
+
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      plugins: [
+        ['transform-runtime', {
+          helpers: false,
+          polyfill: false,
+          regenerator: true,
+          moduleName: 'babel-runtime',
+        }],
+      ],
+    },
+  },
+};
+
+// This is needed for webpack to import static images in JavaScript files
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
 
 module.exports = {
-  debug: true,
+  plugins: [
+    // `process.env.NODE_ENV === 'production'` must be `true` for production
+    // builds to eliminate development checks and reduce build size. You may
+    // wish to include additional optimizations.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      __DEV__,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
+  ],
   devtool: 'sourcemap',
   entry: {
     index: [path.join(__dirname, '/src/index')],
@@ -25,30 +77,40 @@ module.exports = {
   },
   resolve: {
     extensions: [
-      '',
       '.js',
     ],
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.css$/,
-        loader: format('style-loader!css-loader?%s&%s!postcss-loader', importLoaders, cssModules),
+        test: /\.(css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader?sourceMap',
+            query: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName,
+            },
+          },
+          'postcss-loader?sourceMap',
+        ],
       },
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
       {
-        test: /\.js$/,
-        exclude: path.join(__dirname, 'node_modules'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=25000&mimetype=image/png',
+        test: /\.ttf$/,
+        use: [
+          {
+            loader: 'url-loader',
+            query: {
+              limit: 25000,
+              mimetype: 'application/octet-stream',
+            },
+          },
+        ],
       },
     ],
-    postcss: [calc, cssVariables],
   },
 };
