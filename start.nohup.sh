c4fb66f7cf9c8b9fbd6cac975dcc3a89f74d0daf
Franck Fourel <franck@diabeloop.fr>
2018-07-31 09:34:31 +0200

    Add startup script (#13)
    * Use nohup in the startup script.
    
    * add input parameter for service name

---
+++ start.nohup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+
+. "${NVM_DIR}/nvm.sh"
+. version.sh
+
+service=${1:-'blip'}
+
+nvm ls "${START_NODE_VERSION}" > /dev/null || { echo "ERROR: Node version ${START_NODE_VERSION} not installed"; exit 1; }
+nvm use --delete-prefix "${START_NODE_VERSION}"
+
+. config/env.sh
+
+npm run build-config
+nohup node server > ../$service.log 2> ../$service.error.log <&- &
