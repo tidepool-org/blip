d4e6fe7fe4417aa81ae6effca9e6b743d5b426c9
Yvon TANGUY <yvon@sentinellesduweb.com>
2018-07-31 18:09:12 +0200

    PR review: Fix wrong default values for local env var

--- config/local.sh
+++ config/local.sh
@@ -1,4 +1,4 @@
 export API_HOST='http://localhost:8009'
-export I18N_ENABLED=true
-export ALLOW_CHANGE_EMAIL=false
-export ALLOW_CHANGE_PASSWORD=false
+export I18N_ENABLED=false
+export ALLOW_CHANGE_EMAIL=true
+export ALLOW_CHANGE_PASSWORD=true
