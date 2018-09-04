a51f76c9ef2a519b1adaeb988d9ad51db3516b21
franck <franck@diabeloop.fr>
2018-09-04 09:45:41 +0200

    issue #31 related to travis build failing after resync with tidepool upstream

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-wget -q -O artifact_node.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact_node.sh'
+wget -q -O artifact_node.sh 'https://raw.githubusercontent.com/mdblp/tools/dbl/artifact/artifact_node.sh'
 chmod +x artifact_node.sh
 
 . ./version.sh
