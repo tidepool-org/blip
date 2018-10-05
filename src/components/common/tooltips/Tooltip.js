fe23e4964e2345afbf92462411bf053d9e166cf9
Clint Beacock <clint.beacock@gmail.com>
2018-10-05 12:15:39 -0400

    Recalculate tooltip positions after browser reflow is complete

--- src/components/common/tooltips/Tooltip.js
+++ src/components/common/tooltips/Tooltip.js
@@ -30,6 +30,12 @@ class Tooltip extends PureComponent {
     this.calculateOffset(this.props);
   }
 
+  componentDidUpdate() {
+    // In cases where the tooltip width is not statically set, we may need to re-caculate
+    // the offset after updates to get the proper positioning after browser reflow is complete.
+    this.calculateOffset(this.props);
+  }
+
   componentWillReceiveProps(nextProps) {
     this.calculateOffset(nextProps);
   }
@@ -76,7 +82,11 @@ class Tooltip extends PureComponent {
       offset.left = leftOffset + horizontalOffset;
     }
 
-    this.setState({ offset });
+    // We only update the offset state when a change is required.  Otherwise, we'd end up in an
+    // infinite render loop since this method is invoked from componentDidUpdate
+    if (!_.isEqual(offset, this.state.offset)) {
+      this.setState({ offset });
+    }
   }
 
   renderTail(backgroundColor = 'white') {
