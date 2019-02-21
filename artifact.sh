1bd3ca14ab07aedecc7c22559bebbdf3fa605b0a
franck <franck@diabeloop.fr>
2019-02-21 10:26:19 +0100

    remove the prepublish and add traces to understand why package is almost empty

--- artifact.sh
+++ artifact.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -eu
+set -ev
+
+echo 'before test'
+npm test
 
 if [ "${TRAVIS_NODE_VERSION}" != "6.10.2" ]; then
     exit 0
 fi
 
-npm publish -f
+echo 'after test/before build'
+ls -la
+npm run build
+echo 'after build'
+ls -la
+npm publish
+echo 'befor publish'
+ls -la
 
 if [ -n "${TRAVIS_TAG:-}" ]; then
     npm pack
