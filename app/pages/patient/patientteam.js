3cfeb7f2f1dbd1e799aa219065b4daed9dc0c3c6
courtenayhuffman <courtenay.huffman@gmail.com>
2019-04-23 20:27:01 -0700

    strip extra space in share email fields

--- app/pages/patient/patientteam.js
+++ app/pages/patient/patientteam.js
@@ -131,7 +131,7 @@ var MemberInviteForm = translate()(React.createClass({
 
     var validateEmail = function(email) {
       var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+      return re.test(email.trim());
     };
 
     if (!validateEmail(email)) {
