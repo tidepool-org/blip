a35b51db2cf980a48694b43a6ec0adde85454b3c
Derrick Burns <derrickrburns@gmail.com>
2019-05-08 10:51:50 -0700

    Use artifact.sh

--- artifact.sh
+++ artifact.sh
@@ -3,4 +3,4 @@
 wget -q -O artifact_none.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact.sh'
 chmod +x artifact_none.sh
 
-./artifact_none.sh
+./artifact_none.sh none
