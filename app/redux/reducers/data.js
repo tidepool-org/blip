4b3bbe70859e221df624ccffd0621a6fc3075044
Clint Beacock <clint.beacock@gmail.com>
2019-10-08 11:25:29 -0400

    Always fully replace the data state when a query result comes in

--- app/redux/reducers/data.js
+++ app/redux/reducers/data.js
@@ -5,7 +5,7 @@ import * as actionTypes from '../constants/actionTypes';
 const data = (state = {}, action) => {
   switch (action.type) {
     case actionTypes.DATA_WORKER_QUERY_DATA_SUCCESS:
-      return update(state, { $merge: action.payload.data });
+      return update(state, { $set: action.payload.data });
 
     case actionTypes.DATA_WORKER_REMOVE_DATA_SUCCESS:
       return update(state, { $set: {} });
