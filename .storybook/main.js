a921d5c202705e0bc8611e096be248f8e45a2f2d
Thacher <thacher@tidepool.org>
2020-02-04 16:25:27 -0800

    Revert "added storybook, rebass, styled-components (used v3.4.10 rather than"
    This reverts commit 899f372f43da5fefe0fa62a2269a42b7db6aa729.

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', 'storybook-addon-designs'],
-  webpackFinal: async config => {
-    // do mutation to the config
-
-    return config;
-  },
-};
