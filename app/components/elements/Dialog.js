a74ca8b9ebefda3d2045df30ab04376f3960cfda
Clint Beacock <clint.beacock@gmail.com>
2020-03-04 16:41:29 -0500

    [WEB-589] WIP: early experimenting with Dialog component

--- app/components/elements/Dialog.js
+++ app/components/elements/Dialog.js
@@ -1,98 +1,154 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
-import MuiDialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
+import MuiDialog, { DialogProps } from '@material-ui/core/Dialog';
+import MuiDialogContent, { DialogContentProps } from '@material-ui/core/DialogContent';
+import MuiDialogActions, { DialogActionsProps } from '@material-ui/core/DialogActions';
 import styled from 'styled-components';
-import noop from 'lodash/noop';
 import { Flex } from 'rebass/styled-components';
 
 import Button from './Button';
-import { IconButton } from './InputStyles';
-import { Title, Body1 } from './FontStyles';
+import { IconButton } from './IconButton';
 
 import {
-  colors,
-  fontSizes,
-  radii,
-  shadows,
   space,
 } from '../../themes/baseTheme';
 
-const StyledDialog = styled(MuiDialog)`
-`;
-
-const DialogTitle = styled(Flex)`
+const StyledDialogTitle = styled(Flex)`
   justify-content: space-between;
   padding: ${space[3]}px;
 `;
 
-const DialogContent = styled(MuiDialogContent)`
+export const DialogTitle = props => {
+  const {
+    children,
+    closeIcon,
+    onClose,
+    ...dialogTitleProps
+  } = props;
+
+  return (
+    <StyledDialogTitle {...dialogTitleProps}>
+      {children}
+      {closeIcon && <IconButton label="clear dates" onClick={onClose} icon={CloseRoundedIcon} />}
+    </StyledDialogTitle>
+  );
+};
+
+DialogTitle.propTypes = {
+  closeIcon: PropTypes.bool,
+};
+
+DialogTitle.defaultProps = {
+  closeIcon: true,
+};
+
+const StyledDialogContent = styled(MuiDialogContent)`
 `;
 
-const DialogActions = styled(MuiDialogActions)`
+export const DialogContent = props => <StyledDialogContent {...props} />;
+
+DialogContent.propTypes = {
+  ...DialogContentProps,
+};
+
+DialogContent.defaultProps = {
+  dividers: true,
+};
+
+const StyledDialogActions = styled(MuiDialogActions)`
+`;
+
+export const DialogActions = props => <StyledDialogActions {...props} />;
+
+DialogActions.propTypes = {
+  ...DialogActionsProps,
+};
+
+DialogActions.defaultProps = {
+  dividers: true,
+};
+
+const StyledDialog = styled(MuiDialog)`
 `;
 
 export const Dialog = props => {
-  const [open, setOpen] = useState(props.initialOpen);
+  const {
+    initialOpen,
+    id,
+    onClose,
+    onSubmit,
+    titleElement: Title,
+    titleCloseIcon,
+    triggerElement: Trigger,
+    children,
+    ...dialogProps
+  } = props;
 
-  const handleClose = () => props.onClose && setOpen(false);
-  const handleSubmit = () => props.onSubmit && setOpen(false);
+  const [open, setOpen] = useState(initialOpen);
+
+  const handleClickOpen = () => setOpen(true);
+
+  const handleClose = () => {
+    setOpen(false);
+    if (onClose) onClose();
+  };
+
+  const handleSubmit = () => {
+    setOpen(false);
+    if (onSubmit) onSubmit();
+  };
+
+  const titleId = Title ? `${id}-title` : undefined;
 
   return (
     <div>
-      <StyledDialog onClose={handleClose} aria-labelledby={`${props.id}-title`} open={open}>
-        <DialogTitle>
-          <Title id={`${props.id}-title`}>
-            {props.title}
-          </Title>
-          {props.titleCloseIcon ? (
-            <IconButton aria-label="close" onClick={handleClose}>
-              <CloseRoundedIcon />
-            </IconButton>
-          ) : null}
-        </DialogTitle>
-        <DialogContent dividers>
-          <Body1>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          </Body1>
-          <Body1>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Body1>
-          <Body1>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-            auctor fringilla.
-          </Body1>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
+      <Trigger onClick={handleClickOpen} />
+
+      <StyledDialog
+        {...dialogProps}
+        onClose={handleClose}
+        aria-labelledby={titleId}
+        open={open}
+      >
+        {Title ? (
+          <StyledDialogTitle disableTypography>
+            <Title id={titleId} />
+            {titleCloseIcon ? (
+              <IconButton label="clear dates" onClick={handleClose} icon={CloseRoundedIcon} />
+            ) : null}
+          </StyledDialogTitle>
+        ) : null}
+        <StyledDialogContent dividers>
+          {children}
+        </StyledDialogContent>
+        <StyledDialogActions>
+          <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button autoFocus onClick={handleSubmit} color="primary">
+          <Button variant="primary" onClick={handleSubmit}>
             Confirm
           </Button>
-        </DialogActions>
+        </StyledDialogActions>
       </StyledDialog>
     </div>
   );
 };
 
 Dialog.propTypes = {
+  ...DialogProps,
   initialOpen: PropTypes.bool,
-  onClose: PropTypes.func,
   id: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
+  onSubmit: PropTypes.func,
+  title: PropTypes.string,
+  titleCloseIcon: PropTypes.bool,
+  trigger: PropTypes.elementType,
 };
 
 Dialog.defaultProps = {
-  initialOpen: true,
-  onClose: noop,
+  initialOpen: false,
   titleCloseIcon: true,
-  title: ' ',
 };
 
 export default Dialog;
--- stories/Dialog.stories.js
+++ stories/Dialog.stories.js
@@ -1,47 +1,74 @@
 import React from 'react';
 
 import { withDesign } from 'storybook-addon-designs';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
 
+import baseTheme from '../app/themes/baseTheme';
 import Dialog from '../app/components/elements/Dialog';
+import Button from '../app/components/elements/Button';
+import { Body1, Title } from '../app/components/elements/FontStyles';
 
 // This silly decorator allows the components to properly re-render when knob values are changed
-const withWrapper = Story => <Story />;
-
-const getOpen = (open = true) => boolean('Initially Open', open);
-
-const setOpen = open => {
-  const isOpen = getOpen()
-}
-
-const withOpenButton = Story => (
-  <React.Fragment>
-    <button disabled={getOpen()} onClick={setOpen(true)}>
-      Open Dialog
-    </button>
+const withTheme = Story => (
+  <ThemeProvider theme={baseTheme}>
     <Story />
-  </React.Fragment>
+  </ThemeProvider>
 );
 
 export default {
   title: 'Dialogs',
-  decorators: [withDesign, withKnobs, withOpenButton],
+  decorators: [withDesign, withKnobs, withTheme],
 };
 
 export const DialogStory = () => {
-  return <Dialog
-    id="confirmDialog"
-    initialOpen={getOpen()}
-    title="Hello"
-  />;
+  const getInitialOpen = () => boolean('Initially Open', true);
+  const getTitle = () => text('Dialog Title', 'Dialog Title');
+
+
+  const TriggerElement = props => (
+    <Button variant="primary" {...props}>
+      Open Dialog
+    </Button>
+  );
+
+  const TitleElement = props => (
+    <Title {...props}>{unescape(getTitle())}</Title>
+  );
+
+  return (
+    <Dialog
+      id="confirmDialog"
+      initialOpen={getInitialOpen()}
+      titleElement={TitleElement}
+      triggerElement={TriggerElement}
+      onSubmit={() => console.log('Submitted')}
+      onClose={() => console.log('Closed')}
+      disableAutoFocus
+      disableEnforceFocus
+      disableRestoreFocus
+    >
+      <Body1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </Body1>
+      <Body1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </Body1>
+    </Dialog>
+  );
 };
 
 DialogStory.story = {
   name: 'Confirmation',
   parameters: {
     design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/iuXkrpuLTXExSnuPJE3Jtn/Tidepool-Design-System---Sprint-1?node-id=8%3A826',
+      type: 'iframe',
+      // url: 'https://www.figma.com/file/iuXkrpuLTXExSnuPJE3Jtn/Tidepool-Design-System---Sprint-1?node-id=8%3A826',
+      url: 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FiuXkrpuLTXExSnuPJE3Jtn%2FTidepool-Design-System-Sprint-1%3Fnode-id%3D8%253A826',
     },
   },
 };
