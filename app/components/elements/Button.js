ecf8925187962603cc1c94f9aca89d2cb4ffa46f
Clint Beacock <clint.beacock@gmail.com>
2020-03-06 10:34:08 -0500

    [WEB-559] Revert commits accidentally pushed to this branch, instead of intended feature branch

--- app/components/elements/Button.js
+++ app/components/elements/Button.js
@@ -1,8 +1,6 @@
-import React, { useContext } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Button as Base, Box } from 'rebass/styled-components';
-import styled, { ThemeContext } from 'styled-components';
-import cx from 'classnames';
+import React from 'react';
+import { Button as Base } from 'rebass/styled-components';
+import styled from 'styled-components';
 
 import { ButtonFont } from './FontStyles';
 
@@ -13,47 +11,14 @@ import {
 const StyledButton = styled(Base)`
   cursor: pointer;
   transition: ${transitions.easeOut};
-  position: relative;
-
-  &:disabled {
-    pointer-events: none;
-  }
-
-  &.processing {
-    pointer-events: none;
-    line-height: 0;
-
-    ${ButtonFont} {
-      visibility: hidden;
-    }
-  }
-`;
-
-const StyledCircularProgress = styled(Box)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 `;
 
 export const Button = props => {
-  const { children, processing, ...buttonProps } = props;
-  const classNames = cx({ processing });
-
-  const themeContext = useContext(ThemeContext);
+  const { children, ...buttonProps } = props;
 
   return (
-    <StyledButton {...buttonProps} className={classNames}>
+    <StyledButton {...buttonProps}>
       <ButtonFont>{children}</ButtonFont>
-      { processing && (
-        <StyledCircularProgress>
-          <CircularProgress
-            color="inherit"
-            size={themeContext.fontSizes[3]}
-            thickness={5}
-          />
-        </StyledCircularProgress>
-      )}
     </StyledButton>
   );
 };
--- app/themes/base/buttons.js
+++ app/themes/base/buttons.js
@@ -4,29 +4,19 @@ export default ({ colors, borders }) => ({
     border: borders.input,
     borderColor: colors.purpleMedium,
     color: colors.white,
-    '&:hover,&:active': {
+    '&:hover,&:active,&:focus': {
       backgroundColor: colors.text.primary,
       borderColor: colors.text.primary,
-    },
-    '&:disabled': {
-      backgroundColor: colors.lightestGrey,
-      borderColor: colors.lightestGrey,
-      color: colors.text.primaryDisabled,
     },
   },
   secondary: {
     bg: colors.white,
     color: colors.text.primary,
     border: borders.input,
-    '&:hover,&:active': {
+    '&:hover,&:active,&:focus': {
       color: colors.white,
       backgroundColor: colors.text.primary,
       borderColor: colors.text.primary,
-    },
-    '&:disabled': {
-      backgroundColor: colors.lightestGrey,
-      borderColor: colors.lightestGrey,
-      color: colors.text.primaryDisabled,
     },
   },
 });
