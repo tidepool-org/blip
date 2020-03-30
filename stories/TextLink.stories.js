502e176e37e9a5d202db9633079fd1f49cd1821c
Clint Beacock <clint.beacock@gmail.com>
2020-03-30 12:08:53 -0400

    [WEB-584] Examples of using theme variants for rebass link styling

--- stories/TextLink.stories.js
+++ stories/TextLink.stories.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { withDesign } from 'storybook-addon-designs';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { ThemeProvider } from 'styled-components';
+import { Link as RebassLink, Text } from 'rebass/styled-components';
+import { TextLink as StyledLink } from '../app/components/elements/FontStyles';
 
 import baseTheme from '../app/themes/baseTheme';
 import TextLink from '../app/components/elements/TextLink';
@@ -23,9 +25,53 @@ const linkText = () => text('Link Text', 'Link Text');
 const link = () => text('URL', '');
 
 export const TextLinkStory = () => (
-  <TextLink
-    linkText={linkText()}
-    link={link()} />
+  <React.Fragment>
+
+    <Text backgroundColor="text.primary" fontSize={3}>
+      {'Blah Blah Blah '}
+
+      {/* TextLink component is wrapped in 2 divs, so will disrupt inline text flow */}
+      {/* Also, cannot style it via theme colors or variants */}
+      <TextLink
+        mx={3}
+        linkText={linkText()}
+        variant="links.inverted"
+        color="white"
+        sx={{ color: 'white' }}
+        link={link()}
+      />
+      {' Blah Blah Blah '}
+    </Text>
+
+    {/* Variants and color props won't work with styled component link -- will not be underlined or green */}
+    <StyledLink mx={3} color="green" variant="links.underlined" href={link()}>
+      {linkText()}
+    </StyledLink>
+
+    <RebassLink href={link()}>
+      {linkText()}
+    </RebassLink>
+
+    <Text backgroundColor="text.primary" fontSize={3}>
+      {'Blah Blah Blah '}
+      <RebassLink variant="links.inverted" href={link()}>
+        {linkText()}
+      </RebassLink>
+      {' Blah Blah Blah '}
+    </Text>
+
+    <Text lineHeight={3} fontSize={5}>
+      {'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quasi obcaecati iure sapiente eaque. Similique repellendus sequi iusto cupiditate magni nisi dolorum provident velit in, '}
+      <RebassLink variant="links.underlined" href={link()}>
+        {linkText()}
+      </RebassLink>
+      {' explicabo dolor nobis optio autem?'}
+    </Text>
+
+    <RebassLink href={link()} color="orange" variant="links.underlined">
+      {linkText()}
+    </RebassLink>
+  </React.Fragment>
 );
 
 TextLinkStory.story = {
