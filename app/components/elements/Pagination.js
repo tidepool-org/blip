3e6bee33a96daf062c925bfea9b4adccd449bf71
Clint Beacock <clint.beacock@gmail.com>
2020-04-14 12:46:37 -0400

    [WEB-758] Fix linting errors

--- app/components/elements/Pagination.js
+++ app/components/elements/Pagination.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import map from 'lodash/map';
 import remove from 'lodash/remove';
 import includes from 'lodash/includes';
 import capitalize from 'lodash/capitalize';
@@ -10,8 +11,7 @@ import LastPageRoundedIcon from '@material-ui/icons/LastPageRounded';
 import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
 import NavigateNextRoundedIcon from '@material-ui/icons/NavigateNextRounded';
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
-import { PaginationProps } from '@material-ui/lab/Pagination';
-import { usePagination } from '@material-ui/lab/Pagination';
+import { usePagination, PaginationProps } from '@material-ui/lab/Pagination';
 
 import Button from './Button';
 
@@ -20,19 +20,19 @@ export const Pagination = props => {
 
   const classNames = cx({
     condensed: variant === 'condensed',
-  })
+  });
 
   const { items } = usePagination(paginationProps);
-  const prevControls = remove(items, ({type}) => includes(['first', 'previous'], type))
-  const nextControls = remove(items, ({type}) => includes(['next', 'last'], type))
+  const prevControls = remove(items, ({ type }) => includes(['first', 'previous'], type));
+  const nextControls = remove(items, ({ type }) => includes(['next', 'last'], type));
 
   return (
     <Box as="nav" variant={`paginators.${variant}`} {...themeProps}>
       <ul className={classNames}>
         <li>
           <ul className="prev-controls">
-            {prevControls.map(({ type, ...item }, index) => (
-              <li id={`${id}-${type}`} key={index}>
+            {map(prevControls, ({ type, ...item }) => (
+              <li id={`${id}-${type}`} key={`${id}-${type}`}>
                 <Button px={2} variant="pagination" {...item}>
                   {type === 'first' && <FirstPageRoundedIcon />}
                   {type === 'previous' && <NavigateBeforeRoundedIcon />}
@@ -44,7 +44,7 @@ export const Pagination = props => {
         </li>
         <li>
           <ul className="pages">
-            {items.map(({ page, type, selected, ...item }, index) => {
+            {map(items, ({ page, type, selected, ...item }) => {
               const pageClassNames = cx({
                 selected,
               });
@@ -53,7 +53,7 @@ export const Pagination = props => {
               let children;
 
               if (type === 'start-ellipsis' || type === 'end-ellipsis') {
-                children = <Button disabled className='ellipsis' variant="pagination"><MoreHorizRoundedIcon /></Button>;
+                children = <Button disabled className="ellipsis" variant="pagination"><MoreHorizRoundedIcon /></Button>;
               } else if (type === 'page') {
                 children = (
                   <Button className={pageClassNames} variant="pagination" {...item}>
@@ -62,14 +62,14 @@ export const Pagination = props => {
                 );
               }
 
-              return <li id={itemId} key={index}>{children}</li>;
+              return <li id={itemId} key={itemId}>{children}</li>;
             })}
           </ul>
         </li>
         <li>
           <ul className="next-controls">
-            {nextControls.map(({ type, ...item }, index) => (
-              <li id={`${id}-${type}`} key={index}>
+            {map(nextControls, ({ type, ...item }) => (
+              <li id={`${id}-${type}`} key={`${id}-${type}`}>
                 <Button px={2} variant="pagination" {...item}>
                   {variant === 'default' && <Text pr={1}>{capitalize(type)}</Text>}
                   {type === 'next' && <NavigateNextRoundedIcon />}
@@ -93,6 +93,6 @@ Pagination.propTypes = {
 
 Pagination.defaultProps = {
   variant: 'default',
-}
+};
 
 export default Pagination;
