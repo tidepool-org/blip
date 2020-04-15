d9e27857ba274d6bd71cfacddda689e99c11b9e7
Thacher <thacher@tidepool.org>
2020-04-14 22:27:44 -0700

    [WEB-761] linting

--- stories/Banner.stories.js
+++ stories/Banner.stories.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { withDesign } from 'storybook-addon-designs';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
+// import { withKnobs, text } from '@storybook/addon-knobs';
 import { ThemeProvider } from 'styled-components';
-import { Text } from 'rebass/styled-components';
+// import { Text } from 'rebass/styled-components';
 
 import baseTheme from '../app/themes/baseTheme';
-import { Body2 } from '../app/components/elements/FontStyles';
+// import { Body2 } from '../app/components/elements/FontStyles';
 // import Avatar from '../app/components/elements/Avatar';
 import Banner from '../app/components/elements/Banner';
+
+/* eslint-disable max-len */
 
 const withTheme = Story => (
   <ThemeProvider theme={baseTheme}>
@@ -21,8 +24,8 @@ export default {
   decorators: [withDesign, withKnobs, withTheme],
 };
 
-const bannerText = () => text('Banner Text', 'Fluffer ur givin me a spook. lotsa pats smol borking doggo with shooberino boofers. ');
-const bannerText2 = () => text('Banner Text 2', 'He made many woofs mlem, many pats. Wrinkler h*ck doggorino clouds, you are doing me a frighten.');
+// const bannerText = () => text('Banner Text', 'Fluffer ur givin me a spook. lotsa pats smol borking doggo with shooberino boofers. ');
+// const bannerText2 = () => text('Banner Text 2', 'He made many woofs mlem, many pats. Wrinkler h*ck doggorino clouds, you are doing me a frighten.');
 
 export const BannerStory = () => (
   // <React.Fragment>
@@ -30,11 +33,11 @@ export const BannerStory = () => (
   //     {bannerText()}
   //   </Body2>
   // </React.Fragment>
-    <React.Fragment>
-      <Banner my={2} />
-      <Banner my={2} variant="inverse" />
-      <Banner my={2} backgroundColor="oranges.2" />
-    </React.Fragment>
+  <React.Fragment>
+    <Banner my={2} />
+    <Banner my={2} variant="inverse" />
+    <Banner my={2} backgroundColor="oranges.2" />
+  </React.Fragment>
 );
 
 BannerStory.story = {
