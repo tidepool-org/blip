25ff8af059300e633e625543b354afeb1e5a9f14
Clint Beacock <clint.beacock@gmail.com>
2020-04-16 12:26:02 -0400

    [WEB-585] Ensure random avatar colors on Table stories aren't regenerated each render

--- stories/Table.stories.js
+++ stories/Table.stories.js
@@ -4,10 +4,12 @@ import { withDesign } from 'storybook-addon-designs';
 import { withKnobs, boolean, optionsKnob as options } from '@storybook/addon-knobs';
 import { ThemeProvider } from 'styled-components';
 import toUpper from 'lodash/toUpper';
+import random from 'lodash/random';
 import { Flex, Box, Text } from 'rebass/styled-components';
 
 import baseTheme from '../app/themes/baseTheme';
 import Table from '../app/components/elements/Table';
+import Avatar from '../app/components/elements/Avatar';
 
 /* eslint-disable max-len */
 
@@ -23,18 +25,32 @@ export default {
   decorators: [withDesign, withKnobs, withTheme],
 };
 
+function createPatient(name, email) {
+  const colors = [
+    'blues',
+    'cyans',
+    'greens',
+    'indigos',
+    'oranges',
+    'pinks',
+    'purples',
+  ];
+
+  return { name, email, color: colors[random(colors.length - 1)] };
+}
+
 function createData(patient, status, permission, role) {
   return { patient, status, permission, role };
 }
 
-function renderName({ patient }) {
+function renderPatient({ patient }) {
   let initials = patient.name.match(/\b\w/g) || [];
   initials = toUpper((initials.shift() || '') + (initials.pop() || ''));
 
   return (
     <Flex alignItems="center">
-      <Box px={2}>{initials}</Box>
-      <Box px={2}>
+      <Avatar color={`${patient.color}.9`} bg={`${patient.color}.0`} mr={3} initials={initials} />
+      <Box>
         <Text fontWeight="medium">{patient.name}</Text>
         <Text>{patient.email}</Text>
       </Box>
@@ -44,7 +60,7 @@ function renderName({ patient }) {
 
 function renderStatus({ status }) {
   let color = 'text.primary';
-  let bg = 'white';
+  let bg = 'transparent';
 
   if (status) {
     color = status === 'success' ? 'greens.9' : 'indigos.9';
@@ -55,25 +71,25 @@ function renderStatus({ status }) {
 }
 
 const columns = [
-  { title: 'Patient', field: 'patient', align: 'left', sortable: true, render: renderName },
+  { title: 'Patient', field: 'patient', align: 'left', sortable: true, render: renderPatient },
   { title: 'Status', field: 'status', align: 'left', render: renderStatus },
   { title: 'Permission', field: 'permission', align: 'left' },
   { title: 'Role', field: 'role', align: 'left' },
 ];
 
 const data = [
-  createData({ name: 'Claire Clownfish', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Admin'),
-  createData({ name: 'James Jellyfish', email: 'foo@bar.com' }, 'success', '', 'Clinic Member'),
-  createData({ name: 'Bill Barracuda', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Admin'),
-  createData({ name: 'Sam Jellyfish', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Member'),
-  createData({ name: 'Oprah Orca', email: 'foo@bar.com' }, 'success', '', 'Clinic Admin'),
-  createData({ name: 'Wendy Barracuda', email: 'foo@bar.com' }, 'pending', '', 'Clinic Admin'),
-  createData({ name: 'George Grouper', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Admin'),
-  createData({ name: 'Peter Pike', email: 'foo@bar.com' }, 'success', '', 'Clinic Member'),
-  createData({ name: 'Patsy Pickeral', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Admin'),
-  createData({ name: 'Tom Turtle', email: 'foo@bar.com' }, '', 'Prescriber', 'Clinic Member'),
-  createData({ name: 'Perry Porpoise', email: 'foo@bar.com' }, 'pending', '', 'Clinic Admin'),
-  createData({ name: 'Marco Manowar', email: 'foo@bar.com' }, 'success', '', 'Clinic Admin'),
+  createData(createPatient('Claire Clownfish', 'claire@testemail.com'), '', 'Prescriber', 'Clinic Admin'),
+  createData(createPatient('James Jellyfish', 'james@testemail.com'), 'success', '', 'Clinic Member'),
+  createData(createPatient('Bill Barracuda', 'bill@testemail.com'), '', 'Prescriber', 'Clinic Admin'),
+  createData(createPatient('Sam Jellyfish', 'sam@testemail.com'), '', 'Prescriber', 'Clinic Member'),
+  createData(createPatient('Oprah Orca', 'oprah@testemail.com'), 'pending', '', 'Clinic Admin'),
+  createData(createPatient('Wendy Barracuda', 'wendy@testemail.com'), 'pending', '', 'Clinic Admin'),
+  createData(createPatient('George Grouper', 'george@testemail.com'), '', 'Prescriber', 'Clinic Admin'),
+  createData(createPatient('Peter Pike', 'peter@testemail.com'), 'success', '', 'Clinic Member'),
+  createData(createPatient('Patsy Pickeral', 'patsy@testemail.com'), '', 'Prescriber', 'Clinic Admin'),
+  createData(createPatient('Tom Turtle', 'tom@testemail.com'), '', 'Prescriber', 'Clinic Member'),
+  createData(createPatient('Perry Porpoise', 'perry@testemail.com'), 'pending', '', 'Clinic Admin'),
+  createData(createPatient('Marco Manowar', 'marco@testemail.com'), 'success', '', 'Clinic Admin'),
 ];
 
 const stickyHeader = () => boolean('Sticky Header', false);
