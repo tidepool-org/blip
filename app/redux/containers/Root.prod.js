9519dea45326e47fd29d83b60080a3bc2ed7cb06
Clint Beacock <clint.beacock@gmail.com>
2020-05-19 17:21:42 -0400

    [WEB-819] Add theme provider to Root app wrapper

--- app/redux/containers/Root.prod.js
+++ app/redux/containers/Root.prod.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { ThemeProvider } from 'styled-components';
+
+import baseTheme from '../../themes/baseTheme';
 
 export default class Root extends Component {
   render() {
     const { store, routing } = this.props;
     return (
-      <Provider store={store}>
-        <div>
-          <Router history={browserHistory}>
-            {routing}
-          </Router>
-        </div>
-      </Provider>
+      <ThemeProvider theme={baseTheme}>
+        <Provider store={store}>
+          <div>
+            <Router history={browserHistory}>
+              {routing}
+            </Router>
+          </div>
+        </Provider>
+      </ThemeProvider>
     );
   }
 };
