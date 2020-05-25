tidepool-org/blip

commit 7f2af74c87787d04cb19b8ed554cf4e85b4eb997

PT-1309 tideline viz deps (#196)
* Add SOUP list build and publish to the pipeline

Dockerfile
@@ -29,8 +29,12 @@ ENV \
 ### Stage 2 - Create cached `node_modules`
 # Only rebuild layer if `package.json` has changed
 FROM base as dependencies
-COPY package.json .
-ENV nexus_token=''
+
+ARG npm_token
+ENV nexus_token=$npm_token
+
+COPY package.json .npmrc ./
+
 # Run as node user, so that npm run the prepare scripts in dependencies
 USER node
 RUN \

.npmrc
@@ -1,0 +1,3 @@
+registry=https://nexus.ci.diabeloop.eu/repository/npm-diabeloop/
+email=jenkins@diabeloop.fr
+_auth=${nexus_token}

.travis.yml
@@ -10,35 +10,52 @@ node_js:
 
 cache: npm
 
+env:
+  jobs:
+    - BUILD_SOUP=true
+
 before_install:
   - if [[ `npm -v` != 6* ]]; then npm install -g npm@6; fi
 
 install:
   - npm install
+  - npm install --save-dev ci-toolbox@2.1.1
 
 deploy:
   # Control deployment by setting a value for `on`. Setting the `branch`
   # option to `master` means Travis will only attempt a deployment on
   # builds of your repo's master branch (e.g., after you merge a PR).
-  on:
-    tags: true
-    node_js: 10.15.3
-#    branch: dbl
-  provider: s3
-  # You can refer to environment variables from Travis repo settings!
-  access_key_id: $AWS_ACCESS_KEY_ID
-  secret_access_key: $AWS_SECRET_ACCESS_KEY
-  region: $AWS_DEFAULT_REGION
-  # Name of the S3 bucket to which your site should be uploaded.
-  bucket: $AWS_BUCKET
-  # Prevent Travis from deleting your built site so it can be uploaded.
-  skip_cleanup: true
-  # Path of the source directory containing your built site.
-  local_dir: deploy
-  # Path to a directory containing your built site.
-  upload-dir: deploy
-  # Set the Cache-Control header.
-  cache_control: "max-age=21600"
+  - provider: s3
+    on:
+      tags: true
+      node_js: 10.15.3
+    # You can refer to environment variables from Travis repo settings!
+    access_key_id: $AWS_ACCESS_KEY_ID
+    secret_access_key: $AWS_SECRET_ACCESS_KEY
+    region: $AWS_DEFAULT_REGION
+    # Name of the S3 bucket to which your site should be uploaded.
+    bucket: $AWS_BUCKET
+    # Prevent Travis from deleting your built site so it can be uploaded.
+    skip_cleanup: true
+    # Path of the source directory containing your built site.
+    local_dir: deploy
+    # Path to a directory containing your built site.
+    upload-dir: deploy
+    # Set the Cache-Control header.
+    cache_control: "max-age=21600"
+  # Deploy SOUP list
+  - provider: s3
+    on:
+      tags: true
+      node_js: 10.15.3
+    access_key_id: $AWS_ACCESS_KEY_ID
+    secret_access_key: $AWS_SECRET_ACCESS_KEY
+    region: $AWS_DEFAULT_REGION
+    bucket: com.diabeloop.yourloops.doc
+    skip_cleanup: true
+    local_dir: doc
+    upload-dir: soup/blip
+    cache_control: "max-age=21600"
 
 services:
   - docker

commit 4e22bded14ca874f9597b6c6257bc8147e7aca0d

Fix regression introduced in 7f2af74 (#202)
* fix regression

* Disable German language

* fix local docker build

* Bump to 1.0.0

Dockerfile
@@ -96,6 +96,8 @@ ENV \
 
 ### Stage 6 - Build production-ready release
 FROM buildBase as build
+ARG npm_token
+ENV nexus_token=$npm_token
 # Copy all `node_modules` from `dependancies` layer
 COPY --from=dependencies /app/node_modules ./node_modules
 # Copy source files, and possibily invalidate so we have to rebuild

app/core/language.js
@@ -58,10 +58,6 @@ const i18nOptions = {
     fr: {
       main: mainFR,
       params: parameterFR
-    },
-    de: {
-      main: mainDE,
-      params: parameterDE
     }
   }
 }

app/pages/userprofile/userprofile.js
@@ -71,9 +71,7 @@ export var UserProfile = translate()(React.createClass({
     if (config.I18N_ENABLED) {
       let locales = [
         {value: 'en', label: 'English'},
-        {value: 'fr', label: 'Français'},
-        {value: 'de', label: 'Deutsch'}
-      ];
+        {value: 'fr', label: 'Français'}      ];
       // Special "pseudo" language for crowdin live preview
       if (typeof _jipt === 'object') {
         locales.push({value: 'it', label: 'Crowdin'});