e669a121aef13a93c9d3d52b4a0f6975f362e537
Clint Beacock <clint.beacock@gmail.com>
2020-05-25 17:49:46 -0400

    [WEB-819] Add some colored pills for prescription state

--- app/pages/prescription/Prescriptions.js
+++ app/pages/prescription/Prescriptions.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import { translate } from 'react-i18next';
 import SearchIcon from '@material-ui/icons/Search';
-import { Box, Flex } from 'rebass/styled-components';
+import { Box, Flex, Text } from 'rebass/styled-components';
 
 import Table from '../../components/elements/Table';
 import Button from '../../components/elements/Button';
 import TextInput from '../../components/elements/TextInput';
+import { Headline } from '../../components/elements/FontStyles';
 
 const createPlaceholderPrescription = (id, firstName, lastName, mrn, state) => ({ id, firstName, lastName, mrn, state });
 
@@ -22,18 +23,6 @@ const stubPrescriptions = [
 const Prescriptions = props => {
   const { prescriptions = stubPrescriptions, t } = props;
 
-  const renderEdit = ({ id }) => (
-    <Button p={0} fontSize="inherit" variant="textPrimary" onClick={() => browserHistory.push(`prescriptions/${id}/edit`)}>{t('Edit')}</Button>
-  );
-
-  const columns = [
-    { title: 'First Name', field: 'firstName', align: 'left', sortable: true, searchable: true },
-    { title: 'Last Name', field: 'lastName', align: 'left', sortable: true, searchable: true },
-    { title: 'MRN', field: 'mrn', align: 'left', searchable: true },
-    { title: 'Status', field: 'state', align: 'left', sortable: true},
-    { title: 'Edit', field: 'edit', render: renderEdit, align: 'left' },
-  ];
-
   const [searchText, setSearchText] = React.useState();
 
   function handleSearchChange(event) {
@@ -42,18 +31,58 @@ const Prescriptions = props => {
 
   const handleAddNew = () => browserHistory.push('prescriptions/new');
 
+
+  const renderEdit = ({ id }) => (
+    <Button p={0} fontSize="inherit" variant="textPrimary" onClick={() => browserHistory.push(`prescriptions/${id}/edit`)}>{t('Edit')}</Button>
+  );
+
+  const renderState = ({ state }) => {
+    let colors = {
+      draft: {
+        color: 'indigos.9',
+        bg: 'indigos.0',
+      },
+      pending: {
+        color: 'oranges.9',
+        bg: 'oranges.0',
+      },
+      submitted: {
+        color: 'greens.9',
+        bg: 'greens.0',
+      },
+    };
+
+    const stateColors = colors[state] || {
+      color: 'text.primary',
+      bg: 'transparent',
+    }
+
+    return <Text as="span" px={2} py={1} fontWeight="medium" sx={{ borderRadius: 4 }} color={stateColors.color} bg={stateColors.bg}>{state}</Text>;
+  };
+
+  const columns = [
+    { title: t('First Name'), field: 'firstName', align: 'left', sortable: true, searchable: true },
+    { title: t('Last Name'), field: 'lastName', align: 'left', sortable: true, searchable: true },
+    { title: t('MRN #'), field: 'mrn', align: 'left', searchable: true },
+    { title: t('State'), field: 'state', render: renderState, align: 'left', sortable: true},
+    { title: t('Edit'), field: 'edit', render: renderEdit, align: 'left' },
+  ];
+
   return (
     <Box mx={3} my={2} px={2} py={4} bg='white'>
       <Flex my={3} justifyContent="space-between">
         <Box alignSelf="flex-end">
-          <Button variant="primary" onClick={handleAddNew}>{t('Add New')}</Button>
+          <Flex>
+            <Headline mr={3}>{t('Prescriptions')}</Headline>
+            <Button variant="primary" onClick={handleAddNew}>{t('Add New')}</Button>
+          </Flex>
         </Box>
         <TextInput
           themeProps={{
             width: 'auto',
             minWidth: '250px',
           }}
-          placeholder={t('enter search text')}
+          placeholder={t('search by name or MRN #')}
           icon={SearchIcon}
           label={t('Search Prescriptions')}
           name="search-prescriptions"
