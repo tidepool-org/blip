213deae9b49ba8e6d35f4dc6995e612697af6933
Clint Beacock <clint.beacock@gmail.com>
2020-05-29 13:53:13 -0400

    [WEB-819] Fix getFieldsMeta util to recurse into nested fields

--- app/core/forms.js
+++ app/core/forms.js
@@ -2,19 +2,31 @@ import includes from 'lodash/includes';
 import map from 'lodash/map';
 import reduce from 'lodash/reduce';
 import keys from 'lodash/keys';
+import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
 
 export const getFieldsMeta = (schema, getFieldMeta) => {
-  const fields = keys(schema.fields);
+  const fieldKeys = keys(schema.fields);
 
-  return reduce(fields, (result, field) => {
-    const fieldMeta = getFieldMeta(field);
-    result[field] = {
-      ...fieldMeta,
-      valid: (!isEmpty(fieldMeta.value) || fieldMeta.touched) && !fieldMeta.error,
-    };
+  const reduceFields = (_fieldKeys, prefix = '') => reduce(_fieldKeys, (result, field) => {
+    const nestedFields = get(schema.fields, `${field}.fields`);
+
+    if (nestedFields) {
+      // Recurse for nested field keys
+      result[field] = reduceFields(keys(nestedFields), `${field}.`);
+    } else {
+      const fieldKey = `${prefix}${field}`;
+      const fieldMeta = getFieldMeta(fieldKey);
+      result[field] = {
+        ...fieldMeta,
+        valid: (!isEmpty(fieldMeta.value) || fieldMeta.touched) && !fieldMeta.error,
+      };
+    }
+
     return result;
   }, {});
+
+  return reduceFields(fieldKeys)
 };
 
-export const fieldsAreValid = (fieldNames, fieldsMeta) => !includes(map(fieldNames, fieldName => fieldsMeta[fieldName].valid), false);
+export const fieldsAreValid = (fieldNames, fieldsMeta) => !includes(map(fieldNames, fieldName => get(fieldsMeta, `${fieldName}.valid`)), false);
