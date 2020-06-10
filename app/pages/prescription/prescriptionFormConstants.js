ab95b34febf3cd8d17259e63238bb927d510c1b0
Clint Beacock <clint.beacock@gmail.com>
2020-06-10 15:12:22 -0400

    [WEB-821] Update handling of default units, ranges, thresholds, and deviceMeta

--- app/pages/prescription/prescriptionFormConstants.js
+++ app/pages/prescription/prescriptionFormConstants.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Trans } from 'react-i18next';
 import { Link } from 'rebass/styled-components';
 import i18next from '../../core/language';
+import { MGDL_UNITS } from '../../core/constants';
 
 const t = i18next.t.bind(i18next);
 
@@ -35,15 +36,49 @@ export const cgmDeviceOptions = [
   },
 ];
 
-// TODO: placeholder device-specific values until provided by the upcoming devices api.
-export const deviceSpecificValues = {
-  dexcomId: {
-    manufacturerName: 'Dexcom',
-  },
-  omnipodId: {
-    manufacturerName: 'Omnipod',
-  },
+export const defaultUnits = {
+  basalRate: 'Units/hour',
+  bolusAmount: 'Units',
+  insulinCarbRatio: 'g/U',
+  bloodGlucose: MGDL_UNITS,
+};
+
+export const defaultValues = {
+  basalRateMaximum: 5,
+  bolusAmountMaximum: 10,
+  suspendThreshold: 80,
 }
+
+export const defaultThresholds = {
+  basalRateMaximum: { warning: 30 },
+  bolusAmountMaximum: { warning: 30 },
+  suspendThreshold: { warning: 30 },
+};
+
+export const defaultRanges = {
+  basalRateMaximum: { min: 0, max: 30, step: 0.25 },
+  bolusAmountMaximum: { min: 0, max: 30, step: 1 },
+  suspendThreshold: { min: 54, max: 150, step: 1 },
+};
+
+// TODO: placeholder device-specific values until provided by the upcoming devices api.
+export const deviceMeta = deviceId => {
+  const metaByDeviceId = {
+    dexcomId: {
+      manufacturerName: 'Dexcom',
+      ranges: defaultRanges,
+    },
+    omnipodId: {
+      manufacturerName: 'Omnipod',
+      ranges: defaultRanges,
+    },
+  };
+
+  return metaByDeviceId[deviceId] || {
+    manufacturerName: 'Unknown',
+    ranges: defaultRanges,
+  };
+};
 
 export const typeOptions = [
   { value: 'patient', label: t('Patient') },
@@ -59,6 +94,11 @@ export const sexOptions = [
 export const trainingOptions = [
   { value: 'inPerson', label: t('Yes, Patient requires in-person CPT training') },
   { value: 'inModule', label: t('No, Patient can self start with Tidepool Loop in-app tutorial') },
+];
+
+export const insulinTypeOptions = [
+  { value: 'rapidAdult', label: t('Rapid Acting Adult') },
+  { value: 'rapidChild', label: t('Rapid Acting Child') },
 ];
 
 export const validCountryCodes = [1];
