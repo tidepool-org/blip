32b6b4d4ba269aff80e3c0829ef90f3c3509c5ee
Clint Beacock <clint.beacock@gmail.com>
2020-06-10 13:46:17 -0400

    [WEB-821] Add a PopoverLabel component

---
+++ app/components/elements/PopoverLabel.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
+import { Text } from 'rebass/styled-components';
+import { Label } from '@rebass/forms';
+
+import {
+  usePopupState,
+  bindHover,
+  bindPopover,
+  bindTrigger,
+} from 'material-ui-popup-state/hooks';
+
+import Popover from './Popover';
+import { Icon } from './Icon';
+
+const PopoverLabel = props => {
+  const {
+    icon,
+    iconLabel,
+    id,
+    label,
+    popoverContent: PopoverContent,
+    popoverWidth,
+    triggerOnHover,
+    ...labelProps
+  } = props;
+
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: id,
+  });
+
+  return (
+    <React.Fragment>
+      <Label color="text.primary" {...labelProps}>
+        {label && <Text mr={2}>{label}</Text>}
+        <Icon
+          label={iconLabel}
+          icon={icon}
+          {...(triggerOnHover ? bindHover(popupState) : bindTrigger(popupState))}
+        />
+      </Label>
+
+      <Popover width={popoverWidth} {...bindPopover(popupState)}>
+        {React.cloneElement(PopoverContent, {})}
+        {/* {PopoverContent()} */}
+      </Popover>
+    </React.Fragment>
+  );
+};
+
+PopoverLabel.propTypes = {
+  icon: PropTypes.elementType.isRequired,
+  iconLabel: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  popoverContent: PropTypes.node,
+  popoverWidth: PropTypes.string.isRequired,
+  triggerOnHover: PropTypes.bool,
+};
+
+PopoverLabel.defaultProps = {
+  icon: InfoRoundedIcon,
+  iconLabel: 'more info',
+  popoverWidth: '25em',
+  triggerOnHover: false,
+};
+
+export default PopoverLabel;
