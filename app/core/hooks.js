9a26e5bc4e79f226ae66b3d3ed3bbba9edd95671
Clint Beacock <clint.beacock@gmail.com>
2020-06-15 15:11:44 -0400

    [WEB-821] Fix code formatting in useFieldArray hook

--- app/core/hooks.js
+++ app/core/hooks.js
@@ -6,91 +6,90 @@ import { useField, useFormikContext } from 'formik';
 // c.f. https://gist.github.com/joshsalverda/d808d92f46a7085be062b2cbde978ae6
 // Avoids some performance issues in Formik's native <FieldArray />
 export const useFieldArray = props => {
-  const [field, meta] = useField(props)
-  // const [field, meta] = useFastField(props.name)
-  const fieldArray = useRef(field.value)
-  const {setFieldValue} = useFormikContext()
+  const [field, meta] = useField(props);
+  const fieldArray = useRef(field.value);
+  const {setFieldValue} = useFormikContext();
 
   useEffect(() => {
-    fieldArray.current = field.value
-  }, [field.value])
+    fieldArray.current = field.value;
+  }, [field.value]);
 
   const push = useCallback(value => {
     fieldArray.current = update(fieldArray.current, {
-      $push: [value]
-    })
+      $push: [value],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+  }, [field.name, setFieldValue]);
 
   const swap = useCallback((indexA, indexB) => {
-    const swapA = fieldArray.current[indexA]
-    const swapB = fieldArray.current[indexB]
+    const swapA = fieldArray.current[indexA];
+    const swapB = fieldArray.current[indexB];
 
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[indexA, 1, swapB], [indexB, 1, swapA]]
-    })
+      $splice: [[indexA, 1, swapB], [indexB, 1, swapA]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+  }, [field.name, setFieldValue]);
 
   const move = useCallback((from, to) => {
-    const toMove = fieldArray.current[from]
+    const toMove = fieldArray.current[from];
 
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[from, 1], [to, 0, toMove]]
-    })
+      $splice: [[from, 1], [to, 0, toMove]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+  }, [field.name, setFieldValue]);
 
   const insert = useCallback((index, value) => {
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[index, 0, value]]
-    })
+      $splice: [[index, 0, value]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+  }, [field.name, setFieldValue]);
 
   const unshift = useCallback(value => {
     fieldArray.current = update(fieldArray.current, {
-      $unshift: [value]
+      $unshift: [value],
     })
 
-    setFieldValue(field.name, fieldArray.current)
-    return fieldArray.current.length
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+    return fieldArray.current.length;
+  }, [field.name, setFieldValue]);
 
   const remove = useCallback(index => {
-    const removedItem = fieldArray.current[index]
+    const removedItem = fieldArray.current[index];
 
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[index, 1]]
-    })
+      $splice: [[index, 1]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-    return removedItem
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+    return removedItem;
+  }, [field.name, setFieldValue]);
 
   const pop = useCallback(() => {
-    const lastIndex = fieldArray.current.length - 1
-    const poppedItem = fieldArray.current[lastIndex]
+    const lastIndex = fieldArray.current.length - 1;
+    const poppedItem = fieldArray.current[lastIndex];
 
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[lastIndex, 1]]
-    })
+      $splice: [[lastIndex, 1]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-    return poppedItem
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+    return poppedItem;
+  }, [field.name, setFieldValue]);
 
   const replace = useCallback((index, value) => {
     fieldArray.current = update(fieldArray.current, {
-      $splice: [[index, 1, value]]
-    })
+      $splice: [[index, 1, value]],
+    });
 
-    setFieldValue(field.name, fieldArray.current)
-  }, [field.name, setFieldValue])
+    setFieldValue(field.name, fieldArray.current);
+  }, [field.name, setFieldValue]);
 
   return [
     field,
@@ -103,10 +102,10 @@ export const useFieldArray = props => {
       unshift,
       remove,
       pop,
-      replace
-    }
-  ]
-}
+      replace,
+    },
+  ];
+};
 
 // c.f. https://usehooks.com/usePrevious/
 export const usePrevious = value => {
