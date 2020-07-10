8fddb9b48fe2a1a1f96f771bf24cde3cfc1fb637
Thacher <thacher@tidepool.org>
2020-07-10 15:52:09 -0700

    [WEB-763] Removed duplicate chip styling

--- app/themes/base/buttons.js
+++ app/themes/base/buttons.js
@@ -186,57 +186,5 @@ export default ({ colors, borders, fontSizes, radii, fonts, space, fontWeights }
         },
       },
     },
-    chip: {
-      ...defaultStyles,
-      backgroundColor: colors.white,
-      color: colors.text.primary,
-      border: borders.input,
-      borderColor: colors.grays[1],
-      borderRadius: radii.full,
-      fontSize: `${fontSizes[1]}px`,
-      fontWeight: fontWeights.medium,
-      padding: `7px ${space[5]}px`,
-
-      '&.processing': {
-        color: colors.white,
-        backgroundColor: colors.purpleMedium,
-        borderColor: colors.purpleMedium,
-
-        '.MuiCircularProgress-root': {
-          width: `${fontSizes[2]}px !important`,
-          height: `${fontSizes[2]}px !important`,
-        },
-      },
-      '.icon': {
-        fontSize: `${fontSizes[2]}px`,
-      },
-      '&:hover': {
-        borderColor: colors.grays[2],
-      },
-      '&:active': {
-        color: colors.white,
-        backgroundColor: colors.text.primary,
-        borderColor: colors.text.primary,
-      },
-      '&.active': {
-        color: colors.white,
-        backgroundColor: colors.purpleMedium,
-        borderColor: colors.purpleMedium,
-      },
-      '&:disabled': {
-        backgroundColor: colors.lightestGrey,
-        borderColor: colors.lightestGrey,
-        color: colors.text.primaryDisabled,
-      },
-      ':focus': {
-        outline: 'none',
-        boxShadow: '0px 0px 0px 2px Highlight',
-      },
-      '@media (-webkit-min-device-pixel-ratio:0)': {
-        ':focus': {
-          boxShadow: '0px 0px 0px 2px -webkit-focus-ring-color',
-        },
-      },
-    },
   };
 };
