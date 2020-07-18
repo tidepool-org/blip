42c87be1671dce5412ee56b6256044e2dd04aae5
Thacher <thacher@tidepool.org>
2020-07-18 13:07:48 -0700

    [WEB-716] Updated Uploader Button to use Button component

--- app/components/uploaderbutton/uploaderbutton.js
+++ app/components/uploaderbutton/uploaderbutton.js
@@ -1,19 +1,3 @@
-
-/**
- * Copyright (c) 2016, Tidepool Project
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the associated License, which is identical to the BSD 2-Clause
- * License as published by the Open Source Initiative at opensource.org.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the License for more details.
- *
- * You should have received a copy of the License along with this program; if
- * not, you can obtain one from Tidepool Project at tidepool.org.
- */
-
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import GitHub from 'github-api';
@@ -21,8 +5,10 @@ import _ from 'lodash';
 import cx from 'classnames';
 import utils from '../../core/utils';
 import { translate } from 'react-i18next';
+import { Flex, Box } from 'rebass/styled-components';
 
 import { URL_UPLOADER_DOWNLOAD_PAGE } from '../../core/constants';
+import Button from '../elements/Button';
 
 import logoSrc from './images/T-logo-dark-512x512.png';
 
@@ -43,11 +29,15 @@ export default translate()(class UploaderButton extends Component {
     buttonText: PropTypes.string.isRequired
   };
 
+  handleLinkToUploaderDownload = () => { window.location = (URL_UPLOADER_DOWNLOAD_PAGE) };
+  handleMacDownload = () => { window.location = (this.state.latestMacRelease) };
+  handleWinDownload = () => { window.location = (this.state.latestWinRelease) };
+
   UNSAFE_componentWillMount = () => {
     const uploaderRepo = github.getRepo('tidepool-org/uploader');
     uploaderRepo.listReleases((err, releases, request) => {
-      if(err){
-        this.setState({error: true});
+      if (err) {
+        this.setState({ error: true });
       }
       this.setState(utils.getUploaderDownloadURL(releases));
     });
@@ -61,11 +51,11 @@ export default translate()(class UploaderButton extends Component {
         href={URL_UPLOADER_DOWNLOAD_PAGE}
         target="_blank"
         onClick={this.props.onClick}>
-          <div className="uploader-logo">
-            <img src={logoSrc} alt={t('Tidepool Uploader')} />
-          </div>
-          {this.props.buttonText}
-        </a>
+        <div className="uploader-logo">
+          <img src={logoSrc} alt={t('Tidepool Uploader')} />
+        </div>
+        {this.props.buttonText}
+      </a>
     )
   }
 
@@ -83,33 +73,33 @@ export default translate()(class UploaderButton extends Component {
     });
 
     let content;
-    if(this.state.error) {
+    if (this.state.error) {
       content = this.renderErrorText();
     } else {
       content = [
-        <a
-          key={'pc'}
-          className={winReleaseClasses}
-          href={`${this.state.latestWinRelease}`}
-          disabled={!this.state.latestWinRelease}
-          onClick={this.props.onClick}>
-          {t('Download for PC')}
-        </a>,
-        <a
-          key={'mac'}
-          className={macReleaseClasses}
-          href={`${this.state.latestMacRelease}`}
-          disabled={!this.state.latestMacRelease}
-          onClick={this.props.onClick}>
-          {t('Download for Mac')}
-        </a>
+        <Flex justifyContent="center">
+          <Box mx={2}>
+            <Button
+              variant="primary"
+              key={'pc'}
+              onClick={this.handleWinDownload}
+            >Download for PC</Button>
+          </Box>
+          <Box mx={2}>
+            <Button
+              variant="primary"
+              key={'mac'}
+              onClick={this.handleMacDownload}
+            >Download for Mac</Button>
+          </Box>
+        </Flex>
       ]
     }
 
     return (
-      <div className='uploaderbutton-wrap'>
+      <Flex justifyContent="center">
         {content}
-      </div>
+      </Flex>
     );
   }
 });
