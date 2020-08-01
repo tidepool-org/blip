a3035db6c411a4fe5e839729b17ad821913bef6d
Thacher <thacher@tidepool.org>
2020-08-01 15:41:36 -0700

    [WEB-153] Updates per PR

--- test/unit/components/updatetypebanner.test.js
+++ test/unit/components/updatetypebanner.test.js
@@ -1,19 +1,3 @@
-/*
- * == BSD2 LICENSE ==
- * Copyright (c) 2017, Tidepool Project
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the associated License, which is identical to the BSD 2-Clause
- * License as published by the Open Source Initiative at opensource.org.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the License for more details.
- *
- * You should have received a copy of the License along with this program; if
- * not, you can obtain one from Tidepool Project at tidepool.org.
- * == BSD2 LICENSE ==
- */
 /* global chai */
 /* global describe */
 /* global context */
@@ -59,15 +43,6 @@ describe('UpdateTypeBanner', () => {
     expect(wrapper.find('.updateTypeBanner')).to.have.length(1);
     expect(console.error.callCount).to.equal(0);
   });
-
-  // it('should render a link to the dexcom connect info on the website', () => {
-  //   const expectedText = 'Learn More'
-  //   const messageLink = wrapper.find('.message-link');
-
-  //   expect(messageLink).to.have.length(1);
-  //   // expect(messageLink.find({ href: URL_DEXCOM_CONNECT_INFO })).to.have.length(1);
-  //   expect(messageLink.text()).contains(expectedText);
-  // });
 
   it('should render a close link to dismiss the banner', () => {
     const closeLink = wrapper.find('a.close');
@@ -123,15 +98,6 @@ describe('UpdateTypeBanner', () => {
       expect(messageText).to.have.length(1);
       expect(messageText.text()).contains(expectedText);
     });
-
-    // it('should render a link to the dexcom connect info on the website', () => {
-    //   const expectedText = 'Learn More'
-    //   const messageLink = wrapper.find('.message-link');
-
-    //   expect(messageLink).to.have.length(1);
-    //   expect(messageLink.find({ href: URL_DEXCOM_CONNECT_INFO })).to.have.length(1);
-    //   expect(messageLink.text()).contains(expectedText);
-    // });
 
     it('should render a update my profile button', () => {
       const expectedText = 'Update My Profile'
