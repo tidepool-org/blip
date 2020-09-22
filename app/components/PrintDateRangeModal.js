bf668368ccfd36d84ab57a1f060aa3ab5aadad19
Clint Beacock <clint.beacock@gmail.com>
2020-09-22 13:13:59 -0400

    [WEB-981] Update print date range modal to set ranges per chart type

--- app/components/PrintDateRangeModal.js
+++ app/components/PrintDateRangeModal.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
 import map from 'lodash/map';
 import noop from 'lodash/noop';
-import isEqual from 'lodash/isEqual';
-import { Flex } from 'rebass/styled-components';
+import { Flex, Box } from 'rebass/styled-components';
+import { Label, Switch } from '@rebass/forms/styled-components';
 import moment from 'moment-timezone';
 
+import Accordion from './elements/Accordion';
 import Button from './elements/Button';
 import DateRangePicker from './elements/DateRangePicker';
 import {
@@ -14,15 +16,16 @@ import {
   DialogContent,
   DialogTitle,
 } from './elements/Dialog';
-import { Paragraph1, MediumTitle, Caption } from './elements/FontStyles';
+import { MediumTitle, Caption, Body1 } from './elements/FontStyles';
 
 export const PrintDateRangeModal = (props) => {
   const {
+    mostRecentDatumDates,
     onClose,
     onClickPrint,
     onDatesChange,
     open,
-    error,
+    errors,
     timePrefs: { timezoneName },
   } = props;
 
@@ -35,63 +38,190 @@ export const PrintDateRangeModal = (props) => {
     endDate: endDate ? moment(endDate).tz(timezoneName).endOf('day').subtract(1, 'ms') : null,
   });
 
-  const getLastNDays = (days) => {
+  const getLastNDays = (days, chartType) => {
+    const endDate = get(mostRecentDatumDates, chartType)
+      ? moment.utc(mostRecentDatumDates[chartType])
+      : endOfToday;
+
     return setDateRangeToExtents({
-      startDate: moment(endOfToday).tz(timezoneName).subtract(days - 1, 'days'),
-      endDate: endOfToday,
+      startDate: moment(endDate).tz(timezoneName).subtract(days - 1, 'days'),
+      endDate: endDate,
     });
   };
 
-  const presetOptions = [7, 14, 21, 30];
-  const presetDateRanges = useMemo(() => map(presetOptions, getLastNDays), [open]);
-  const [dates, setDates] = useState(getLastNDays(presetOptions[0]));
+  const basicsDaysOptions = [7,14,21];
+  const [basicsEnabled, setBasicsEnabled] = useState(true);
+  const [basicsDays, setBasicsDays] = useState(basicsDaysOptions[2]);
+
+  const bgLogDaysOptions = [7,14,21,30];
+  const [bgLogEnabled, setBgLogEnabled] = useState(true);
+  const [bgLogDays, setBgLogDays] = useState(bgLogDaysOptions[3]);
+
+  const dailyDaysOptions = [7,14,21];
+  const [dailyEnabled, setDailyEnabled] = useState(true);
+  const [dailyDays, setDailyDays] = useState(dailyDaysOptions[0]);
+
+  const [deviceSettingsEnabled, setDeviceSettingsEnabled] = useState(true);
+
+  const presetDateRanges = {
+    basics: useMemo(() => map(basicsDaysOptions, days => getLastNDays(days, 'basics')), [open]),
+    bgLog: useMemo(() => map(bgLogDaysOptions, days => getLastNDays(days, 'bgLog')), [open]),
+    daily: useMemo(() => map(dailyDaysOptions, days => getLastNDays(days, 'daily')), [open]),
+  };
+
+  const [dates, setDates] = useState({
+    basics: getLastNDays(basicsDays, 'basics'),
+    bgLog: getLastNDays(bgLogDays, 'bgLog'),
+    daily: getLastNDays(dailyDays, 'daily'),
+  });
+
   const [datePickerOpen, setDatePickerOpen] = useState(false);
 
   useEffect(() => {
     onDatesChange(dates);
-  }, [dates])
+  }, [dates]);
+
+  const datesMatchPreset = (dates, presetDates) => {
+    return moment(dates.startDate).isSame(presetDates.startDate) && moment(dates.endDate).isSame(presetDates.endDate);
+  };
+
+  // Accordion Panels
+  const [expandedPanel, setExpandedPanel] = React.useState('basics');
+
+  const handleAccordionChange = key => (event, isExpanded) => {
+    setExpandedPanel(key);
+  };
+
+  const accordionProps = (chartType, header) => ({
+    label: chartType,
+    key: chartType,
+    expanded: expandedPanel === chartType,
+    onChange: handleAccordionChange(chartType),
+    themeProps: {
+      wrapper: {
+        width: '100%',
+      },
+      panel: {
+        width: '100%',
+        px: 5,
+        pt: 1,
+        pb: 3,
+      },
+      header: {
+        width: '100%',
+        color: errors[chartType] ? 'feedback.danger' : undefined,
+      },
+    },
+    header,
+  });
+
+  const panels = [
+    {
+      days: basicsDays,
+      daysOptions: basicsDaysOptions,
+      enabled: basicsEnabled,
+      error: errors.basics,
+      header: 'Basics Chart',
+      key: 'basics',
+      setDays: setBasicsDays,
+      setEnabled: setBasicsEnabled,
+    },
+    {
+      days: dailyDays,
+      daysOptions: dailyDaysOptions,
+      enabled: dailyEnabled,
+      error: errors.daily,
+      header: 'Daily Charts',
+      key: 'daily',
+      setDays: setDailyDays,
+      setEnabled: setDailyEnabled,
+    },
+    {
+      days: bgLogDays,
+      daysOptions: bgLogDaysOptions,
+      enabled: bgLogEnabled,
+      error: errors.bgLog,
+      header: 'BG Log Chart',
+      key: 'bgLog',
+      setDays: setBgLogDays,
+      setEnabled: setBgLogEnabled,
+    },
+    {
+      enabled: deviceSettingsEnabled,
+      header: 'Device Settings',
+      key: 'deviceSettings',
+      setEnabled: setDeviceSettingsEnabled,
+    },
+  ];
 
   return (
     <Dialog id="printDateRangePicker" maxWidth="md" open={open} onClose={onClose}>
       <DialogTitle divider={false} onClose={onClose}>
         <MediumTitle>Print Report</MediumTitle>
       </DialogTitle>
-      <DialogContent divider pb={6}>
-        <Paragraph1>Number of days (most recent)</Paragraph1>
-        <Flex mb={4}>
-          {map(presetOptions, (days, i) => (
-            <Button
-              mx={1}
-              variant="chip"
-              id={`latest-${days}-days`}
-              key={i}
-              value={days}
-              selected={isEqual(dates, presetDateRanges[i])}
-              onClick={() => setDates(getLastNDays(days))}
-            >
-              {days} days
-            </Button>
-          ))}
-        </Flex>
-        <Paragraph1>Or select a custom date range</Paragraph1>
-        <DateRangePicker
-          startDate={dates.startDate}
-          startDateId="printDateRangeStart"
-          endDate={dates.endDate}
-          endDateId="printDateRangeEnd"
-          onDatesChange={dates => setDates(setDateRangeToExtents(dates))}
-          isOutsideRange={day => (endOfToday.diff(day) < 0)}
-          onFocusChange={input => setDatePickerOpen(!!input)}
-          themeProps={{
-            minWidth: '580px',
-            minHeight: datePickerOpen ? '300px' : undefined,
-          }}
-        />
-        {error && (
-          <Caption mt={2} color="feedback.danger">
-            Please select a date range
-          </Caption>
-        )}
+      <DialogContent divider={false} minWidth="400px" p={0}>
+        {map(panels, panel => (
+          <Accordion {...accordionProps(panel.key, panel.header)}>
+            <Box width="100%">
+              <Flex mb={4}>
+                <Label htmlFor={`enabled-${panel.key}`}>
+                  <Body1 alignSelf="center">Include {panel.header}</Body1>
+                  <Switch
+                    name={`enabled-${panel.key}`}
+                    ml={4}
+                    checked={panel.enabled}
+                    onClick={() => panel.setEnabled(enabled => !enabled)}
+                  />
+                </Label>
+              </Flex>
+
+              {panel.enabled && panel.days && (
+                <Box>
+                  <Box mb={5}>
+                    <Body1 mb={2}>Number of days (most recent)</Body1>
+                    <Flex>
+                      {map(panel.daysOptions, (days, i) => (
+                        <Button
+                          mr={2}
+                          variant="chip"
+                          id={`days-${panel.key}`}
+                          name={`days-${panel.key}`}
+                          key={`days-${panel.key}-${i}`}
+                          value={days}
+                          selected={datesMatchPreset(dates[panel.key], presetDateRanges[panel.key][i])}
+                          onClick={() => setDates({ ...dates, [panel.key]: getLastNDays(days, panel.key) })}
+                        >
+                          {days} days
+                        </Button>
+                      ))}
+                    </Flex>
+                  </Box>
+                  <Box mb={3}>
+                    <Body1 mb={2}>Or select a custom date range</Body1>
+                    <DateRangePicker
+                      startDate={dates[panel.key].startDate}
+                      startDateId={`${[panel.key]}-start-date`}
+                      endDate={dates[panel.key].endDate}
+                      endDateId={`${[panel.key]}-end-date`}
+                      onDatesChange={newDates => setDates({ ...dates, [panel.key]: setDateRangeToExtents(newDates) })}
+                      isOutsideRange={day => (endOfToday.diff(day) < 0)}
+                      onFocusChange={input => setDatePickerOpen(!!input)}
+                      themeProps={{
+                        minWidth: '580px',
+                        minHeight: datePickerOpen ? '300px' : undefined,
+                      }}
+                    />
+                  </Box>
+                </Box>
+              )}
+            </Box>
+            {panel.error && (
+              <Caption mt={2} color="feedback.danger">
+                Please select a date range
+              </Caption>
+            )}
+          </Accordion>
+        ))}
       </DialogContent>
       <DialogActions justifyContent="space-between" py={2}>
         <Button variant="textSecondary" onClick={onClose}>
@@ -106,7 +236,16 @@ export const PrintDateRangeModal = (props) => {
 };
 
 PrintDateRangeModal.propTypes = {
-  error: PropTypes.bool,
+  errors: PropTypes.shape({
+    basics: PropTypes.bool,
+    bgLog: PropTypes.bool,
+    daily: PropTypes.bool,
+  }),
+  mostRecentDatumDates: PropTypes.shape({
+    basics: PropTypes.number.isRequired,
+    bgLog: PropTypes.number.isRequired,
+    daily: PropTypes.number.isRequired,
+  }),
   onClickPrint: PropTypes.func,
   onClose: PropTypes.func,
   onDatesChange: PropTypes.func,
--- stories/PrintDateRangeModal.stories.js
+++ stories/PrintDateRangeModal.stories.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import moment from 'moment-timezone';
 import { withDesign } from 'storybook-addon-designs';
 import { action } from '@storybook/addon-actions';
 import { ThemeProvider } from 'styled-components';
@@ -7,23 +7,6 @@ import { ThemeProvider } from 'styled-components';
 import baseTheme from '../app/themes/baseTheme';
 import Button from '../app/components/elements/Button';
 import PrintDateRangeModal from '../app/components/PrintDateRangeModal';
-
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-} from '../app/components/elements/Dialog';
-
-import DatePicker from '../app/components/elements/DatePicker';
-import RadioGroup from '../app/components/elements/RadioGroup';
-import { Caption, Paragraph1, MediumTitle, Subheading } from '../app/components/elements/FontStyles';
-import moment from 'moment-timezone';
-import { Label, Switch } from '@rebass/forms/styled-components';
-import { Flex, Box } from 'rebass/styled-components';
-
-import Accordion from '../app/components/elements/Accordion';
-import { map } from 'lodash';
 
 /* eslint-disable max-len */
 
@@ -40,8 +23,14 @@ export default {
 };
 
 export const PrintDateRangeModalStory = () => {
+  const initialErrorState = {
+    basics: false,
+    bgLog: false,
+    daily: false,
+  };
+
   const [open, setOpen] = useState(true);
-  const [error, setError] = useState(false);
+  const [errors, setErrors] = useState(initialErrorState);
   const [submitted, setSubmitted] = useState(false);
 
   const handleClickOpen = () => setOpen(true);
@@ -49,10 +38,14 @@ export const PrintDateRangeModalStory = () => {
   const handleClose = () => {
     setOpen(false);
     setSubmitted(false);
-    setError(false);
+    setErrors(initialErrorState);
   };
 
-  const validateDates = ({ startDate, endDate }) => setError(!startDate || !endDate);
+  const validateDates = ({ basics, bgLog, daily }) => setErrors({
+    basics: !basics.startDate || !basics.endDate,
+    bgLog: !bgLog.startDate || !bgLog.endDate,
+    daily: !daily.startDate || !daily.endDate,
+  });
 
   return (
     <React.Fragment>
@@ -60,7 +53,12 @@ export const PrintDateRangeModalStory = () => {
         Open Print Dialog
       </Button>
       <PrintDateRangeModal
-        error={error}
+        errors={errors}
+        mostRecentDatumDates={{
+          basics: moment.utc().valueOf(),
+          bgLog: moment.utc().subtract(2, 'd').valueOf(),
+          daily: moment.utc().valueOf(),
+        }}
         open={open}
         onClose={handleClose}
         onClickPrint={dates => {
@@ -79,229 +77,6 @@ export const PrintDateRangeModalStory = () => {
 
 PrintDateRangeModalStory.story = {
   name: 'Print Date Range Modal',
-  parameters: {
-    design: {
-      type: 'figma',
-      url:
-        'https://www.figma.com/file/iuXkrpuLTXExSnuPJE3Jtn/Tidepool-Design-System---Sprint-1?node-id=2209%3A256',
-    },
-  },
-};
-
-export const AltPrintDateRangeModalStory = () => {
-  const [open, setOpen] = useState(true);
-  const [error, setError] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-
-  const [datePickerOpen, setDatePickerOpen] = useState(false);
-
-  const dateTypeOptions = [
-    { value: 'latest', label: 'Latest available data' },
-    { value: 'custom', label: 'Custom' },
-  ];
-
-  const dailyDaysOptions = [
-    { value: 7, label: '7' },
-    { value: 14, label: '14' },
-    { value: 21, label: '21' },
-  ];
-
-  const basicsDaysOptions = [
-    { value: 7, label: '7' },
-    { value: 14, label: '14' },
-    { value: 21, label: '21' },
-  ];
-
-  const bgLogDaysOptions = [
-    { value: 7, label: '7' },
-    { value: 14, label: '14' },
-    { value: 21, label: '21' },
-    { value: 30, label: '30' },
-  ];
-
-  const [basicsEnabled, setBasicsEnabled] = useState(true);
-  const [basicsDateType, setBasicsDateType] = useState(dateTypeOptions[0].value);
-  const [basicsDays, setBasicsDays] = useState(basicsDaysOptions[2].value);
-
-  const [dailyEnabled, setDailyEnabled] = useState(true);
-  const [dailyDateType, setDailyDateType] = useState(dateTypeOptions[0].value);
-  const [dailyDays, setDailyDays] = useState(dailyDaysOptions[0].value);
-
-  const [bgLogEnabled, setBgLogEnabled] = useState(true);
-  const [bgLogDateType, setBgLogDateType] = useState(dateTypeOptions[0].value);
-  const [bgLogDays, setBgLogDays] = useState(bgLogDaysOptions[3].value);
-
-  const [deviceSettingsEnabled, setDeviceSettingsEnabled] = useState(true);
-
-  const handleClickOpen = () => setOpen(true);
-
-  const handleClose = () => {
-    setOpen(false);
-    setSubmitted(false);
-    setError(false);
-  };
-
-  const validateDates = ({ startDate, endDate }) => setError(!startDate || !endDate);
-  const handleSubmit = (dates) => {
-    setSubmitted(true);
-    validateDates(dates);
-    action('Clicked Print')(dates);
-  };
-
-  const endOfToday = moment().tz('utc').endOf('day').subtract(1, 'ms');
-
-  // Accordion
-  const [expanded, setExpanded] = React.useState('basics');
-
-  const handleChange = key => (event, isExpanded) => {
-    setExpanded(key);
-  };
-
-  const accordionProps = (label, header) => ({
-    label,
-    key: label,
-    expanded: expanded === label,
-    onChange: handleChange(label),
-    themeProps: {
-      wrapper: {
-        width: '100%',
-      },
-      panel: {
-        width: '100%',
-        px: 5,
-        pt: 1,
-        pb: 3,
-      },
-      header: {
-        width: '100%',
-      },
-    },
-    header,
-  });
-
-  const panels = [
-    {
-      dateType: basicsDateType,
-      days: basicsDays,
-      daysOptions: basicsDaysOptions,
-      enabled: basicsEnabled,
-      header: 'Basics Chart',
-      key: 'basics',
-      setDateType: setBasicsDateType,
-      setDays: setBasicsDays,
-      setEnabled: setBasicsEnabled,
-    },
-    {
-      dateType: dailyDateType,
-      days: dailyDays,
-      daysOptions: dailyDaysOptions,
-      enabled: dailyEnabled,
-      header: 'Daily Charts',
-      key: 'daily',
-      setDateType: setDailyDateType,
-      setDays: setDailyDays,
-      setEnabled: setDailyEnabled,
-    },
-    {
-      dateType: bgLogDateType,
-      days: bgLogDays,
-      daysOptions: bgLogDaysOptions,
-      enabled: bgLogEnabled,
-      header: 'BG Log Chart',
-      key: 'bgLog',
-      setDateType: setBgLogDateType,
-      setDays: setBgLogDays,
-      setEnabled: setBgLogEnabled,
-    },
-    {
-      enabled: deviceSettingsEnabled,
-      header: 'Device Settings',
-      key: 'deviceSettings',
-      setEnabled: setDeviceSettingsEnabled,
-    },
-  ];
-
-  return (
-    <React.Fragment>
-      <Button variant="primary" onClick={handleClickOpen}>
-        Open Print Dialog
-      </Button>
-      <Dialog id="printDateRangePicker" open={open} onClose={handleClose}>
-        <DialogTitle divider={false} onClose={handleClose}>
-          <MediumTitle>Print Report</MediumTitle>
-        </DialogTitle>
-        <DialogContent divider={false} minWidth="400px" p={0}>
-          {map(panels, panel => (
-            <Accordion {...accordionProps(panel.key, panel.header)}>
-              <Box width="100%">
-                <Flex mb={4}>
-                  <Label htmlFor={`enabled-${panel.key}`}>
-                    <Caption alignSelf="center">Include {panel.header}</Caption>
-                    <Switch
-                      name={`enabled-${panel.key}`}
-                      ml={4}
-                      checked={panel.enabled}
-                      onClick={() => panel.setEnabled(enabled => !enabled)}
-                    />
-                  </Label>
-                </Flex>
-
-                {panel.enabled && panel.dateType && (
-                  <Box>
-                    <Box mb={3}>
-                      <RadioGroup
-                        label="Select the most recent date to include in the report"
-                        id={`date-type-${panel.key}`}
-                        name={`date-type-${panel.key}`}
-                        options={dateTypeOptions}
-                        value={panel.dateType}
-                        onChange={e => panel.setDateType(e.target.value)}
-                        variant="horizontal"
-                      />
-
-                      {panel.dateType === 'custom' && <DatePicker
-                        id={`date-custom-${panel.key}`}
-                        onFocusChange={input => setDatePickerOpen(!!input)}
-                        isOutsideRange={day => (endOfToday.diff(day) < 0)}
-                        themeProps={{
-                          mb: 3,
-                          minWidth: '320px',
-                          minHeight: datePickerOpen ? '300px' : undefined,
-                        }}
-                      />}
-                    </Box>
-                    <Box>
-                      <RadioGroup
-                        label="Days included in report"
-                        id={`days-${panel.key}`}
-                        name={`days-${panel.key}`}
-                        variant="horizontal"
-                        options={panel.daysOptions}
-                        value={panel.days}
-                        onChange={e => panel.setDays(parseInt(e.target.value, 10))}
-                      />
-                    </Box>
-                  </Box>
-                )}
-              </Box>
-            </Accordion>
-          ))}
-        </DialogContent>
-        <DialogActions justifyContent="space-between" py={2}>
-          <Button variant="textSecondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="textPrimary" onClick={handleSubmit}>
-            Print
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-  );
-};
-
-AltPrintDateRangeModalStory.story = {
-  name: 'Alt Print Date Range Modal',
   parameters: {
     design: {
       type: 'figma',
