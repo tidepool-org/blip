8c74d6149cd9c10e8b04929161f1b7383e2ca9c4
CedricPerotto <cedric.perotto@diabeloop.fr>
2020-11-05 12:14:18 +0100

    Add tests on field display

--- test/unit/pages/userprofile.test.js
+++ test/unit/pages/userprofile.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import TestUtils from 'react-dom/test-utils';
 import mutationTracker from 'object-invariant-test-helper';
@@ -6,6 +7,7 @@ import sinon from 'sinon';
 import chai from 'chai';
 import { shallow } from 'enzyme';
 
+import config from '../../../app/config';
 import { UserProfile, mapStateToProps } from '../../../app/pages/userprofile';
 
 const { assert, expect } = chai;
@@ -27,6 +29,18 @@ describe('UserProfile', function () {
         lastName: 'Dent'
       },
     },
+  };
+
+  const patientProps = {...props, 
+    ...{
+      user:{
+        profile:{
+          patient:{
+            fullName: 'Gordon Dent'
+          }
+        }
+      }
+    }
   };
 
   before(() => {
@@ -80,6 +94,93 @@ describe('UserProfile', function () {
       expect(props.trackMetric.callCount).to.equal(2);
       expect(props.router.goBack.callCount).to.equal(1);
     });
+  });
+
+  describe('config injection', function() {
+    let container;
+    const getInputs = () => {
+      return {
+        firstNameInput: container.querySelector('#firstName'),
+        lastNameInput : container.querySelector('#lastName'),
+        emailInput: container.querySelector('#username'),
+        passwordInput: container.querySelector('#password'),
+        passwordConfirmInput: container.querySelector('#passwordConfirm')
+      };
+    };
+    
+    beforeEach(() => {
+      config.ALLOW_PATIENT_CHANGE_EMAIL=true;
+      config.ALLOW_PATIENT_CHANGE_PASSWORD=true;
+      config.ALLOW_PATIENT_CHANGE_NAME=true;
+      container = document.createElement('div');
+      document.body.appendChild(container);
+    });
+    afterEach(() => {
+      document.body.removeChild(container);
+      container = null;
+    });
+    it('should display all fields enabled when everything is allowed for a patient', function() {
+      TestUtils.act(() => {
+        ReactDOM.render(<UserProfile {...patientProps} />, container);
+      });
+      const inputs = getInputs();
+      expect(inputs.firstNameInput.disabled).to.equal(false);
+      expect(inputs.lastNameInput.disabled).to.equal(false);
+      expect(inputs.emailInput.disabled).to.equal(false);
+      expect(inputs.passwordInput.disabled).to.equal(false);
+      expect(inputs.passwordConfirmInput.disabled).to.equal(false);
+    });
+    it('should display name fields disabled when config is disallowed for a patient', function() {
+      config.ALLOW_PATIENT_CHANGE_NAME=false;
+      TestUtils.act(() => {
+        ReactDOM.render(<UserProfile {...patientProps} />, container);
+      });
+      const inputs = getInputs();
+      expect(inputs.firstNameInput.disabled).to.equal(true);
+      expect(inputs.lastNameInput.disabled).to.equal(true);
+      expect(inputs.emailInput.disabled).to.equal(false);
+      expect(inputs.passwordInput.disabled).to.equal(false);
+      expect(inputs.passwordConfirmInput.disabled).to.equal(false);
+    });
+    it('should not display email field when config is disallowed for a patient', function() {
+      config.ALLOW_PATIENT_CHANGE_EMAIL=false;
+      TestUtils.act(() => {
+        ReactDOM.render(<UserProfile {...patientProps} />, container);
+      });
+      const inputs = getInputs();
+      expect(inputs.firstNameInput.disabled).to.equal(false);
+      expect(inputs.lastNameInput.disabled).to.equal(false);
+      expect(inputs.emailInput).to.equal(null);
+      expect(inputs.passwordInput.disabled).to.equal(false);
+      expect(inputs.passwordConfirmInput.disabled).to.equal(false);
+    });
+    it('should not display password fields when config is disallowed for a patient', function() {
+      config.ALLOW_PATIENT_CHANGE_PASSWORD=false;
+      TestUtils.act(() => {
+        ReactDOM.render(<UserProfile {...patientProps} />, container);
+      });
+      const inputs = getInputs();
+      expect(inputs.firstNameInput.disabled).to.equal(false);
+      expect(inputs.lastNameInput.disabled).to.equal(false);
+      expect(inputs.emailInput.disabled).to.equal(false);
+      expect(inputs.passwordInput).to.equal(null);
+      expect(inputs.passwordConfirmInput).to.equal(null);
+    });
+    it('should display all fields enabled when everything is disallowed for a non patient', function() {
+      config.ALLOW_PATIENT_CHANGE_EMAIL=false;
+      config.ALLOW_PATIENT_CHANGE_PASSWORD=false;
+      config.ALLOW_PATIENT_CHANGE_NAME=false;
+      TestUtils.act(() => {
+        ReactDOM.render(<UserProfile {...props} />, container);
+      });
+      const inputs = getInputs();
+      expect(inputs.firstNameInput.disabled).to.equal(false);
+      expect(inputs.lastNameInput.disabled).to.equal(false);
+      expect(inputs.emailInput.disabled).to.equal(false);
+      expect(inputs.passwordInput.disabled).to.equal(false);
+      expect(inputs.passwordConfirmInput.disabled).to.equal(false);
+    });
+
   });
 
   describe('mapStateToProps', () => {
