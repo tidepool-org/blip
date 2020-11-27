a98e5e7d373e3a05f13c21fff033855f73082be5
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2020-11-27 14:49:22 +0000

    YLP-147 Cleanup packages

--- packages/blip/build-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# add configuration
-. ./config/env.docker.sh
-
-export NODE_OPTIONS='--max-old-space-size=4096'
-npm run build-dev
--- packages/viz/postcss.config.js
+++
@@ -1,6 +0,0 @@
-const calc = require('postcss-calc');
-const cssVariables = require('postcss-custom-properties');
-
-module.exports = {
-  plugins: [calc, cssVariables],
-};
