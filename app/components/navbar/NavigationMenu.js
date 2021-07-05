6edae8bdccc2eba14fb0db570ec63c33843ebc30
Clint Beacock <clint.beacock@gmail.com>
2021-07-05 10:05:28 -0400

    [WEB-880] Add new navigation menu component

---
+++ app/components/navbar/NavigationMenu.js
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { translate } from 'react-i18next';
+import { push } from 'connected-react-router';
+import filter from 'lodash/filter';
+import forEach from 'lodash/forEach';
+import has from 'lodash/has';
+import map from 'lodash/map';
+import values from 'lodash/values';
+import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
+import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
+import { Flex , Box } from 'rebass/styled-components';
+
+import {
+  usePopupState,
+  bindPopover,
+  bindTrigger,
+} from 'material-ui-popup-state/hooks';
+
+import * as actions from '../../redux/actions';
+import Button from '../elements/Button';
+import Popover from '../elements/Popover';
+import personUtils from '../../core/personutils';
+import { borders, colors } from '../../themes/baseTheme';
+
+export const NavigationMenu = props => {
+  const { t, api } = props;
+  const dispatch = useDispatch();
+  const loggedInUserId = useSelector((state) => state.blip.loggedInUserId);
+  const allUsersMap = useSelector((state) => state.blip.allUsersMap);
+  const clinics = useSelector((state) => state.blip.clinics);
+  const { fetchingClinicsForClinician } = useSelector((state) => state.blip.working);
+
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'navigationMenu',
+  });
+
+  const personalWorkspaceOption = {
+    action: handleSelectWorkspace.bind(null, null),
+    icon: SupervisedUserCircleRoundedIcon,
+    label: t('Personal Workspace'),
+  };
+
+  const accountSettingsOption = {
+    action: () => dispatch(push('/profile')),
+    icon: SettingsRoundedIcon,
+    label: t('Account Settings'),
+  };
+
+  const logoutOption = {
+    action: () => dispatch(actions.async.logout(api)),
+    icon: ExitToAppRoundedIcon,
+    label: t('Logout'),
+  };
+
+  const [menuOptions, setMenuOptions] = useState([
+    personalWorkspaceOption,
+    accountSettingsOption,
+    logoutOption,
+  ]);
+
+  // Fetchers
+  useEffect(() => {
+    if (loggedInUserId && personUtils.isClinicianAccount(allUsersMap?.[loggedInUserId])) {
+      forEach([
+        {
+          workingState: fetchingClinicsForClinician,
+          action: actions.async.getClinicsForClinician.bind(null, api, loggedInUserId),
+        },
+      ], ({ workingState, action }) => {
+        if (
+          !workingState.inProgress &&
+          !workingState.completed &&
+          !workingState.notification
+        ) {
+          dispatch(action());
+        }
+      });
+    }
+  }, [loggedInUserId]);
+
+  useEffect(() => {
+    const userClinics = filter(values(clinics), ({ clinicians }) => has(clinicians, loggedInUserId));
+
+    setMenuOptions([
+      personalWorkspaceOption,
+      ...map(userClinics, clinic => ({
+        action: handleSelectWorkspace.bind(null, clinic.id),
+        icon: DashboardRoundedIcon,
+        label: t('{{name}} Workspace', { name: clinic.name }),
+      })),
+      accountSettingsOption,
+      logoutOption,
+    ]);
+  }, [clinics]);
+
+  function handleSelectWorkspace(clinicId) {
+    dispatch(actions.sync.selectClinic(clinicId));
+    dispatch(push('/patients'));
+  }
+
+  function handleMenuAction(menuOption) {
+    menuOption.action();
+    popupState.close();
+  }
+
+  return (
+    <Flex width="auto" justifyContent="flex-end">
+      <Button
+        variant="textPrimary"
+        color="text.primary"
+        fontSize={2}
+        {...bindTrigger(popupState)}
+        icon={KeyboardArrowDownRoundedIcon}
+        iconLabel="Search By"
+        sx={{
+          '&:hover': {
+            color: colors.purpleDark,
+          },
+        }}
+      >
+        {personUtils.patientFullName(allUsersMap?.[loggedInUserId])}
+      </Button>
+
+      <Popover
+        width="15em"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        {...bindPopover(popupState)}
+      >
+        <Box py={2}>
+          {map(menuOptions, (option, key) => (
+            <Button
+              variant="textPrimary"
+              color="text.primary"
+              width="100%"
+              py={3}
+              pr={3}
+              ml={3}
+              justifyContent="flex-end"
+              key={key}
+              fontSize={2}
+              icon={option.icon}
+              iconLabel={option.label}
+              iconPosition="left"
+              onClick={() => handleMenuAction(option)}
+              sx={{
+                borderBottom: borders.divider,
+                '&:hover': {
+                  color: colors.purpleDark,
+                },
+                '&:last-child': {
+                  borderBottom: 'none',
+                },
+              }}
+            >
+              {option.label}
+            </Button>
+          ))}
+        </Box>
+      </Popover>
+    </Flex>
+  );
+};
+
+NavigationMenu.propTypes = {
+  api: PropTypes.object.isRequired,
+  trackMetric: PropTypes.func.isRequired,
+};
+
+export default translate()(NavigationMenu);
--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -5,11 +5,11 @@ import { Flex } from 'rebass/styled-components'
 import { translate } from 'react-i18next';
 
 import _ from 'lodash';
-import cx from 'classnames';
 
 import personUtils from '../../core/personutils';
-import NavbarPatientCard from '../../components/navbarpatientcard';
-import WorkspaceSwitcher from '../../components/clinic/WorkspaceSwitcher';
+import NavbarPatientCard from '../navbarpatientcard';
+import WorkspaceSwitcher from '../clinic/WorkspaceSwitcher';
+import NavigationMenu from './NavigationMenu';
 
 import logoSrc from './images/tidepool-logo-408x46.png';
 export default translate()(class extends React.Component {
@@ -74,16 +74,13 @@ export default translate()(class extends React.Component {
   renderMiddleSection = () => {
     var patient = this.props.patient;
 
-    console.log('this.props.currentPage', this.props.currentPage);
-
     if (_.isEmpty(patient)) {
       if (
         this.props.currentPage === '/patients' &&
-        personUtils.isClinic(this.props.user) &&
-        personUtils.flaggedForClinicWorkflow(this.props.user)
+        personUtils.isClinicianAccount(this.props.user)
       ) {
         return (
-          <Flex alignItems="center" justifyContent="center">
+          <Flex flex={1} alignItems="center" justifyContent="center">
             <WorkspaceSwitcher api={this.props.api} />
           </Flex>
         );
@@ -126,14 +123,12 @@ export default translate()(class extends React.Component {
   };
 
   renderMenuSection = () => {
-    var currentPage = (this.props.currentPage && this.props.currentPage[0] === '/') ? this.props.currentPage.slice(1) : this.props.currentPage;
-    const {user, t} = this.props;
+    const {user} = this.props;
 
     if (_.isEmpty(user)) {
       return <div className="Navbar-menuSection"></div>;
     }
 
-    var displayName = this.getUserDisplayName();
     var self = this;
     var handleClickUser = function() {
       self.props.trackMetric('Clicked Navbar Logged In User');
@@ -143,64 +138,11 @@ export default translate()(class extends React.Component {
     var handleCareteam = function() {
       self.props.trackMetric('Clicked Navbar CareTeam');
     };
-    var patientsClasses = cx({
-      'Navbar-button': true,
-      'Navbar-selected': currentPage && currentPage === 'patients',
-    });
-
-    var accountSettingsClasses = cx({
-      'Navbar-button': true,
-      'Navbar-dropdownIcon-show': currentPage && currentPage === 'profile',
-    });
-
-    var dropdownClasses = cx({
-      'Navbar-menuDropdown': true,
-      'Navbar-menuDropdown-hide': !self.state.showDropdown,
-    });
-
-    var dropdownIconClasses = cx({
-      'Navbar-dropdownIcon': true,
-      'Navbar-dropdownIcon-show': self.state.showDropdown,
-      'Navbar-dropdownIcon-current': currentPage && currentPage === 'profile',
-    });
-
-    var dropdownIconIClasses = cx({
-      'Navbar-icon': true,
-      'icon-account--down': !self.state.showDropdown,
-      'icon-account--up': self.state.showDropdown,
-    });
 
     return (
-      <ul className="Navbar-menuSection" ref="user">
-        <li className="Navbar-menuItem">
-          <Link to="/patients" title="Care Team" onClick={handleCareteam} className={patientsClasses} ref="careteam"><i className="Navbar-icon icon-careteam"></i></Link>
-        </li>
-        <li className={dropdownIconClasses}>
-          <div onClick={this.toggleDropdown}>
-            <i className='Navbar-icon Navbar-icon-profile icon-profile'></i>
-            <div className="Navbar-logged">
-              <span className="Navbar-loggedInAs">{t('Logged in as ')}</span>
-              <span className="Navbar-userName" ref="userFullName" title={displayName}>{displayName}</span>
-            </div>
-            <i className='Navbar-icon Navbar-icon-down icon-arrow-down'></i>
-            <div className='clear'></div>
-          </div>
-          <div onClick={this.stopPropagation} className={dropdownClasses}>
-            <ul>
-              <li>
-                <Link to="/profile" title={t('Account')} onClick={handleClickUser} className={accountSettingsClasses}>
-                  <i className='Navbar-icon icon-settings'></i><span className="Navbar-menuText">{t('Account Settings')}</span>
-                </Link>
-              </li>
-              <li>
-                <a href="" title={t('Logout')} onClick={this.handleLogout} className="Navbar-button" ref="logout">
-                  <i className='Navbar-icon icon-logout'></i><span className="Navbar-menuText">{t('Logout')}</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
+      <Flex flex={1} justifyContent="flex-end">
+        <NavigationMenu api={this.props.api} />
+      </Flex>
     );
   };
 
