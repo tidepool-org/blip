70ff8c35c340502b715ba05d55d74a5f2b98db48
Clint Beacock <clint.beacock@gmail.com>
2021-08-10 16:40:14 -0400

    [WEB-880] Update workspace switcher to only show personal workspace when appropriate

--- app/components/clinic/WorkspaceSwitcher.js
+++ app/components/clinic/WorkspaceSwitcher.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { push } from 'connected-react-router';
 import { translate } from 'react-i18next';
+import get from 'lodash/get';
 import filter from 'lodash/filter';
 import find from 'lodash/find';
 import has from 'lodash/has';
@@ -10,7 +11,7 @@ import map from 'lodash/map';
 import values from 'lodash/values';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
-import { Flex , Box } from 'rebass/styled-components';
+import { Flex, Box, Text } from 'rebass/styled-components';
 
 import {
   usePopupState,
@@ -27,17 +28,19 @@ export const WorkspaceSwitcher = props => {
   const { t, trackMetric } = props;
   const dispatch = useDispatch();
   const loggedInUserId = useSelector((state) => state.blip.loggedInUserId);
+  const allUsersMap = useSelector((state) => state.blip.allUsersMap);
   const clinics = useSelector((state) => state.blip.clinics);
+  const membershipInOtherCareTeams = useSelector((state) => state.blip.membershipInOtherCareTeams);
   const selectedClinicId = useSelector((state) => state.blip.selectedClinicId);
+  const hasPatientProfile = !!get(allUsersMap, [loggedInUserId, 'profile', 'patient'], false);
 
   const popupState = usePopupState({
     variant: 'popover',
     popupId: 'jumpMenu',
   });
 
-  const personalWorkspaceOption = { id: null, label: t('Personal Workspace') };
 
-  const [menuOptions, setMenuOptions] = useState([personalWorkspaceOption])
+  const [menuOptions, setMenuOptions] = useState([])
   const [selectedClinic, setSelectedClinic] = useState(menuOptions[0]);
 
   useEffect(() => {
@@ -48,13 +51,21 @@ export const WorkspaceSwitcher = props => {
   useEffect(() => {
     const userClinics = filter(values(clinics), ({ clinicians }) => has(clinicians, loggedInUserId));
 
-    setMenuOptions([
-      ...map(userClinics, clinic => ({
-        id: clinic.id,
-        label: t('{{name}} Workspace', { name: clinic.name }),
-      })),
-      personalWorkspaceOption,
-    ]);
+    if (userClinics.length) {
+      const personalWorkspaceOption = { id: null, label: t('Personal Workspace') };
+      const hidePersonalWorkspaceOption = !hasPatientProfile && !membershipInOtherCareTeams.length;
+
+      const options = [
+        ...map(userClinics, clinic => ({
+          id: clinic.id,
+          label: t('{{name}} Workspace', { name: clinic.name }),
+        })),
+      ];
+
+      if (!hidePersonalWorkspaceOption) options.push(personalWorkspaceOption);
+
+      setMenuOptions(options);
+    }
   }, [clinics]);
 
   const handleSelect = option => {
@@ -67,67 +78,75 @@ export const WorkspaceSwitcher = props => {
     popupState.close();
   };
 
-  return (menuOptions.length > 1) ? (
+  return menuOptions.length ? (
     <Flex id="workspace-switcher" width="400px" justifyContent="center">
-      <Button
-        id="workspace-switcher-current"
-        variant="textPrimary"
-        color="text.primary"
-        fontSize={2}
-        {...bindTrigger(popupState)}
-        icon={KeyboardArrowDownRoundedIcon}
-        iconLabel={t('Open navigation menu')}
-        sx={{
-          '&:hover': {
-            color: colors.purpleDark,
-          },
-        }}
-      >
-        {selectedClinic?.label}
-      </Button>
+      {menuOptions.length > 1 && (
+        <>
+          <Button
+            id="workspace-switcher-current"
+            variant="textPrimary"
+            color="text.primary"
+            fontSize={2}
+            {...bindTrigger(popupState)}
+            icon={KeyboardArrowDownRoundedIcon}
+            iconLabel={t('Open navigation menu')}
+            sx={{
+              '&:hover': {
+                color: colors.purpleDark,
+              },
+            }}
+          >
+            {selectedClinic?.label}
+          </Button>
 
-      <Popover
-        width="15em"
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        {...bindPopover(popupState)}
-      >
-        <Box py={2}>
-          {map(menuOptions, (option, key) => (
-            <Button
-              className="workspace-option"
-              variant="textPrimary"
-              color="text.primary"
-              width="100%"
-              pt={2}
-              pb={3}
-              px={3}
-              justifyContent="space-between"
-              key={key}
-              fontSize={2}
-              icon={option.id === selectedClinic.id ? CheckRoundedIcon : null}
-              iconLabel={t('Selected')}
-              onClick={() => handleSelect(option)}
-              sx={{
-                '&:hover': {
-                  color: colors.purpleDark,
-                },
-                '&:last-child': {
-                  pb: 2,
-                },
-              }}
-            >
-              {option.label}
-            </Button>
-          ))}
-        </Box>
-      </Popover>
+          <Popover
+            width="15em"
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            {...bindPopover(popupState)}
+          >
+            <Box py={2}>
+              {map(menuOptions, (option, key) => (
+                <Button
+                  className="workspace-option"
+                  variant="textPrimary"
+                  color="text.primary"
+                  width="100%"
+                  pt={2}
+                  pb={3}
+                  px={3}
+                  justifyContent="space-between"
+                  key={key}
+                  fontSize={2}
+                  icon={option.id === selectedClinic?.id ? CheckRoundedIcon : null}
+                  iconLabel={t('Selected')}
+                  onClick={() => handleSelect(option)}
+                  sx={{
+                    '&:hover': {
+                      color: colors.purpleDark,
+                    },
+                    '&:last-child': {
+                      pb: 2,
+                    },
+                  }}
+                >
+                  {option.label}
+                </Button>
+              ))}
+            </Box>
+          </Popover>
+        </>
+      )}
+
+      {menuOptions.length === 1 && (
+        <Text fontSize={2} color="text.primary">{selectedClinic?.label}</Text>
+      )}
     </Flex>
   ) : null;
 };
