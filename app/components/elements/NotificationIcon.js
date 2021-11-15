59e7cc23eef57953bec14bf7f5ede3974109a8a6
Clint Beacock <clint.beacock@gmail.com>
2021-11-15 11:55:56 -0500

    [WEB-1290] Add NotificationIcon component

---
+++ app/components/elements/NotificationIcon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, BoxProps } from 'rebass/styled-components';
+import { colors, radii } from '../../themes/baseTheme';
+
+const NotificationIcon = props => (
+  <Box
+    ml={2}
+    bg={colors.pinks[5]}
+    width="8px"
+    height="8px"
+    sx={{
+      borderRadius: radii.full,
+    }}
+    {...props}
+  />
+);
+
+NotificationIcon.propTypes = {
+  ...BoxProps,
+};
+
+export default NotificationIcon;
