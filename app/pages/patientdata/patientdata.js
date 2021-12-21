8c11c69dbebe790d3347afe9bbf2f496c33ea668
Clint Beacock <clint.beacock@gmail.com>
2021-12-21 14:08:26 -0500

    [WEB-1204] Render avg. daily insulin ratio on trends view

--- app/pages/patientdata/patientdata.js
+++ app/pages/patientdata/patientdata.js
@@ -146,6 +146,9 @@ export const PatientDataClass = createReactClass({
           focusedSmbgRangeAvg: null,
           showingCbgDateTraces: false,
           touched: false,
+          stats: {
+            excludeDaysWithoutBolus: false,
+          },
         },
         bgLog: {
           bgSource: 'smbg',
@@ -578,7 +581,7 @@ export const PatientDataClass = createReactClass({
           id='exclude-bolus-info'
           label={(
             <Checkbox
-              checked={_.get(state, 'chartPrefs.basics.stats.excludeDaysWithoutBolus')}
+              checked={_.get(state, ['chartPrefs', state.chartType, 'stats', 'excludeDaysWithoutBolus'])}
               label={t('Exclude days with no boluses')}
               onChange={this.toggleDaysWithoutBoluses}
               themeProps={{
@@ -634,8 +637,8 @@ export const PatientDataClass = createReactClass({
     }
 
     const prefs = _.cloneDeep(this.state.chartPrefs);
-    prefs.basics.stats.excludeDaysWithoutBolus = !prefs.basics.stats.excludeDaysWithoutBolus;
-    if (prefs.basics.stats.excludeDaysWithoutBolus) this.props.trackMetric('Basics exclude days without boluses');
+    prefs[this.state.chartType].stats.excludeDaysWithoutBolus = !prefs[this.state.chartType].stats.excludeDaysWithoutBolus;
+    if (prefs[this.state.chartType].stats.excludeDaysWithoutBolus) this.props.trackMetric(`${_.capitalize(this.state.chartType)} exclude days without boluses`);
     this.updateChartPrefs(prefs, false, true, true);
   },
 
@@ -688,7 +691,7 @@ export const PatientDataClass = createReactClass({
         manufacturer,
       });
 
-      if (statType === 'totalInsulin' && chartType === 'basics') {
+      if (statType === 'totalInsulin' && _.includes(['basics', 'trends'], chartType)) {
         // We nest the averageDailyDose stat within the totalInsulin stat
         stat.title = props.t('Avg. Daily Insulin Ratio');
         delete stat.dataFormat.title;
@@ -730,6 +733,9 @@ export const PatientDataClass = createReactClass({
             />
           </Box>
         );
+
+        console.log('daysWithBoluses', daysWithBoluses);
+        console.log('activeDays', activeDays);
 
         if (daysWithBoluses > 0 && daysWithBoluses < activeDays) {
           // If any of the calendar dates within the range are missing boluses,
@@ -779,7 +785,7 @@ export const PatientDataClass = createReactClass({
         aggregationsByDate: 'basals, boluses, fingersticks, siteChanges',
         bgSource: _.get(this.state.chartPrefs, 'basics.bgSource'),
         stats: this.getStatsByChartType('basics'),
-        excludeDaysWithoutBolus: _.get(this.state, ['chartPrefs', this.state.chartType, 'stats', 'excludeDaysWithoutBolus']),
+        excludeDaysWithoutBolus: _.get(this.state, 'chartPrefs.basics.stats.excludeDaysWithoutBolus'),
         ...commonQueries,
       };
     }
@@ -1162,9 +1168,15 @@ export const PatientDataClass = createReactClass({
   getAggregationsByChartType: function(chartType = this.state.chartType) {
     let aggregations;
 
+    console.log('chartType', chartType);
+
     switch (chartType) {
       case 'basics':
         aggregations = 'basals, boluses, fingersticks, siteChanges';
+        break;
+
+      case 'trends':
+        aggregations = 'boluses';
         break;
 
       default:
@@ -1223,6 +1235,8 @@ export const PatientDataClass = createReactClass({
         cbgSelected && stats.push(commonStats.timeInRange);
         smbgSelected && stats.push(commonStats.readingsInRange);
         stats.push(commonStats.averageGlucose);
+        stats.push(commonStats.totalInsulin);
+        stats.push(commonStats.averageDailyDose);
         cbgSelected && stats.push(commonStats.sensorUsage);
         isAutomatedBasalDevice && stats.push(commonStats.timeInAuto);
         isSettingsOverrideDevice && stats.push(commonStats.timeInOverride);
@@ -1618,6 +1632,8 @@ export const PatientDataClass = createReactClass({
             cbg: {},
             smbg: {},
           };
+
+          chartQuery.aggregationsByDate = 'boluses';
           break;
       }
 
