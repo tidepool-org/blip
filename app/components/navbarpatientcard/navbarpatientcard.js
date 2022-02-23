339630b6fd41c6d2fdf6625971e497cb4e5879b2
bcecilio <brendon.cecilio@gmail.com>
2022-02-23 11:38:36 -0500

    delete commented variable

--- app/components/navbarpatientcard/navbarpatientcard.js
+++ app/components/navbarpatientcard/navbarpatientcard.js
@@ -57,7 +57,7 @@ var NavbarPatientCard = translate()(class extends React.Component {
     var month = new Intl.DateTimeFormat('en-US', options).format(dateOfBirth);
     var day = dateOfBirth.getUTCDate();
     var year = dateOfBirth.getFullYear();
-    // var dob = this.renderView(dateOfBirth);
+ 
     return (
       <div className={classes}>
         <i className="Navbar-icon icon-face-standin"></i>
