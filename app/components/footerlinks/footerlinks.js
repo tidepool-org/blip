21036d9785d6d352a0f0ddc43246529964893ba4
Guillaume Charest <guillaume.w.charest@gmail.com>
2022-03-24 19:00:03 -0400

    Import i18next to component

--- app/components/footerlinks/footerlinks.js
+++ app/components/footerlinks/footerlinks.js
@@ -17,14 +17,18 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
+import i18next from '../../core/language';
 
 require('./images/jdrf.png');
 require('./images/jdrf_hover.png');
 
+
+const t = i18next.t.bind(i18next);
+
 const FooterLinks = (props) => {
   const metricFnMkr = (link) => {
     return () => { props.trackMetric(`Clicked Footer ${link}`); };
-  }
+  };
   return (
     <div className='footer-section footer-section-top'>
       <div className='footer-link social-media large-format-only'>
@@ -37,7 +41,7 @@ const FooterLinks = (props) => {
           rel="noreferrer noopener"
         >
           <svg viewBox="0 0 49 35">
-            <path d="M43.94 9.783c0-.376 0-.75-.02-1.127 1.957-1.23 3.66-2.783 5.012-4.54-1.802.7-3.74 1.16-5.777 1.382 2.076-1.076 3.662-2.8 4.426-4.85-1.938 1.008-4.092 1.725-6.383 2.118C39.357 1.06 36.753 0 33.874 0 28.334 0 23.83 3.927 23.83 8.76c0 .68.097 1.347.254 1.996-8.34-.358-15.743-3.858-20.697-9.15-.86 1.296-1.35 2.8-1.35 4.404 0 3.04 1.78 5.72 4.464 7.29-1.644-.05-3.19-.444-4.542-1.093v.12c0 4.234 3.466 7.785 8.048 8.588-.842.205-1.723.307-2.644.307-.646 0-1.272-.05-1.88-.154 1.273 3.483 4.994 6.01 9.38 6.078-3.447 2.356-7.774 3.756-12.473 3.756-.804 0-1.607-.034-2.39-.12 4.425 2.46 9.712 3.91 15.37 3.91 18.465 0 28.57-13.35 28.57-24.91z"/>
+            <path d="M43.94 9.783c0-.376 0-.75-.02-1.127 1.957-1.23 3.66-2.783 5.012-4.54-1.802.7-3.74 1.16-5.777 1.382 2.076-1.076 3.662-2.8 4.426-4.85-1.938 1.008-4.092 1.725-6.383 2.118C39.357 1.06 36.753 0 33.874 0 28.334 0 23.83 3.927 23.83 8.76c0 .68.097 1.347.254 1.996-8.34-.358-15.743-3.858-20.697-9.15-.86 1.296-1.35 2.8-1.35 4.404 0 3.04 1.78 5.72 4.464 7.29-1.644-.05-3.19-.444-4.542-1.093v.12c0 4.234 3.466 7.785 8.048 8.588-.842.205-1.723.307-2.644.307-.646 0-1.272-.05-1.88-.154 1.273 3.483 4.994 6.01 9.38 6.078-3.447 2.356-7.774 3.756-12.473 3.756-.804 0-1.607-.034-2.39-.12 4.425 2.46 9.712 3.91 15.37 3.91 18.465 0 28.57-13.35 28.57-24.91z" />
           </svg>
         </a>
         <a
@@ -59,19 +63,19 @@ const FooterLinks = (props) => {
           id='mobile'
           onClick={metricFnMkr('Mobile App')}
           target="_blank"
-          rel="noreferrer noopener">Get Mobile App</a>
+          rel="noreferrer noopener">{t('Get Mobile App')}</a>
         <a
           href="http://support.tidepool.org/"
           id='support'
           onClick={metricFnMkr('Support')}
           target="_blank"
-          rel="noreferrer noopener">Get Support</a>
+          rel="noreferrer noopener">{t('Get Support')}</a>
         <a
           href='http://tidepool.org/legal/'
           id='legal'
           onClick={metricFnMkr('PP and TOU')}
           target='_blank'
-          rel="noreferrer noopener">Privacy and Terms of Use</a>
+          rel="noreferrer noopener">{t('Privacy and Terms of Use')}</a>
       </div>
       <div className='footer-link footer-jdrf'>
         <a
@@ -80,7 +84,7 @@ const FooterLinks = (props) => {
           onClick={metricFnMkr('JDRF')}
           target='_blank'
           rel="noreferrer noopener">
-          Made possible by
+          {t('Made possible by')}
           <img />
         </a>
       </div>
