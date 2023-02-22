bb311b1645fdf39c476b478b820ede91a9024a57
Chris McGee <krystophv@gmail.com>
2023-02-21 23:01:19 -0500

    [UPLOAD-1000] prevent double launch attempt on re-render

--- app/pages/uploadredirect/uploadredirect.js
+++ app/pages/uploadredirect/uploadredirect.js
@@ -6,11 +6,13 @@ import { Redirect } from 'react-router-dom';
 import { Subheading, Title } from '../../components/elements/FontStyles';
 import Button from '../../components/elements/Button';
 import UAParser from 'ua-parser-js';
+import { useIsFirstRender } from '../../core/hooks';
 
 let launched = false;
 
 const UploadRedirect = (props) => {
   const { t } = props;
+  const isFirstRender = useIsFirstRender();
   const linkUrl = `tidepooluploader://localhost/keycloak-redirect${props.location.hash}`;
   const ua = new UAParser().getResult();
   let openText = 'Open Tidepool Uploader';
@@ -31,7 +33,7 @@ const UploadRedirect = (props) => {
     return <Redirect to="/login" />;
   }
 
-  if (!launched) {
+  if (!launched && isFirstRender) {
     if (props.location.hash) {
       customProtocolCheck(
         linkUrl,
