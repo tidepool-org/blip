bb73d7c599862048bd880d4ac0a08d03cbe7e083
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2023-03-02 21:33:06 +0100

    Add firefox as supported browser

--- app/core/utils.js
+++ app/core/utils.js
@@ -69,7 +69,7 @@ utils.getIn = (obj, props, notFound) => {
 
 utils.isSupportedBrowser = () => {
   var userAgent = navigator.userAgent.toLowerCase();
-  return (userAgent.indexOf('chrome') > -1 && userAgent.indexOf('opr') === -1 && userAgent.indexOf('mobi') === -1);
+  return (userAgent.indexOf('chrome') > -1 && userAgent.indexOf('opr') === -1 && userAgent.indexOf('mobi') === -1 && userAgent.indexOf('firefox') === -1);
 };
 
 utils.isMobile = () => {
