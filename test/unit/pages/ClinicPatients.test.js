54a653f05123c5d48ea6fd370f996f6f63da2414
Chris McGee <chris@tidepool.org>
2023-09-25 17:13:48 -0400

    fix unit test

--- test/unit/pages/ClinicPatients.test.js
+++ test/unit/pages/ClinicPatients.test.js
@@ -2122,7 +2122,7 @@ describe('ClinicPatients', () => {
           it('should use the stored sort parameters when fetching the initial patient list', () => {
             sinon.assert.calledWith(defaultProps.api.clinics.getPatientsForClinic, 'clinicID123', sinon.match({
               ...defaultFetchOptions,
-              sort: '-averageGlucose',
+              sort: '-averageGlucoseMmol',
               sortType: 'bgm',
             }));
           });
