10b045cb400541f2e93a538ad9214eeb26142887
Henry Liang <henry.liang@tidepool.org>
2024-11-25 15:32:12 -0800

    fix spacing for demographic info

--- app/pages/patientdata/PatientDataHeader/DemographicInfo.js
+++ app/pages/patientdata/PatientDataHeader/DemographicInfo.js
@@ -1,16 +1,30 @@
 import { withTranslation } from 'react-i18next';
 import { Flex, Text } from 'theme-ui';
 import moment from 'moment';
+import { colors } from '../../../themes/baseTheme';
+
+const containerStyles = {
+    sx: {
+      color: 'text.primary', 
+      flexShrink: 0, 
+      gap: 2, 
+      fontSize: 1, 
+      alignItems: 'flex-start', 
+      pl: 24,
+      borderLeft: `2px solid ${colors.lightGrey}`,
+      height: '1.5rem',
+      alignItems: 'center'
+  }
+}
 
 const DemographicInfo = ({ t, patient }) => {
-  const { birthday, mrn } = patient.profile.patient; // TODO: pass through helper
+  const { birthday, mrn } = patient.profile.patient;
 
   const hasValidBirthday = moment(birthday, 'YYYY-MM-DD', true).isValid();
   const hasMrn = !!mrn
 
   return (
-    <Flex sx={{ color: 'text.primary', flexShrink: 0, gap: 2, fontSize: 1, alignItems: 'flex-start', ml: 24 }}>
-
+    <Flex {...containerStyles}>
       { hasValidBirthday && <>
         <Text>{t('DOB:')}</Text>
         <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
@@ -21,7 +35,7 @@ const DemographicInfo = ({ t, patient }) => {
       </>}
 
       { hasMrn && <>
-        <Text>{t('MRN:')}</Text>
+        <Text ml={24}>{t('MRN:')}</Text>
         <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
           <Text as="span" sx={{ whiteSpace: 'nowrap', fontWeight: 'medium' }}>
             {mrn}
