e97cc99ae7ab975feac17c99f052c4dd4bf905d4
Henry Liang <henry.liang@tidepool.org>
2024-12-02 15:40:19 -0800

    long dash for invalid birthday

--- app/components/navpatientheader/DemographicInfo.js
+++ app/components/navpatientheader/DemographicInfo.js
@@ -26,24 +26,23 @@ const DemographicInfo = ({ t, patient }) => {
 
   return (
     <DemographicsContainer>
-      { hasValidBirthday && 
-        <Flex sx={{ color: 'text.primary', flexShrink: 0, gap: 2, fontSize: 1, alignItems: 'flex-end' }}>
-          <Text>{t('DOB:')}</Text>
-          <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
-            <Text as="span" sx={{ whiteSpace: 'nowrap', fontWeight: 'medium' }}>
-              {moment(birthday).format(JS_DATE_FORMAT)}
-            </Text>
-          </Flex>
-        </Flex>}
-
-        <Flex sx={{ color: 'text.primary', flexShrink: 0, gap: 2, fontSize: 1, alignItems: 'flex-end' }}>
-          <Text>{t('MRN:')}</Text>
-          <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
-            <Text as="span" sx={{ whiteSpace: 'nowrap', fontWeight: 'medium' }}>
-              {mrn || <>&mdash;</>}
-            </Text>
-          </Flex>
+      <Flex sx={{ color: 'text.primary', flexShrink: 0, gap: 2, fontSize: 1, alignItems: 'flex-end' }}>
+        <Text>{t('DOB:')}</Text>
+        <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
+          <Text as="span" sx={{ whiteSpace: 'nowrap', fontWeight: 'medium' }}>
+            {hasValidBirthday ? moment(birthday).format(JS_DATE_FORMAT) : <>&mdash;</>}
+          </Text>
         </Flex>
+      </Flex>
+
+      <Flex sx={{ color: 'text.primary', flexShrink: 0, gap: 2, fontSize: 1, alignItems: 'flex-end' }}>
+        <Text>{t('MRN:')}</Text>
+        <Flex sx={{ columnGap: 2, alignItems: 'flex-start' }}>
+          <Text as="span" sx={{ whiteSpace: 'nowrap', fontWeight: 'medium' }}>
+            {mrn || <>&mdash;</>}
+          </Text>
+        </Flex>
+      </Flex>
     </DemographicsContainer>
   )
 };
