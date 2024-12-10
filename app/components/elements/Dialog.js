c7603f2977439e099e0b33c9adf43711c8456575
clintonium-119 <clint.beacock@gmail.com>
2024-12-10 13:13:38 -0500

    Add a back button to the DialogTitle component, and center Title text

--- app/components/elements/Dialog.js
+++ app/components/elements/Dialog.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 import MuiDialog, { DialogProps } from '@material-ui/core/Dialog';
 import styled from '@emotion/styled';
-import { Flex, FlexProps, Box, BoxProps } from 'theme-ui';
+import { Flex, FlexProps, Box, BoxProps, Text } from 'theme-ui';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
 import { Icon } from './Icon';
+import i18next from '../../core/language';
+
+const t = i18next.t.bind(i18next);
 
 import {
   borders,
@@ -19,6 +23,7 @@ export function DialogTitle(props) {
   const {
     children,
     closeIcon,
+    onBack,
     onClose,
     sx = {},
     ...dialogTitleProps
@@ -29,27 +34,47 @@ export function DialogTitle(props) {
   return (
     <Flex
       p={3}
-      sx={{ alignItems: 'center', borderBottom: props.divider ? borders.divider : 'unset', ...sx }}
+      sx={{ alignItems: 'center', justifyContent: 'space-between', flexWrap: 'nowrap', borderBottom: props.divider ? borders.divider : 'unset', ...sx }}
       {...dialogTitleProps}
     >
-      <Flex
-        // offset by Icon width to ensure Title is properly centered when closeIcon is present
-        pl={closeIcon ? ICON_WIDTH : 0}
-        mx={[0, 'auto']}
-      >
+      <Box sx={{ flexGrow: 1, flexBasis: 0, textAlign: 'left' }}>
+        {onBack && (
+          <Icon
+            as={Flex}
+            label="dialog back button"
+            onClick={onBack}
+            icon={ArrowBackRoundedIcon}
+            variant="button"
+            sx={{
+              zIndex: 1,
+              position: 'absolute',
+              alignItems: 'center',
+            }}
+          >
+            <Text p={1} sx={{ fontWeight: 'medium', fontSize: 2 }}>
+              {t('Back')}
+            </Text>
+          </Icon>
+        )}
+      </Box>
+
+      <Box sx={{ textAlign: 'center' }}>
         {children}
-      </Flex>
-      {closeIcon && (
-        <Icon
-          label="close dialog"
-          onClick={onClose}
-          icon={CloseRoundedIcon}
-          variant="button"
-          sx={{
-            zIndex: 1,
-          }}
-        />
-      )}
+      </Box>
+
+      <Box sx={{ flexGrow: 1, flexBasis: 0, textAlign: 'right' }}>
+        {closeIcon && (
+          <Icon
+            label="close dialog"
+            onClick={onClose}
+            icon={CloseRoundedIcon}
+            variant="button"
+            sx={{
+              zIndex: 1,
+            }}
+          />
+        )}
+      </Box>
     </Flex>
   );
 }
@@ -58,6 +83,7 @@ DialogTitle.propTypes = {
   ...FlexProps,
   closeIcon: PropTypes.bool,
   divider: PropTypes.bool,
+  onBack: PropTypes.func,
 };
 
 DialogTitle.defaultProps = {
