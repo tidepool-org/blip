2efb3a3da31afa5c421533f1a0e645538a9af8a6
Henry Liang <henry.liang@tidepool.org>
2024-12-16 17:42:13 -0800

    add patient fullname to drawer

--- app/pages/dashboard/DrawerMenuBar.js
+++ app/pages/dashboard/DrawerMenuBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
 import { Flex, Box, Text } from 'theme-ui';
@@ -8,13 +9,22 @@ import PatientLastReviewed from '../../components/clinic/PatientLastReviewed';
 
 const DrawerMenuBar = ({ patientId, api, trackMetric }) => {
   const { t } = useTranslation();
+  const history = useHistory();
+  const clinic = useSelector(state => state.blip.clinics[state.blip.selectedClinicId]);
+  const patient = clinic?.patients?.[patientId];
+
   const handleViewData = () => history.push(`/patients/${patientId}/data?chart=trends&dashboard=tide`);
 
   const recentlyReviewedThresholdDate = moment().startOf('isoWeek').toISOString();
 
+  // TODO: DOB
+
   return (
     <Flex sx={{ gap: '12px' }}>
-      <Box>{t('Name/DOB')}</Box>
+      <Box>
+        <Box>{patient.fullName}</Box>
+        <Box></Box>
+      </Box>
 
       <Button onClick={handleViewData}>{t('View Data')}</Button>
       <Button onClick={() => {}}>{t('Copy as Text')}</Button>
