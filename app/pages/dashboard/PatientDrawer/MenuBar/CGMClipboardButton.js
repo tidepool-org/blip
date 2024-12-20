4b88edc7c4816d372698f886e9f683e88a5a3471
Henry Liang <henry.liang@tidepool.org>
2024-12-20 12:59:39 -0800

    implement copy as text

---
+++ app/pages/dashboard/PatientDrawer/MenuBar/CGMClipboardButton.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from '../../../../components/elements/Button';
+import utils from '../../../../core/utils';
+import { MGDL_UNITS } from '../../../../core/constants';
+import { utils as vizUtils } from '@tidepool/viz';
+const { TextUtil } = vizUtils.text;
+import { Box } from 'theme-ui'
+
+const getCGMClipboardText = (patientName, agpCGM, t) => {
+  if (!agpCGM) return '';
+
+  const { veryLow, low, target, total } = agpCGM.data?.current?.stats?.timeInRange?.counts || {};
+  const { averageGlucose } =  agpCGM.data?.current?.stats?.averageGlucose || {};
+  const { bgUnits } = agpCGM.bgPrefs;
+
+  const avgGlucosePrecision = bgUnits === MGDL_UNITS ? 0 : 1;
+  const targetRange = bgUnits === MGDL_UNITS ? '70-180' : '4.0-10.0';
+
+  const avgGlucose   = utils.roundToPrecision(averageGlucose, avgGlucosePrecision);
+  const timeInTarget = utils.roundToPrecision((target / total) * 100, 1);
+  const timeInLows   = utils.roundToPrecision(((veryLow + low) * 100 ) / total, 1);
+
+  const textUtil = new TextUtil();
+  let clipboardText = '';
+
+  const paragraph1 = '{{patientName}} has an average blood glucose of {{avgGlucose}} {{bgUnits}} and is in target'
+                     + ' range ({{targetRange}}) {{timeInTarget}}% of the time with {{timeInLows}}% lows.';
+
+  const paragraph1Opts = { patientName, avgGlucose, targetRange, bgUnits, timeInTarget, timeInLows };
+
+  const paragraph2 = 'Our goal is to get people into target range more than 70% (16 hours 48 min/day) of the time'
+                     + ' with less than 4% lows (58 min/day).';
+  
+  const paragraph3 = 'Please let us know if you have questions or concerns. We will review trends again next'
+                     + ' month. You can also view the trends if you log into clarity on your computer and look at'
+                     + ' the overall trends and average blood glucose.';
+
+  // TODO: Verify copy text, especially line 3 - (reviewing Q monthly)
+  clipboardText += textUtil.buildTextLine(t(paragraph1, paragraph1Opts));
+  clipboardText += textUtil.buildTextLine('');
+  clipboardText += textUtil.buildTextLine(t(paragraph2));
+  clipboardText += textUtil.buildTextLine('');
+  clipboardText += textUtil.buildTextLine(t(paragraph3));
+
+  return clipboardText;
+}
+
+const STATE = {
+  INITIAL: 'INITIAL',
+  CLICKED: 'CLICKED',
+}
+
+const CGMClipboardButton = ({ patientName, agpCGM }) => {
+  const { t } = useTranslation();
+  const [buttonState, setButtonState] = useState(STATE.INITIAL);
+
+  useEffect(() => {
+    let effect = setTimeout(() => {
+      setButtonState(STATE.INITIAL)
+    }, 1000);
+
+    return () => {
+      clearTimeout(effect);
+    }
+  }, [buttonState])
+
+  const isDisabled = !agpCGM;
+
+  const clipboardText = useMemo(() => getCGMClipboardText(patientName, agpCGM, t), [patientName, agpCGM, t]);
+
+  const handleCopy = () => {
+    navigator.clipboard?.writeText(clipboardText);
+    setButtonState(STATE.CLICKED);
+  }
+
+  return (
+    <Button disabled={isDisabled} onClick={handleCopy} variant="secondary">
+      {buttonState === STATE.CLICKED 
+        ? <Box>{t('Copied ✓')}</Box> 
+        : <Box>{t('Copy as Text')}</Box>
+      }
+    </Button>
+  )
+}
+
+export default CGMClipboardButton;
--- app/pages/dashboard/PatientDrawer/MenuBar/MenuBar.js
+++ app/pages/dashboard/PatientDrawer/MenuBar/MenuBar.js
@@ -1,39 +1,15 @@
 import React, { useEffect } from 'react';
-import styled from '@emotion/styled';
-import * as actions from '../../../redux/actions';
+import * as actions from '../../../../redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { push } from 'connected-react-router';
 import { Flex, Box, Text } from 'theme-ui';
-import colorPalette from '../../../themes/colorPalette';
-import Button from '../../../components/elements/Button';
+import colorPalette from '../../../../themes/colorPalette';
+import Button from '../../../../components/elements/Button';
 import moment from 'moment';
-import PatientLastReviewed from '../../../components/clinic/PatientLastReviewed';
+import PatientLastReviewed from '../../../../components/clinic/PatientLastReviewed';
 import { useFlags } from 'launchdarkly-react-client-sdk';
-
-const getCopyToClipboardData = (agpCGM, _patientName, t) => {
-  if (!agpCGM) return { canCopy: false, clipboardText: '' };
-
-  const { veryLow, low, target, total } = agpCGM.data?.current?.stats?.timeInRange?.counts || {};
-
-  const timeInTarget = Math.round((target / total) * 100);
-  const timeInLows = Math.round(((veryLow + low) * 100 ) / total);
-
-  const clipboardText = t('Lows: {{timeInLows}}, Target: {{timeInTarget}}', { timeInLows, timeInTarget });
-
-  return {
-    canCopy: true,
-    clipboardText,
-  };
-}
-
-const StyledMenuBar = styled(Box)`
-  display: grid;
-  grid-template-columns: 32fr 18fr 18fr 32fr;
-  gap: 16px;
-  min-height: 42px;
-  margin-bottom: 16px;
-`
+import CGMClipboardButton from './CGMClipboardButton';
 
 const MenuBar = ({ patientId, api, trackMetric, onClose }) => {
   const dispatch = useDispatch();
@@ -45,8 +21,6 @@ const MenuBar = ({ patientId, api, trackMetric, onClose }) => {
   const patientName = useSelector(state => state.blip.clinics[state.blip.selectedClinicId]?.patients?.[patientId]?.fullName);
   const patientDOB = useSelector(state => state.blip.clinics[state.blip.selectedClinicId]?.patients?.[patientId]?.birthDate);
   const agpCGM = useSelector(state => state.blip.pdf?.data?.agpCGM); // IMPORTANT: Data taken from Redux PDF slice
-
-  const { clipboardText, canCopy } = getCopyToClipboardData(agpCGM, patientName, t);
 
   useEffect(() => {
     // DOB field in Patient object may not be populated in TIDE Dashboard, so we need to refetch
@@ -64,13 +38,9 @@ const MenuBar = ({ patientId, api, trackMetric, onClose }) => {
       onClose();
     }, 500)
   }
-
-  const handleCopyAsText = () => {
-    console.log(clipboardText) // TODO: Implement  
-  }
-
+  
   return (
-    <StyledMenuBar>
+    <Box sx={{ display: 'grid', gridTemplateColumns: '32fr 18fr 18fr 32fr', gap: 3, minHeight: '42px', marginBottom: 3 }}>
       <Flex sx={{ justifyContent: 'center', flexDirection: 'column' }}>
         <Text sx={{ color: colorPalette.primary.purpleDark, fontWeight: 'bold', fontSize: 2 }}>
           {patientName}
@@ -89,9 +59,7 @@ const MenuBar = ({ patientId, api, trackMetric, onClose }) => {
       </Flex>
       
       <Flex sx={{ justifyContent: 'flex-start', alignItems: 'center' }}>
-        <Button disabled={!canCopy} onClick={handleCopyAsText} variant="secondary">
-          {t('Copy as Text')}
-        </Button>
+        <CGMClipboardButton patientName={patientName} agpCGM={agpCGM} />
       </Flex>
 
       <Flex sx={{ fontSize: 0, alignItems: 'center', justifyContent: 'flex-end' }}>
@@ -116,7 +84,7 @@ const MenuBar = ({ patientId, api, trackMetric, onClose }) => {
           </>
         }
       </Flex>
-    </StyledMenuBar>
+    </Box>
   )
 }
 
