fad8c1f445462d19dc0f03247c43afe51a2ab486
Henry Liang <henry.liang@tidepool.org>
2025-01-13 10:19:42 -0800

    fix CGMStatistics test

--- test/unit/pages/dashboard/PatientDrawer/CGMStatistics.test.js
+++ test/unit/pages/dashboard/PatientDrawer/CGMStatistics.test.js
@@ -19,70 +19,118 @@ const agpCGM = {
     'current': {
       'aggregationsByDate': {},
       'stats': {
-        'averageGlucose': {
-          'averageGlucose': 121.24147339699863,
-          'total': 733
+        'averageGlucose': { 
+          'averageGlucose': 121.4013071783735, 
+          'total': 8442,
         },
         'bgExtents': {
-          'bgMax': 350,
-          'bgMin': 39,
-          'bgDaysWorn': 7,
-          'newestDatum': { 'time': 1733164212000 },
-          'oldestDatum': { 'time': 1732608015000 }
+          'bgMax': 401.00001001500004,
+          'bgMin': 38.9999690761,
+          'bgDaysWorn': 30,
+          'newestDatum': { 'time': 1736783190269 },
+          'oldestDatum': { 'time': 1734249705225 },
         },
-        'coefficientOfVariation': {
-          'coefficientOfVariation': 49.838071869255685,
-          'total': 733
+        'coefficientOfVariation': { 
+          'coefficientOfVariation': 49.82047988955789, 
+          'total': 8442,
         },
         'glucoseManagementIndicator': {
-          'glucoseManagementIndicator': null,
-          'glucoseManagementIndicatorAGP': 6.210096043656208,
-          'insufficientData': true
+          'glucoseManagementIndicator': 6.236871267706695,
+          'glucoseManagementIndicatorAGP': 6.236871267706695,
+          'total': 8442,
         },
         'sensorUsage': {
-          'sensorUsage': 219900000,
-          'sensorUsageAGP': 39.514824797843666,
-          'total': 604800000,
+          'sensorUsage': 2532600000,
+          'sensorUsageAGP': 99.95264030310206,
+          'total': 2592000000,
           'sampleFrequency': 300000,
-          'count': 733
+          'count': 8442,
         },
         'timeInRange': {
           'durations': {
-            'veryLow': 5186357.435197817,
-            'low': 12376534.788540244,
-            'target': 56106957.70804911,
-            'high': 8368894.952251023,
-            'veryHigh': 4361255.115961801,
-            'total': 219900000
+            'veryLow': 337739.8720682303,
+            'low': 2507462.686567164,
+            'target': 66023027.7185501,
+            'high': 14031556.503198294,
+            'veryHigh': 3500213.219616205,
+            'total': 2532600000,
           },
           'counts': {
-            'veryLow': 44,
-            'low': 105,
-            'target': 476,
-            'high': 71,
-            'veryHigh': 37,
-            'total': 733
-          }
-        }
+            'veryLow': 33,
+            'low': 245,
+            'target': 6451,
+            'high': 1371,
+            'veryHigh': 342,
+            'total': 8442,
+          },
+        },
       },
       'endpoints': {
-        'range': [
-          1732608000000,
-          1733212800000
-        ],
-        'days': 7,
-        'activeDays': 7
-      }
-    }
+        'range': [1734249600000, 1736841600000 ],
+        'days': 30,
+        'activeDays': 30,
+      },
+      'data': {},
+    },
   },
-  'timePrefs': {
-    'timezoneAware': true,
-    'timezoneName': 'US/Eastern'
-  },
+  'timePrefs': { 'timezoneAware': true, 'timezoneName': 'Etc/GMT+8' },
   'bgPrefs': {
-    'bgUnits': 'mg/dL'
+    'bgBounds': {
+      'veryHighThreshold': 250,
+      'targetUpperBound': 180,
+      'targetLowerBound': 70,
+      'veryLowThreshold': 54,
+      'extremeHighThreshold': 350,
+      'clampThreshold': 600,
+    },
+    'bgClasses': {
+      'low': { 'boundary': 70 },
+      'target': { 'boundary': 180 },
+      'very-low': { 'boundary': 54 },
+      'high': { 'boundary': 250 },
+    },
+    'bgUnits': 'mg/dL',
   },
-  'query': {},
+  'query': {
+    'endpoints': [
+      1734249600000,
+      1736841600000,
+    ],
+    'aggregationsByDate': 'dataByDate, statsByDate',
+    'bgSource': 'cbg',
+    'stats': [
+      'averageGlucose',
+      'bgExtents',
+      'coefficientOfVariation',
+      'glucoseManagementIndicator',
+      'sensorUsage',
+      'timeInRange',
+    ],
+    'types': { 'cbg': {} },
+    'bgPrefs': {
+      'bgUnits': 'mg/dL',
+      'bgClasses': { 
+        'low': { 'boundary': 70 }, 
+        'target': { 'boundary': 180 }, 
+        'very-low': { 'boundary': 54 }, 
+        'high': { 'boundary': 250 },
+      },
+      'bgBounds': {
+        'veryHighThreshold': 250,
+        'targetUpperBound': 180,
+        'targetLowerBound': 70,
+        'veryLowThreshold': 54,
+        'extremeHighThreshold': 350,
+        'clampThreshold': 600,
+      },
+    },
+    'metaData': 'latestPumpUpload, bgSources',
+    'timePrefs': {
+      'timezoneAware': true,
+      'timezoneName': 'Etc/GMT+8',
+    },
+    'excludedDevices': [],
+  },
   'metaData': {},
 };
 
@@ -99,11 +147,11 @@ describe('PatientDrawer/CGMStatistics', () => {
     const wrapper = mount(<CGMStatistics agpCGM={agpCGM} />);
 
     it('renders the time range in the expected format', () => {
-      expect(wrapper.find('#agp-table-time-range').hostNodes().text()).to.include('November 26 - December 2, 2024 (7 days)');
+      expect(wrapper.find('#agp-table-time-range').hostNodes().text()).to.include('December 15, 2024 - January 13, 2025 (30 days)');
     });
 
     it('renders the CGM Active % in the expected format', () => {
-      expect(wrapper.find('#agp-table-cgm-active').hostNodes().text()).to.include('39.5%');
+      expect(wrapper.find('#agp-table-cgm-active').hostNodes().text()).to.include('100%');
     });
 
     it('renders the Average Glucose in the expected format', () => {
