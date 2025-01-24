e02793fdee15f9990b40a939bc798c4b26c84674
Henry Liang <henry.liang@tidepool.org>
2025-01-24 00:28:19 -0800

    add initial menu option rows

--- app/components/navbar/MobileNavbar/Menu.js
+++ app/components/navbar/MobileNavbar/Menu.js
@@ -1,33 +1,134 @@
 import React from 'react';
-import { Box } from 'theme-ui';
 import styled from '@emotion/styled';
-import Popover from '../../elements/Popover';
+import { Box, Flex } from 'theme-ui';
+import { useTranslation } from 'react-i18next';
+import { usePopupState, bindPopover, bindTrigger } from 'material-ui-popup-state/hooks';
+import colorPalette from '../../../themes/colorPalette';
 
-import {
-  usePopupState,
-  bindPopover,
-  bindTrigger,
-} from 'material-ui-popup-state/hooks';
+import Popover from '../../elements/Popover';
+import Button from '../../elements/Button';
+
+import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import viewIcon from '../../../core/icons/viewIcon.svg';
+import shareIcon from '../../../core/icons/shareIcon.svg';
+
+const MenuOption = styled.div`
+  padding: 8px 0;
+  width: 100%;
+  justify-content: flex-end;
+  border-top: 1px solid ${colorPalette.primary.gray10};
+`;
+
+const AccountMenuOption = styled.div`
+  padding: 4px 0;
+  width: 100%;
+  justify-content: flex-end;
+`;
+
+const buttonStyleProps = {
+  variant: 'textSecondary',
+  iconPosition: 'left',
+  iconFontSize: '1.25em',
+  sx: { fontSize: 1, fontWeight: 'medium' },
+  pl: 0,
+};
+
+const StyledPopover = styled(Popover)`
+  .MuiPopover-paper {
+    width: 100%;
+    padding: 0;
+  }
+`;
 
 const Menu = () => {
   const popupState = usePopupState({ variant: 'popover', popupId: 'mobileNavigationMenu' });
+  const { t } = useTranslation();
 
   return (
     <>
       <button {...bindTrigger(popupState)}>
         { 'Menu' }
       </button>
-      <Popover
-        sx={{ minWidth: '15em' }}
+      <StyledPopover
         classes={{ root: 'Navbar-mobileBackdrop' }}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         transformOrigin={{ vertical: 'top', horizontal: 'center' }}
         {...bindPopover(popupState)}
       >
-        <Box py={2}>
-          <p>Item List</p>
+        <Box px={4}>
+          <Box py={3}>
+            Patient Name
+          </Box>
+          <Box>
+            <MenuOption>
+              <Button
+                id="mobileNavbar_viewDataButton"
+                onClick={() => {}}
+                iconSrc={viewIcon}
+                iconLabel="View"
+                {...buttonStyleProps}
+              >
+                {t('Summary Stats')}
+              </Button>
+            </MenuOption>
+            <MenuOption>
+              <Button
+                id="mobileNavbar_devicesButton"
+                onClick={() => {}}
+                iconSrc={viewIcon}
+                iconLabel="Devices"
+                {...buttonStyleProps}
+              >
+                {t('Devices')}
+              </Button>
+            </MenuOption>
+            <MenuOption>
+              <Button
+                id="mobileNavbar_shareButton"
+                onClick={() => {}}
+                iconSrc={shareIcon}
+                iconLabel="Share"
+                {...buttonStyleProps}
+              >
+                {t('Share')}
+              </Button>
+            </MenuOption>
+          </Box>
         </Box>
-      </Popover>
+        <Box px={4} py={3} sx={{ background: colorPalette.primary.bluePrimary00 }}>
+          <AccountMenuOption>
+            <Button // TODO: Need one for every workspace
+              id="mobileNavbar_workspaceButton"
+              onClick={() => {}}
+              icon={SupervisedUserCircleRoundedIcon}
+              iconLabel="Private Workspace"
+              {...buttonStyleProps}
+            >
+              {t('Private Workspace')}
+            </Button>
+            <Button // TODO: Need one for every workspace
+              id="mobileNavbar_accountSettingsButton"
+              onClick={() => {}}
+              icon={SettingsRoundedIcon}
+              iconLabel="Account Settings"
+              {...buttonStyleProps}
+            >
+              {t('Account Settings')}
+            </Button>
+            <Button // TODO: Need one for every workspace
+              id="mobileNavbar_logoutButton"
+              onClick={() => {}}
+              icon={ExitToAppRoundedIcon}
+              iconLabel="Logout"
+              {...buttonStyleProps}
+            >
+              {t('Logout')}
+            </Button>
+          </AccountMenuOption>
+        </Box>
+      </StyledPopover>
     </>
   );
 };
