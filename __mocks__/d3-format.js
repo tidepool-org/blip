ae9f602a8d47d16189e923580bb4beeb526063c5
Henry Liang <henry.liang@tidepool.org>
2025-05-01 11:57:01 -0700

    WEB-3595 clean up unneeded config options

--- __mocks__/d3-format.js
+++
@@ -1,8 +0,0 @@
-/* global jest */
-
-// Mock for d3-format
-module.exports = {
-  format: jest.fn((specifier) => (value) => `${value}`),
-  formatPrefix: jest.fn(),
-  formatDefaultLocale: jest.fn(),
-};
--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,12 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
+
+    // use minified versions of d3 modules
+    'd3-format': '<rootDir>/node_modules/d3-format/dist/d3-format.js',
+
+    // Mock import of Viz
     '@tidepool/viz': '<rootDir>/__mocks__/@tidepool/viz.js',
-    'd3-format': '<rootDir>/__mocks__/d3-format.js',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
@@ -14,7 +18,6 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    // This pattern will transform all ES modules in node_modules
     'node_modules/(?!(.*\\.mjs$))',
   ],
   collectCoverageFrom: [
