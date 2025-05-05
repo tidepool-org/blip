7d29eafd79152d2feaf920ba1186cbb8760bb234
Henry Liang <henry.liang@tidepool.org>
2025-05-05 08:32:50 -0700

    WEB-3595 use fuller mock for property-information

--- __mocks__/property-information.js
+++ __mocks__/property-information.js
@@ -3,8 +3,22 @@
 // Mock for property-information
 const propertyInformation = {
   find: jest.fn(() => ({
+    property: 'property-mock',
+    attribute: 'attribute-mock',
     boolean: false,
+    overloadedBoolean: false,
+    number: false,
+    spaceSeparated: false,
+    commaSeparated: false,
+    commaOrSpaceSeparated: false,
+    mustUseProperty: false,
+    defined: true,
   })),
+  normalize: jest.fn(),
+  html: {},
+  svg: {},
+  normal: {},
+  schema: {},
 };
 
 module.exports = propertyInformation;
