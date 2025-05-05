9ed8dbbf1f79b3bd1982256b70d66344d64b0979
Henry Liang <henry.liang@tidepool.org>
2025-05-05 08:08:07 -0700

    WEB-3595 simplify mock for property-information

--- __mocks__/property-information.js
+++ __mocks__/property-information.js
@@ -3,23 +3,8 @@
 // Mock for property-information
 const propertyInformation = {
   find: jest.fn(() => ({
-    property: 'mocked-property',
-    attribute: 'mocked-attribute',
     boolean: false,
-    overloadedBoolean: false,
-    number: false,
-    spaceSeparated: false,
-    commaSeparated: false,
-    commaOrSpaceSeparated: false,
-    mustUseProperty: false,
-    defined: true
   })),
-  normalize: jest.fn(value => value.toLowerCase()),
-  html: {},
-  svg: {},
-  find: jest.fn(),
-  normal: {},
-  schema: {}
 };
 
 module.exports = propertyInformation;
