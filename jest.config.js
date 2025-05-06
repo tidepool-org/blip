9608246d3719de4842dde3d89175d96e970e48de
Henry Liang <henry.liang@tidepool.org>
2025-05-06 16:07:42 -0700

    WEB-3595 update comments

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
 
-    // use minified versions of d3 modules (prevents errors due to Jest not liking ESM)
+    // use minified versions of d3 modules (prevents errors due Jest / ESM conflict)
     'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
     'd3-format': '<rootDir>/node_modules/d3-format/dist/d3-format.min.js',
     'd3-scale': '<rootDir>/node_modules/d3-scale/dist/d3-scale.min.js',
@@ -38,9 +38,9 @@ module.exports = {
     ],
   },
   transformIgnorePatterns: [
-    // This pattern ensures that esbuild processes @tidepool/viz and all its dependencies.
-    // In the event that a `Jest failed to parse a file` error is encountered and it is
-    // a node_module, the module name can be added here to be processed by esbuild.
+    // This pattern ensures that @swc/jest transforms @tidepool/viz and all other dependencies.
+    // In the event that a `Jest failed to parse a file` error is encountered and it is a
+    // node_module, the module name can be added here to ensure pre-processing by @swc/jest.
 
     'node_modules/(?!(.*\\.mjs$|@tidepool/viz|internmap|react-markdown|vfile|unist-util-stringify-position|unified|bail|is-plain-obj|trough|remark-parse|mdast-util-from-markdown|mdast-util-to-string|mdast-util-to-hast|micromark|decode-named-character-reference|remark-rehype|unist-util-position|trim-lines|unist-util-visit|unist-util-is|unist-util-generated|mdast-util-definitions|property-information|hast-util-whitespace|space-separated-tokens|comma-separated-tokens))',
   ],
