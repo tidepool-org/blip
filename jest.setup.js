cc30b77ad44486f632c6e7137f90590e55cba5ff
Henry Liang <henry.liang@tidepool.org>
2025-05-07 09:13:34 -0700

    WEB-3595 remove unused test setup config items

--- jest.setup.js
+++ jest.setup.js
@@ -6,15 +6,9 @@ require('@babel/polyfill');
 // Setup Intl polyfill
 global.IntlPolyfill = require('intl');
 require('intl/locale-data/jsonp/en.js');
-require('intl-pluralrules');
 
 // Setup Jest DOM matchers
 require('@testing-library/jest-dom');
-
-// Mock window.config for tests
-if (window.config === undefined) {
-  window.config = {};
-}
 
 // Define global variables that might be expected in tests
 global.__DEV__ = false;
