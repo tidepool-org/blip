ba944a575ff7e4fb22b27b729bec82490ad4e13b
Henry Liang <henry.liang@tidepool.org>
2025-05-08 15:50:19 -0700

    WEB-3662 use suggested filters

--- app/components/clinic/PatientForm/SelectTags.js
+++ app/components/clinic/PatientForm/SelectTags.js
@@ -1,28 +1,54 @@
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import keyBy from 'lodash/keyBy';
+import partition from 'lodash/partition';
 import Select from 'react-select';
 import { Box } from 'theme-ui';
+import { useLocation } from 'react-router-dom';
+import useClinicPatientsFilters from '../../../pages/clinicworkspace/useClinicPatientsFilters';
+
+export const getSelectOptions = (
+  clinicTags = [],
+  activeFilters,
+  shouldSuggestTags = false,
+) => {
+  const allOptions = clinicTags.map(tag => ({ label: tag.name, value: tag.id }));
+
+  if (!shouldSuggestTags) return allOptions;
+
+  const [suggested, nonSuggested] = partition(allOptions, option => {
+    const currentFilterTagIds = activeFilters?.patientTags || [];
+    return currentFilterTagIds.includes(option.value);
+  });
+
+  const groupedOptions = [
+    { label: 'Suggested', options: suggested },
+    { label: '', options: nonSuggested },
+  ];
+
+  return groupedOptions;
+};
 
 const SelectTags = ({ onChange, currentTagIds }) => {
+  const { pathname } = useLocation();
   const selectedClinicId = useSelector((state) => state.blip.selectedClinicId);
   const clinic = useSelector(state => state.blip.clinics?.[selectedClinicId]);
   const clinicPatientTags = useMemo(() => keyBy(clinic?.patientTags, 'id'), [clinic?.patientTags]);
+  const [activeFilters] = useClinicPatientsFilters();
 
   const handleSelect = (tags) => {
     const tagIds = tags.map(tag => tag.value);
     onChange(tagIds);
   };
 
+  // If clinic has tags feature and the current page is the patient list, suggest tags
+  const shouldSuggestTags = clinic?.entitlements?.patientTags && pathname === '/clinic-workspace';
+
+  const selectOptions = getSelectOptions(clinic?.patientTags, activeFilters, shouldSuggestTags);
   const selectValue = currentTagIds.map(tagId => ({
     label: clinicPatientTags[tagId]?.name || '',
     value: tagId,
   }));
-
-  const selectOptions = clinic?.patientTags?.map(tag => ({
-    label: tag.name,
-    value: tag.id,
-  })) || [];
 
   return (
     <Box className='input-group form-group clearfix'>
