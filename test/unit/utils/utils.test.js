7288d2237e8b88f5241bacda65b8465155731ac1
Henry Liang <henry.liang@tidepool.org>
2025-05-27 09:43:55 -0700

    WEB-3573 add tests for sorting

--- test/unit/utils/utils.test.js
+++ test/unit/utils/utils.test.js
@@ -512,7 +512,7 @@ describe('utils', () => {
       expect(utils.getCBGFilter(location)).to.equal(null);
     });
   });
-    
+
   describe('translateBg', () => {
     it('should translate a BG value to the desired target unit', () => {
       expect(utils.translateBg(180, MMOLL_UNITS)).to.equal(10);
@@ -736,6 +736,39 @@ describe('utils', () => {
 
     it('returns null if the arg is not a valid date string', () => {
       expect(utils.parseDatetimeParamToInteger('not-a-date')).to.be.null;
+    });
+  });
+
+  describe('sortByLabel', function() {
+    it('Sorts a blank arg first', function() {
+      expect(utils.sortByLabel(undefined, undefined)).to.equal(0);
+      expect(utils.sortByLabel('', undefined)).to.equal(0);
+      expect(utils.sortByLabel(undefined, '')).to.equal(0);
+      expect(utils.sortByLabel(undefined, 'test')).to.equal(-1);
+      expect(utils.sortByLabel('', 'test')).to.equal(-1);
+      expect(utils.sortByLabel('test', undefined)).to.equal(1);
+      expect(utils.sortByLabel('test', '')).to.equal(1);
+    });
+
+    it('Sorts numerically rather than lexicographically', () => {
+      let arr = ['Tag 12', 'Tag 8', 'Tag 9a', 'Tag 9'];
+      arr.sort((a, b) => utils.sortByLabel(a, b));
+
+      expect(arr).to.eql(['Tag 8', 'Tag 9', 'Tag 9a', 'Tag 12']);
+    });
+
+    it('Sorts base characters ahead of variant characters', () => {
+      let arr = ['café', 'cafe'];
+      arr.sort((a, b) => utils.sortByLabel(a, b));
+
+      expect(arr).to.eql(['cafe', 'café']);
+    });
+
+    it('Sorts uppercase characters ahead of lowercase characters', () => {
+      let arr = ['john', 'jOhn', 'John'];
+      arr.sort((a, b) => utils.sortByLabel(a, b));
+
+      expect(arr).to.eql(['John', 'jOhn', 'john']);
     });
   });
 });
