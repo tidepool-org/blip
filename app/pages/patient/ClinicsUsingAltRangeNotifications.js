91b29578e4ac2492a168cadc25c2b4f91f4cb749
Henry Liang <henry.liang@tidepool.org>
2025-08-20 15:37:23 -0700

    WEB-3832 insert target bounds given bgUnits and glycemicRanges

--- app/pages/patient/ClinicsUsingAltRangeNotifications.js
+++ app/pages/patient/ClinicsUsingAltRangeNotifications.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectPatient, selectUser } from '../../core/selectors';
 import mapValues from 'lodash/mapValues';
-import { utils as vizUtils, colors as vizColors } from '@tidepool/viz';
-const { GLYCEMIC_RANGE } = vizUtils.constants;
+import { utils as vizUtils, colors as vizColors, constants as vizConstants } from '@tidepool/viz';
+const {
+  GLYCEMIC_RANGE,
+  ADA_OLDER_HIGH_RISK_BG_BOUNDS,
+  ADA_PREGNANCY_T1_BG_BOUNDS,
+  ADA_GESTATIONAL_T2_BG_BOUNDS,
+} = vizUtils.constants;
 import personUtils from '../../core/personutils';
 import { Box, Flex } from 'theme-ui';
 import { useTranslation } from 'react-i18next';
@@ -13,10 +18,28 @@ import moment from 'moment';
 
 import cloneDeep from 'lodash/cloneDeep';
 import pickBy from 'lodash/pickBy';
+import { MGDL_UNITS } from '../../core/constants';
 
-const Notification = ({ clinicId, glycemicRanges, onDismiss }) => {
+const getRenderedTargetRange = (glycemicRanges, bgUnits) => {
+  switch(glycemicRanges) {
+    case GLYCEMIC_RANGE.ADA_OLDER_HIGH_RISK: return ADA_OLDER_HIGH_RISK_BG_BOUNDS[bgUnits];
+    case GLYCEMIC_RANGE.ADA_PREGNANCY_T1:    return ADA_PREGNANCY_T1_BG_BOUNDS[bgUnits];
+    case GLYCEMIC_RANGE.ADA_GESTATIONAL_T2:  return ADA_GESTATIONAL_T2_BG_BOUNDS[bgUnits];
+    default:                                 return null;
+  }
+};
+
+const Notification = ({ clinicId, onDismiss }) => {
   const { t } = useTranslation();
-  const name = useSelector(state => state.blip.clinics?.[clinicId]?.name) || '';
+  const clinic = useSelector(state => state.blip.clinics?.[clinicId]) || {};
+  const currentPatientInViewId = useSelector(state => state.blip.currentPatientInViewId);
+
+  const { name, preferredBgUnits: clinicBgUnits = MGDL_UNITS } = clinic;
+  const { glycemicRanges } = clinic.patients?.[currentPatientInViewId];
+
+  if (!glycemicRanges) return null;
+
+  const { targetLowerBound, targetUpperBound } = getRenderedTargetRange(glycemicRanges, clinicBgUnits) || {};
 
   return (
     <Box sx={{ background: vizColors.white, borderRadius: 8 }} py={2} px={4} mb={3}>
@@ -26,7 +49,10 @@ const Notification = ({ clinicId, glycemicRanges, onDismiss }) => {
             {t('Adjusted Target Range')}
           </Box>
           <Box sx={{ color: vizColors.blue50 }}>
-            {t('{{ name }} is using a non-standard target range of {{glycemicRanges}} [UNITS] to view your data.', { name, glycemicRanges })}
+            {
+              t('{{ name }} is using a non-standard target range of {{targetLowerBound}}-{{targetUpperBound}} {{clinicBgUnits}} to view your data.',
+              { name, targetLowerBound, targetUpperBound, clinicBgUnits })
+            }
           </Box>
         </Box>
         <Button onClick={onDismiss} variant='secondaryCondensed' sx={{ border: 'none' }}>
@@ -62,7 +88,7 @@ const ClinicsUsingAltRangeNotifications = ({ api }) => {
 
   if (!isUserPatient) return null;
 
-  const clinicsWithNotificatons = pickBy(clinics, clinic => {
+  const clinicsWithNotifications = pickBy(clinics, clinic => {
     const glycemicRanges = clinic?.patients?.[patient.userid]?.glycemicRanges;
 
     const isNonStandardRange = !!glycemicRanges && glycemicRanges !== GLYCEMIC_RANGE.ADA_STANDARD;
@@ -72,19 +98,15 @@ const ClinicsUsingAltRangeNotifications = ({ api }) => {
     return isNonStandardRange && hasUpdatedValue;
   });
 
-  const notifications = mapValues(clinicsWithNotificatons, clinic => clinic.patients[patient.userid].glycemicRanges);
-
   return (
     <Box>
-      { Object.entries(notifications)
-              .map(([clinicId, glycemicRanges]) => (
-                <Notification
-                  key={clinicId}
-                  clinicId={clinicId}
-                  glycemicRanges={glycemicRanges}
-                  onDismiss={() => handleDismiss(clinicId, glycemicRanges)}
-                />
-              ))
+      { Object.keys(clinicsWithNotifications).map(clinicId => (
+          <Notification
+            key={clinicId}
+            clinicId={clinicId}
+            onDismiss={() => handleDismiss(clinicId)}
+          />
+        ))
       }
     </Box>
   );
