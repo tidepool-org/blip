76c66f33c201d42c832c56240952e6f64ac0ff18
Henry Liang <henry.liang@tidepool.org>
2025-09-15 16:43:46 -0700

    WEB-3890 revert unneeded safe navigation

--- app/pages/dashboard/TideDashboard.js
+++ app/pages/dashboard/TideDashboard.js
@@ -1264,7 +1264,7 @@ export const TideDashboard = (props) => {
       dispatch(push('/clinic-workspace/patients'));
     };
 
-    each((patientGroups?.noData || []), record => {
+    each(patientGroups.noData, record => {
       record.daysSinceLastData = moment.utc().diff(record.lastData, 'days');
     })
 
