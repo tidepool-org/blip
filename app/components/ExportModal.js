61533e8a31ec093b1b62e134c299a42698d1b4c5
henry-tp <henry.liang@tidepool.org>
2026-01-12 02:06:44 -0800

    WEB-4224 remove useMemo

--- app/components/ExportModal.js
+++ app/components/ExportModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser, selectPatient } from '../core/selectors';
 import { useTranslation } from 'react-i18next';
