01185469ffba9bfd4762b9a6455c2bc43cf54105
henry-tp <henry.liang@tidepool.org>
2026-02-06 14:31:41 -0800

    inject x-tidepool-session-token into requests

--- app/redux/api/baseApi.js
+++ app/redux/api/baseApi.js
@@ -1,9 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import config from '../../config';
+import { keycloak } from '../../keycloak';
 
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/',
+    baseUrl: `${config.API_HOST}/v1/`,
+    prepareHeaders: (headers) => {
+      headers.set('x-tidepool-session-token', keycloak?.token || '');
+
+      return headers;
+    },
   }),
   endpoints: () => ({}),
 });
