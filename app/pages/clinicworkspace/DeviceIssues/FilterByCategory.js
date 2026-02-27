ebfe23e9045b5892bd439dc06e2f84b5a656c7c7
henry-tp <henry.liang@tidepool.org>
2026-02-26 16:40:44 -0800

    WEB-4454 rename SelectCategory

--- app/pages/clinicworkspace/DeviceIssues/FilterByCategory.js
+++ app/pages/clinicworkspace/DeviceIssues/FilterByCategory.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
-import { CategorySelector, CategoryTab } from '../components/CategoryTabs';
+import { SelectCategory, Tab } from '../components/SelectCategory';
 import { setCategory } from './deviceIssuesSlice';
 
 export const CATEGORY_TAB = {
@@ -23,14 +23,14 @@ const FilterByCategory = () => {
   const handleChange = (category) => dispatch(setCategory(category));
 
   return (
-    <CategorySelector>
-      <CategoryTab selected={category === DEFAULT} onClick={() => handleChange(DEFAULT)}>{t('All Issues')}</CategoryTab>
-      <CategoryTab selected={category === MISSING_DATA} onClick={() => handleChange(MISSING_DATA)}>{t('Missing Data')}</CategoryTab>
-      <CategoryTab selected={category === DISCONNECTED} onClick={() => handleChange(DISCONNECTED)}>{t('Disconnected or Error')}</CategoryTab>
-      <CategoryTab selected={category === INVITE_EXPIRED} onClick={() => handleChange(INVITE_EXPIRED)}>{t('Invite Expired')}</CategoryTab>
-      <CategoryTab selected={category === INVITE_SENT} onClick={() => handleChange(INVITE_SENT)}>{t('Invite Sent')}</CategoryTab>
-      <CategoryTab selected={category === HIDDEN} onClick={() => handleChange(HIDDEN)}>{t('Hidden Issues')}</CategoryTab>
-    </CategorySelector>
+    <SelectCategory>
+      <Tab selected={category === DEFAULT} onClick={() => handleChange(DEFAULT)}>{t('All Issues')}</Tab>
+      <Tab selected={category === MISSING_DATA} onClick={() => handleChange(MISSING_DATA)}>{t('Stale Data')}</Tab>
+      <Tab selected={category === DISCONNECTED} onClick={() => handleChange(DISCONNECTED)}>{t('Disconnected or Error')}</Tab>
+      <Tab selected={category === INVITE_EXPIRED} onClick={() => handleChange(INVITE_EXPIRED)}>{t('Invite Expired')}</Tab>
+      <Tab selected={category === INVITE_SENT} onClick={() => handleChange(INVITE_SENT)}>{t('Invite Sent')}</Tab>
+      <Tab selected={category === HIDDEN} onClick={() => handleChange(HIDDEN)}>{t('Hidden Issues')}</Tab>
+    </SelectCategory>
   );
 };
 
