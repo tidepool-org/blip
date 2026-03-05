be23bd7933bb82ed46a3324ddad9c621d5ab9fa8
henry-tp <henry.liang@tidepool.org>
2026-03-04 16:45:50 -0800

    WEB-4460 remove erroneous comment

--- app/pages/clinicworkspace/TideDashboardV2/tideDashboardApi.js
+++ app/pages/clinicworkspace/TideDashboardV2/tideDashboardApi.js
@@ -1,19 +1,5 @@
 import { RTKQueryApi } from '../../../redux/api/baseApi';
 
-      // query: ({ clinicId, offset, category, lastDataTo, lastDataFrom, tags, limit }) => {
-      //   const formattedTags = tags.length > 0 ? tags.join(',') : undefined;
-
-      //   return {
-      //     url: `/clinics/${clinicId}/patients`,
-      //     params: {
-      //       offset,
-      //       category,
-      //       'cgm.lastDataTo': lastDataTo,
-      //       'cgm.lastDataFrom': lastDataFrom,
-      //       tags: formattedTags,
-      //       limit,
-      //     },
-
 export const buildGetTideDashboardPatientsParams = (offset, limit, category, lastDataFrom, lastDataTo, tags = []) => {
   const formattedTags = tags.length > 0 ? tags.join(',') : undefined;
 
