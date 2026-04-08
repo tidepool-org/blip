e3b68408ff3ce3f7c84af8de49f591ec79c48efa
henry-tp <henry.liang@tidepool.org>
2026-04-08 14:17:30 -0700

    WEb-4526 trigger new print window

--- app/pages/clinicworkspace/ClinicPatientsPrintModal/usePrintPDF/usePrintPDF.js
+++ app/pages/clinicworkspace/ClinicPatientsPrintModal/usePrintPDF/usePrintPDF.js
@@ -16,6 +16,8 @@ import min from 'lodash/min';
 import at from 'lodash/at';
 import map from 'lodash/map';
 import keys from 'lodash/keys';
+import { useTranslation } from 'react-i18next';
+import { useToasts } from '../../../../providers/ToastProvider';
 
 export const STATUS = {
   // States in order of happy path AGP generation sequence
@@ -98,6 +100,70 @@ const getFetchPatientOpts = (timePrefs, opts) => {
   };
 };
 
+const usePrintWindow = () => {
+  const { t } = useTranslation();
+  const { set: setToast } = useToasts();
+
+  const printWindowRef = useRef(null);
+  const getPrintWindow = () => printWindowRef.current;
+
+  // TODO: Clear windowRef to prevent race condition?
+
+  const waitMessage = t('Please wait while Tidepool generates your PDF report.');
+  const waitHTML = `<p align="center" style="margin-top:20px;font-size:16px;font-family:sans-serif">${waitMessage}</p>`;
+
+  const openPrintWindow = () => {
+    if (!printWindowRef.current || printWindowRef.current?.closed) {
+      printWindowRef.current = window.open();
+      printWindowRef.current.document.write(waitHTML);
+    }
+  };
+
+  const triggerPrint = (pdf) => {
+    if (!pdf?.combined?.url) return; // TODO: error?
+
+    if (printWindowRef.current && !printWindowRef.current?.closed) {
+      // If we already have a ref to a PDF window, (re)use it
+      printWindowRef.current.location.href = pdf.combined.url;
+    } else {
+      // Otherwise, we create and open a new PDF window ref.
+      printWindowRef.current = window.open(pdf.combined.url);
+    }
+
+    setTimeout(() => {
+      if (printWindowRef.current) {
+        printWindowRef.current.focus();
+        printWindowRef.current.print();
+      } else {
+        if (!pdf?.combined?.url) return; // TODO: error?
+
+        setToast({
+          message: t('A popup blocker is preventing your report from opening.'),
+          variant: 'warning',
+          autoHideDuration: null,
+          action: (
+            <Button
+              p={0}
+              sx= {{ lineHeight: 1.5, fontSize: 1 }}
+              variant="textPrimary"
+              onClick={() => {
+                printWindowRef.current = window.open(pdf.combined.url);
+                printWindowRef.current.focus();
+                printWindowRef.current.print();
+                setToast(null);
+              }}
+            >
+              {t('Open it anyway')}
+            </Button>
+          ),
+        });
+      }
+    });
+  };
+
+  return { openPrintWindow, triggerPrint };
+};
+
 const usePrintPDF = (
   api,
   patientId,
@@ -105,6 +171,7 @@ const usePrintPDF = (
 ) => {
   const dispatch = useDispatch();
   const generateAGPImages = buildGenerateAGPImages(dispatch);
+  const { openPrintWindow, triggerPrint } = usePrintWindow();
 
   const data   = useSelector(state => state.blip.data);
   const pdf    = useSelector(state => state.blip.pdf);
@@ -144,6 +211,7 @@ const usePrintPDF = (
       case STATUS.PRINT_STARTED:
         const fetchPatientOpts = getFetchPatientOpts(getTimePrefs(), getPrintOpts());
         dispatch(actions.async.fetchPatientData(api, fetchPatientOpts, patientId));
+        openPrintWindow();
         break;
 
       case STATUS.SECOND_LOADED:
@@ -163,9 +231,7 @@ const usePrintPDF = (
         break;
 
       case STATUS.PDF_GENERATED:
-        const printWindowRef = window.open(pdf.combined.url);
-        printWindowRef.focus();
-        printWindowRef.print();
+        triggerPrint(pdf);
         onPrintTriggered();
 
       default:
