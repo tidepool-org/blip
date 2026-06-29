b685103a8c21ca48955cfa8ff1cbb3801160b882
henry-tp <henry.liang@tidepool.org>
2026-06-29 15:11:01 -0700

    WEB-4460 add PatientCount component

--- app/pages/clinicworkspace/TideDashboardV2/TideDashboardV2.js
+++ app/pages/clinicworkspace/TideDashboardV2/TideDashboardV2.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation, Trans } from 'react-i18next';
 import { colors as vizColors } from '@tidepool/viz';
 import Table from '../../../components/elements/Table';
-import { Flex, Text } from 'theme-ui';
+import { Box, Flex, Grid, Text } from 'theme-ui';
 
 import FilterByCategory from './FilterByCategory';
 import FilterByTags from './FilterByTags';
@@ -22,6 +22,7 @@ import usePruneInvalidFilters from './usePruneInvalidFilters';
 import { resetTideDashboardFilters } from './tideDashboardFiltersSlice';
 import EmptyContentNode from './EmptyContentNode';
 import FilterBySites from './FilterBySites';
+import PatientCount from '../components/PatientCount';
 
 const LIMIT = 12;
 
@@ -53,6 +54,8 @@ const TideDashboard = () => {
   if (!data) return null;
 
   const tableData = data?.data || [];
+
+  const total = data?.meta?.count || 0;
 
   return (
     <>
@@ -97,14 +100,20 @@ const TideDashboard = () => {
         // onClickRow={handleClickPatient}
       />
 
-      <Flex pb={4} sx={{ maxWidth: '640px', justifyContent: 'center', margin: '0 auto' }}>
-        <PaginationControls
-          limit={LIMIT}
-          total={data?.meta?.count || 0}
-          offset={offset}
-          onOffsetChange={handleChangeOffset}
-        />
-      </Flex>
+      <Grid sx={{ gridTemplateColumns: '1fr 2fr 1fr' }}>
+        <Flex sx={{ alignItems: 'flex-end', padding: '0 0 24px 12px' }}>
+          <PatientCount offset={offset} limit={LIMIT} total={total} />
+        </Flex>
+        <Flex pb={4} sx={{ maxWidth: '640px', justifyContent: 'center', margin: '0 auto' }}>
+          <PaginationControls
+            limit={LIMIT}
+            total={total}
+            offset={offset}
+            onOffsetChange={handleChangeOffset}
+          />
+        </Flex>
+        <Box></Box>
+      </Grid>
     </>
   );
 };
