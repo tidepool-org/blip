24df5986c5149fd3ac5e4d6499d48696ed99d80a
henry-tp <henry.liang@tidepool.org>
2026-07-08 16:10:28 -0700

    WEB-4460 use getInitialState fn for redux slice

--- app/pages/clinicworkspace/TideDashboardV2/tideDashboardFiltersSlice.js
+++ app/pages/clinicworkspace/TideDashboardV2/tideDashboardFiltersSlice.js
@@ -1,22 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const getInitialState = () => ({
   patientTags: [],
   clinicSites: [],
-};
+});
 
 const tideDashboardFiltersSlice = createSlice({
   name: 'tideDashboardFilters',
-  initialState,
+  initialState: getInitialState(),
   reducers: {
-    setTideDashboardFilters: (_state, action) => action.payload || initialState,
+    setTideDashboardFilters: (_state, action) => action.payload || getInitialState(),
     setPatientTagsFilter: (state, action) => {
       state.patientTags = action.payload;
     },
     setClinicSitesFilter: (state, action) => {
       state.clinicSites = action.payload;
     },
-    resetTideDashboardFilters: () => initialState,
+    resetTideDashboardFilters: () => getInitialState(),
   },
 });
 
@@ -24,7 +24,7 @@ export const {
   setPatientTagsFilter,
   setClinicSitesFilter,
   setTideDashboardFilters,
-  resetTideDashboardFilters
+  resetTideDashboardFilters,
 } = tideDashboardFiltersSlice.actions;
 
 export default tideDashboardFiltersSlice.reducer;
