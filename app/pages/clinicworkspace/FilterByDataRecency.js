9e2b61276977a8098a3b78125d6fc382303e0946
henry-tp <henry.liang@tidepool.org>
2026-07-14 00:55:34 -0700

    WEB-4654 abstract out Data Recency

---
+++ app/pages/clinicworkspace/FilterByDataRecency.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import noop from 'lodash/noop';
+
+import DataRecencyFilterDropdown from './components/DataRecencyFilterDropdown';
+
+const FilterByDataRecency = ({
+  activeFilters = {},
+  setActiveFilters = noop,
+}) => {
+  const handleChange = ({ lastData, lastDataType }) => {
+    setActiveFilters({ ...activeFilters, lastData, lastDataType });
+  };
+
+  const { lastData, lastDataType } = activeFilters;
+
+  return (
+    <DataRecencyFilterDropdown
+      onChange={handleChange}
+      lastData={lastData}
+      lastDataType={lastDataType}
+    />
+  );
+};
+
+export default FilterByDataRecency;
