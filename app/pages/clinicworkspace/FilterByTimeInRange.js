1c410adb2c1767471071a0de3d3541ce019eed74
henry-tp <henry.liang@tidepool.org>
2026-07-15 13:16:12 -0700

    WEB-4654 abstract out TIR range filter

---
+++ app/pages/clinicworkspace/FilterByTimeInRange.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import noop from 'lodash/noop';
+
+import TimeInRangeFilterDropdown from './components/TimeInRangeFilterDropdown';
+
+const FilterByTimeInRange = ({
+  activeFilters = {},
+  setActiveFilters = noop,
+}) => {
+  const handleChange = (timeInRange) => {
+    setActiveFilters({ ...activeFilters, timeInRange });
+  };
+
+  const { timeInRange } = activeFilters;
+
+  return (
+    <TimeInRangeFilterDropdown
+      onChange={handleChange}
+      timeInRange={timeInRange}
+    />
+  );
+};
+
+export default FilterByTimeInRange;
