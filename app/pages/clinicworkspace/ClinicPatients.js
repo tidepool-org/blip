3d408985616ae7625e253084cd7a109f7d43efc5
henry-tp <henry.liang@tidepool.org>
2026-07-15 16:25:34 -0700

    WEB-4654 move ClearFilterButtons rendering into AppliedFiltersList

--- app/pages/clinicworkspace/ClinicPatients.js
+++ app/pages/clinicworkspace/ClinicPatients.js
@@ -3556,13 +3556,8 @@ export const ClinicPatients = (props) => {
         <AppliedFiltersList
           activeFilters={activeFilters}
           setActiveFilters={setActiveFilters}
-          rightContent={
-            <ClearFilterButtons
-              patientQueryState={patientQueryState}
-              onClearSearch={handleClearSearch}
-              onResetFilters={handleResetFilters}
-            />
-          }
+          onClearSearch={handleClearSearch}
+          onResetFilters={handleResetFilters}
         />
 
         <Table
