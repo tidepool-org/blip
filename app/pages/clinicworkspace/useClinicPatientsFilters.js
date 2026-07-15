b932de272fea7c9945348962be0b207f3fc45102
henry-tp <henry.liang@tidepool.org>
2026-07-15 12:52:00 -0700

    WEB-4654 re-add comments for `SPECIAL_FILTER_STATES`

--- app/pages/clinicworkspace/useClinicPatientsFilters.js
+++ app/pages/clinicworkspace/useClinicPatientsFilters.js
@@ -11,6 +11,10 @@ export const defaultFilterState = {
   clinicSites: [],
 };
 
+// If we HTTP GET `/patients` without a sites/tags query arg, we receive a list of PwDs with zero
+// or many sites/tags. We need to pass an explicit argument to request PwDs with exactly zero
+// sites/tags. By setting the filter to `['_']`, the query path is set to `/patients?sites=_` or
+// `/patients?tags=_`, which the backend understands as a request for PwDs with zero sites/tags
 export const SPECIAL_FILTER_STATES = {
   ZERO_SITES: ['_'],
   ZERO_TAGS: ['_'],
