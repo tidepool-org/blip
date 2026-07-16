440b71bb59e2b124016ad34b6f7679e19bb8fb47
henry-tp <henry.liang@tidepool.org>
2026-07-16 10:17:29 -0700

    WEB-4654 update trackMetric fn calls

--- app/core/metricUtils.js
+++ app/core/metricUtils.js
@@ -1,3 +1,17 @@
 import appContext from '../bootstrap';
+import { useLocation } from 'react-router-dom';
 
 export const trackMetric = (...args) => appContext.trackMetric(...args);
+
+export const useClinicMetricsPageName = () => {
+  const { pathname } = useLocation();
+
+  switch (pathname) {
+    case '/clinic-workspace':
+    case '/clinic-workspace/patients':
+      return 'Population Health';
+
+    default:
+      return 'Unknown';
+  };
+};
