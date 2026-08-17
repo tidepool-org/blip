4966b96391987596da3c3636db411af9e799c8a7
henry-tp <henry.liang@tidepool.org>
2026-08-17 16:14:07 -0700

    WEB-4460 add modal controllers to dashboard

---
+++ app/pages/clinicworkspace/TideDashboardV2/EditPatientDialogController.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { closeModals } from './tideDashboardSlice';
+import { RTKQueryApi } from '../../../redux/api/baseApi';
+import EditPatientDialog from '../../../components/navpatientheader/EditPatientDialog';
+import { tagTypes } from './tideDashboardApi';
+
+const { TIDE_DASHBOARD_PATIENTS } = tagTypes;
+
+const EditPatientDialogController = ({ api, patients }) => {
+  const dispatch = useDispatch();
+  const editPatientDialog = useSelector(state => state.blip.tideDashboard.editPatientDialog);
+
+  const clinicPatient = patients.find(patient => patient.id === editPatientDialog.patientId);
+
+  const handleCloseModal = () => {
+    dispatch(closeModals());
+  };
+
+  const handleEditSuccess = () => {
+    dispatch(RTKQueryApi.util.invalidateTags([TIDE_DASHBOARD_PATIENTS]));
+  };
+
+  return (
+    <>
+      <EditPatientDialog
+        api={api}
+        clinicPatient={clinicPatient}
+        isOpen={editPatientDialog.isOpen}
+        onClose={handleCloseModal}
+        onEditSuccess={handleEditSuccess}
+      />
+    </>
+  );
+};
+
+export default EditPatientDialogController;
