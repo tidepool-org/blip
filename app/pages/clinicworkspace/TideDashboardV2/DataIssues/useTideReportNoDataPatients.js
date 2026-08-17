e464c171e50acb1b9b2935f64e3be4984b977780
henry-tp <henry.liang@tidepool.org>
2026-08-17 16:57:32 -0700

    WEB-4460 add Data Issues segment

---
+++ app/pages/clinicworkspace/TideDashboardV2/DataIssues/useTideReportNoDataPatients.js
@@ -0,0 +1,35 @@
+import { useSelector } from 'react-redux';
+import { useGetTideReportQuery } from './tideDashboardLegacyApi';
+import useDerivedDataRecencyEndpoints from '../useDerivedDataRecencyEndpoints';
+
+// The Data Issues table always requests the report; `categories` is a required
+// param on the endpoint, but the `noData` group is returned regardless of which
+// category is requested, so we send a single category to satisfy the contract.
+const CATEGORIES = ['meetingTargets'];
+
+const useTideReportNoDataPatients = () => {
+  const selectedClinicId = useSelector(state => state.blip.selectedClinicId);
+  const { summaryPeriod, lastData, patientTags } = useSelector(state => state.blip.tideDashboardFilters);
+
+  // `lastDataFrom` is derived identically to V1's `lastDataCutoff`.
+  const [lastDataCutoff] = useDerivedDataRecencyEndpoints();
+
+  const result = useGetTideReportQuery(
+    {
+      clinicId: selectedClinicId,
+      period: summaryPeriod,
+      lastData,
+      tags: patientTags,
+      lastDataCutoff,
+      categories: CATEGORIES,
+    },
+    { skip: !selectedClinicId }
+  );
+
+  return {
+    ...result,
+    patients: result.data?.patients || [],
+  };
+};
+
+export default useTideReportNoDataPatients;
