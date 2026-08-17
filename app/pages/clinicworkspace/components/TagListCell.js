caa51ad612a1b1faf3948bdd636094432bf3b82f
henry-tp <henry.liang@tidepool.org>
2026-08-17 15:59:07 -0700

    WEB-4460 fix chained fn indent

---
+++ app/pages/clinicworkspace/components/TagListCell.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { TagList } from '../../../components/elements/Tag';
+
+const MAX_TAGS = 2;
+
+const TagListCell = ({ patient }) => {
+  const selectedClinicId = useSelector(state => state.blip.selectedClinicId);
+  const clinic = useSelector(state => state.blip.clinics?.[selectedClinicId]);
+  const patientTags = clinic?.patientTags || [];
+
+  const tagIds = patient?.tags || [];
+  const tags = tagIds
+    .map(tag => patientTags
+    .find(ptTag => ptTag.id === tag)) // TODO: index
+    .filter(Boolean);
+
+  return <TagList tags={tags} maxTagsVisible={MAX_TAGS} />;
+};
+
+export default TagListCell;
