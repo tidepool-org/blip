f8f84d3d626acac9f5db9b4516c21fe7b9768c9d
henry-tp <henry.liang@tidepool.org>
2026-08-18 14:05:00 -0700

    WEB-4460 move last reviewed calls to own api slice

---
+++ app/pages/clinicworkspace/components/ReviewPatientToggle/reviewPatientApi.js
@@ -0,0 +1,28 @@
+import { RTKQueryApi } from '../../../../redux/api/baseApi';
+import { tagTypes as tideDashboardTagTypes } from '../../TideDashboardV2/tideDashboardApi';
+
+const { TIDE_DASHBOARD_PATIENTS } = tideDashboardTagTypes;
+
+const reviewPatientApi = RTKQueryApi.injectEndpoints({
+  endpoints: (builder) => ({
+    setClinicPatientLastReviewed: builder.mutation({
+      query: ({ clinicId, patientId }) => ({
+        url: `/clinics/${clinicId}/patients/${patientId}/reviews`,
+        method: 'PUT',
+      }),
+      invalidatesTags: [TIDE_DASHBOARD_PATIENTS],
+    }),
+    revertClinicPatientLastReviewed: builder.mutation({
+      query: ({ clinicId, patientId }) => ({
+        url: `/clinics/${clinicId}/patients/${patientId}/reviews`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: [TIDE_DASHBOARD_PATIENTS],
+    }),
+  }),
+});
+
+export const {
+  useSetClinicPatientLastReviewedMutation,
+  useRevertClinicPatientLastReviewedMutation,
+} = reviewPatientApi;
