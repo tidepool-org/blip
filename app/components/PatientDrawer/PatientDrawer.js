a38bc9ac9a50985145d328021d4261734f227e76
henry-tp <henry.liang@tidepool.org>
2026-08-18 13:14:49 -0700

    WEB-4460 update drawer import directories

--- app/components/PatientDrawer/PatientDrawer.js
+++ app/components/PatientDrawer/PatientDrawer.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import Icon from '../../../components/elements/Icon';
+import Icon from '../../components/elements/Icon';
 import { useLocation, useHistory } from 'react-router-dom';
 import Drawer from '@material-ui/core/Drawer';
 import styled from '@emotion/styled';
 import { makeStyles } from '@material-ui/core/styles';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 import { Box } from 'theme-ui';
-import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import Overview from './Overview';
 import StackedDaily from './StackedDaily';
 import MenuBar, { OVERVIEW_TAB_INDEX, STACKED_DAILY_TAB_INDEX } from './MenuBar';
 import useAgpCGM from './useAgpCGM';
-import { shadows } from '../../../themes/baseTheme';
-import { useScrollToTop } from '../../../core/hooks';
+import { shadows } from '../../themes/baseTheme';
+import { useScrollToTop } from '../../core/hooks';
 
 const StyledCloseButton = styled(Icon)`
   position: absolute;
@@ -53,7 +52,7 @@ const getAgpPeriodInDays = (period) => {
   }
 };
 
-const DrawerContent = ({ patientId, onClose, api, trackMetric, period }) => {
+const DrawerContent = ({ patientId, onClose, api, period }) => {
   // Only rendered when patient is selected and isOpen is true
   // this will also allow the hook to dismount and for the cleanup to be called
   const location = useLocation();
@@ -85,7 +84,7 @@ const DrawerContent = ({ patientId, onClose, api, trackMetric, period }) => {
   return (
     <>
       <Box sx={{ flexShrink: 0 }}>
-        <MenuBar patientId={patientId} trackMetric={trackMetric} onClose={onClose} selectedTab={selectedTab} onSelectTab={handleSelectTab} />
+        <MenuBar patientId={patientId} onClose={onClose} selectedTab={selectedTab} onSelectTab={handleSelectTab} />
 
         <Box className='sticky-shadow' sx={{ height: '8px', position: 'sticky', zIndex: 1, visibility: scrolledToTop ? 'hidden' : 'visible', boxShadow: shadows.large }} />
       </Box>
@@ -108,12 +107,9 @@ const DrawerContent = ({ patientId, onClose, api, trackMetric, period }) => {
   )
 }
 
-const PatientDrawer = ({ patientId, onClose, api, trackMetric, period }) => {
+const PatientDrawer = ({ patientId, onClose, api, period }) => {
   const classes = useStyles();
-  const { showTideDashboardPatientDrawer } = useFlags();
   const isOpen = !!patientId && isValidAgpPeriod(period);
-
-  if (!showTideDashboardPatientDrawer) return null;
 
   return (
     <StyledDrawer
@@ -133,7 +129,7 @@ const PatientDrawer = ({ patientId, onClose, api, trackMetric, period }) => {
           flexDirection: 'column',
         }}
       >
-        {isOpen && <DrawerContent patientId={patientId} onClose={onClose} api={api} trackMetric={trackMetric} period={period} />}
+        {isOpen && <DrawerContent patientId={patientId} onClose={onClose} api={api} period={period} />}
       </Box>
     </StyledDrawer>
   );
