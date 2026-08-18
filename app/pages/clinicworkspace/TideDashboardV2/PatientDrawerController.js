bcc1e5a292191b7fea361d7acf61392d16408733
henry-tp <henry.liang@tidepool.org>
2026-08-18 12:58:09 -0700

    WEB-4460 pull in PatientDrawer

---
+++ app/pages/clinicworkspace/TideDashboardV2/PatientDrawerController.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation, useHistory } from 'react-router-dom';
+import PatientDrawer from '../../../components/PatientDrawer';
+
+const PatientDrawerController = ({ api }) => {
+  const summaryPeriod = useSelector(state => state.blip.tideDashboardFilters.summaryPeriod);
+  const { search, pathname } = useLocation();
+  const history = useHistory();
+
+  const drawerPatientId = new URLSearchParams(search)?.get('drawerPatientId') || null;
+
+  const handleClose = () => {
+    const params = new URLSearchParams(search);
+    params.delete('drawerPatientId');
+    params.delete('drawerTab');
+    history.replace({ pathname, search: params.toString() });
+  };
+
+  return (
+    <PatientDrawer
+      api={api}
+      patientId={drawerPatientId}
+      onClose={handleClose}
+      period={summaryPeriod}
+    />
+  );
+};
+
+export default PatientDrawerController;
