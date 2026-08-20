cd93597a14604b2dfe624cc0c0099fc6b254deb9
henry-tp <henry.liang@tidepool.org>
2026-08-20 16:00:05 -0700

    Revert "WEB-4460 delete work related to filter persistence to move it upstream"
    This reverts commit 9c68578b4f53518182b106b0d7742691375dafe3.

--- app/pages/clinicworkspace/TideDashboardV2/useDerivedDataRecencyEndpoints.js
+++ app/pages/clinicworkspace/TideDashboardV2/useDerivedDataRecencyEndpoints.js
@@ -4,18 +4,8 @@ import { useSelector } from 'react-redux';
 import { utils as vizUtils } from '@tidepool/viz';
 const { getLocalizedCeiling } = vizUtils.datetime;
 
-// TEMPORARY, will be set in redux
-const tideDashboardFilters = {
-  lastData: 7,
-  patientTags: [],
-  clinicSites: [],
-  summaryPeriod: '14d',
-};
-
-const useDerivedDataRecencyEndpoints = (
-  filters = tideDashboardFilters // TEMPORARY, will be set in redux
-) => {
-  const lastData = filters.lastData;
+const useDerivedDataRecencyEndpoints = () => {
+  const lastData = useSelector(state => state.blip.tideDashboardFilters.lastData);
   const timePrefs = useSelector((state) => state.blip.timePrefs);
 
   const lastDataTo = getLocalizedCeiling(new Date().toISOString(), timePrefs).toISOString();
