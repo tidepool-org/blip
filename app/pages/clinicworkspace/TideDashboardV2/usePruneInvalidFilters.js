2d0feeadd848a4f2dc63eb83bcf733ebca28eb60
henry-tp <henry.liang@tidepool.org>
2026-08-24 16:59:39 -0700

    WEB-4460 add tests for usePruneInvalidFilters

--- app/pages/clinicworkspace/TideDashboardV2/usePruneInvalidFilters.js
+++ app/pages/clinicworkspace/TideDashboardV2/usePruneInvalidFilters.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPatientTagsFilter, setClinicSitesFilter } from './tideDashboardFiltersSlice';
+import { SPECIAL_FILTER_STATES } from '../useClinicPatientsFilters';
 import keyBy from 'lodash/keyBy';
+import isEqual from 'lodash/isEqual';
 
 const usePruneInvalidFilters = () => {
   const dispatch = useDispatch();
@@ -11,6 +13,8 @@ const usePruneInvalidFilters = () => {
 
   useEffect(() => {
     if (!patientTags?.length || !clinic) return;
+
+    if (isEqual(patientTags, SPECIAL_FILTER_STATES.ZERO_TAGS)) return;
 
     const availableTags = keyBy(clinic.patientTags || [], 'id');
     const prunedTags = patientTags.filter(tagId => !!availableTags[tagId]);
@@ -22,6 +26,8 @@ const usePruneInvalidFilters = () => {
 
   useEffect(() => {
     if (!clinicSites?.length || !clinic) return;
+
+    if (isEqual(clinicSites, SPECIAL_FILTER_STATES.ZERO_SITES)) return;
 
     const availableSites = keyBy(clinic.sites || [], 'id');
     const prunedSites = clinicSites.filter(siteId => !!availableSites[siteId]);
