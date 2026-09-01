3995119e7e2de21f51da9bb13637e0cde2ab1196
henry-tp <henry.liang@tidepool.org>
2026-09-01 15:45:03 -0700

    WEB-4460 call generic adapter directly in cells

--- app/pages/clinicworkspace/TideDashboardV2/PatientLastReviewed.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PatientLastReviewedGenericAdapter from '../components/ReviewPatientToggle/PatientLastReviewedGenericAdapter';
-
-const PatientLastReviewed = (props) => {
-  return <PatientLastReviewedGenericAdapter {...props} />;
-};
-
-export default PatientLastReviewed;
