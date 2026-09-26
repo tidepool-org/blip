fe66d04774cce3e5bd2d47d96caf28d297f068ed
henry-tp <henry.liang@tidepool.org>
2026-09-25 19:00:05 -0700

    WEB-4740 update bgDaysWorn to capture all cases

--- app/pages/dashboard/PatientDrawer/getReportDaysText.js
+++ app/pages/dashboard/PatientDrawer/getReportDaysText.js
@@ -3,8 +3,8 @@ import { MS_IN_MIN } from '../../../core/constants';
 import { utils as vizUtils } from '@tidepool/viz';
 const { getOffset, formatDataDateRange } = vizUtils.datetime;
 
-const getReportDaysText = ({ newestDatum, oldestDatum, bg24hPeriodsWorn, timezone }) => {
-  if (bg24hPeriodsWorn === 1) {
+const getReportDaysText = ({ newestDatum, oldestDatum, bgDaysWorn, timezone }) => {
+  if (bgDaysWorn === 1) {
     return moment.utc(newestDatum?.time - getOffset(newestDatum?.time, timezone) * MS_IN_MIN).format('MMM D, YYYY');
   }
 
